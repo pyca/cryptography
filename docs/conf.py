c113c3b661c161ae7c3fef54f9abca8ce8428c18
Alex Gaynor <alex.gaynor@gmail.com>
2013-08-07 08:36:20 -0700

    Python 3.2 compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -42,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Cryptography'
-copyright = u'2013, Individual Contributors'
+project = 'Cryptography'
+copyright = '2013, Individual Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,8 +185,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual])
 latex_documents = [
-    ('index', 'Cryptography.tex', u'Cryptography Documentation',
-        u'Individual Contributors', 'manual'),
+    ('index', 'Cryptography.tex', 'Cryptography Documentation',
+        'Individual Contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,8 +215,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'cryptography', u'Cryptography Documentation',
-        [u'Individual Contributors'], 1)
+    ('index', 'cryptography', 'Cryptography Documentation',
+        ['Individual Contributors'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'Cryptography', u'Cryptography Documentation',
-        u'Individual Contributors', 'Cryptography',
+    ('index', 'Cryptography', 'Cryptography Documentation',
+        'Individual Contributors', 'Cryptography',
         'One line description of project.',
         'Miscellaneous'),
 ]
