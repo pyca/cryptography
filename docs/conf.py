af82d5eaeda02561b29adec38d40bf22c3a8e23b
Alex Gaynor <alex.gaynor@gmail.com>
2013-10-29 17:07:24 -0700

    Made a sphinx extension which emits the hazardous materials danger admonition

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os
+import sys
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ----------------------------------------------------
 
@@ -28,6 +31,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'cryptography-docs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
