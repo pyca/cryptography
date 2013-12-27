c9aeb34db5f0a675f60742ea82d67d92ea75865c
Alex Gaynor <alex.gaynor@gmail.com>
2013-12-27 11:07:42 -0800

    A handful of style fixes in the cffi bindings

--- cryptography/hazmat/backends/openssl/objects.py
+++ cryptography/hazmat/backends/openssl/objects.py
@@ -19,17 +19,17 @@ TYPES = """
 """
 
 FUNCTIONS = """
-ASN1_OBJECT * OBJ_nid2obj(int);
-const char * OBJ_nid2ln(int);
-const char * OBJ_nid2sn(int);
+ASN1_OBJECT *OBJ_nid2obj(int);
+const char *OBJ_nid2ln(int);
+const char *OBJ_nid2sn(int);
 int OBJ_obj2nid(const ASN1_OBJECT *);
 int OBJ_ln2nid(const char *);
 int OBJ_sn2nid(const char *);
 int OBJ_txt2nid(const char *);
-ASN1_OBJECT * OBJ_txt2obj(const char *, int);
+ASN1_OBJECT *OBJ_txt2obj(const char *, int);
 int OBJ_obj2txt(char *, int, const ASN1_OBJECT *, int);
 int OBJ_cmp(const ASN1_OBJECT *, const ASN1_OBJECT *);
-ASN1_OBJECT * OBJ_dup(const ASN1_OBJECT *);
+ASN1_OBJECT *OBJ_dup(const ASN1_OBJECT *);
 int OBJ_create(const char *, const char *, const char *);
 void OBJ_cleanup();
 """
