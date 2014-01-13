26287be0dccc51730608475fdde8f0b3f474e20e
Paul Kehrer <paul.l.kehrer@gmail.com>
2014-01-12 23:50:42 -0500

    check if openssl is installed via homebrew on osx. install if not

--- .travis/run.sh
+++ .travis/run.sh
@@ -6,8 +6,7 @@ set -x
 if [[ "$(uname -s)" == "Darwin" ]]; then
     eval "$(pyenv init -)"
     if [[ "${OPENSSL}" != "0.9.8" ]]; then
-        # travis has 1.0.1e installed via homebrew on OS X by default
-        # so set our flags to use that
+        # so set our flags to use homebrew openssl
         export ARCHFLAGS="-arch x86_64"
         export LDFLAGS="-L/usr/local/opt/openssl/lib"
         export CFLAGS="-I/usr/local/opt/openssl/include"
