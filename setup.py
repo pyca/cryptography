1b3899f94b6e7186bc661965c85eb1038b6ff0ff
Alex Gaynor <alex.gaynor@gmail.com>
2014-02-07 10:31:35 -0800

    We're now past pre-alpha

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     author_email=about["__email__"],
 
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
