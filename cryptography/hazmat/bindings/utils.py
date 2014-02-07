f2ae260e3858c78477b1e9b66e86a87b6c0f5fe5
Alex Gaynor <alex.gaynor@gmail.com>
2014-02-07 07:07:25 -0800

    These are not for windows

--- cryptography/hazmat/bindings/utils.py
+++ cryptography/hazmat/bindings/utils.py
@@ -52,7 +52,10 @@ def build_ffi(module_prefix, modules, pre_include, post_include, libraries):
 
     ffi.cdef("\n".join(types + functions + macros))
 
-    compiler_args = ["-Werror", "-Wno-error=unused-command-line-argument"]
+    if sys.platform != "win32":
+        compiler_args = ["-Werror", "-Wno-error=unused-command-line-argument"]
+    else:
+        compiler_args = []
     # We include functions here so that if we got any of their definitions
     # wrong, the underlying C compiler will explode. In C you are allowed
     # to re-declare a function if it has the same signature. That is:
