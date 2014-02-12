e2d45452119e3992513605f2b32d37d37093abaf
Alex Gaynor <alex.gaynor@gmail.com>
2014-02-12 09:58:41 -0800

    Also fix this module

--- cryptography/fernet.py
+++ cryptography/fernet.py
@@ -22,8 +22,8 @@ import six
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes
-from cryptography.hazmat.primitives.hmac import HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.hmac import HMAC
 
 
 class InvalidToken(Exception):
