2656187b3407af85facdd65df6c36ca5607fdb84
Paul Kehrer <paul.l.kehrer@gmail.com>
2014-02-20 17:42:34 -0600

    another null check

--- cryptography/hazmat/backends/openssl/backend.py
+++ cryptography/hazmat/backends/openssl/backend.py
@@ -336,6 +336,7 @@ class Backend(object):
 
     def _rsa_ctx_from_public_key(self, public_key):
         ctx = self._lib.RSA_new()
+        assert ctx != self._ffi.NULL
         ctx = self._ffi.gc(ctx, self._lib.RSA_free)
         ctx.e = self._int_to_bn(public_key.e)
         ctx.n = self._int_to_bn(public_key.n)
