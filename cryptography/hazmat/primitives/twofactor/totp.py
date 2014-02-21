f39716de33ad0b387f829bc111c8490d57ad6cf6
Ayrx <terrycwk1994@gmail.com>
2014-02-21 14:38:30 +0800

    Small fixes

--- cryptography/hazmat/primitives/twofactor/totp.py
+++ cryptography/hazmat/primitives/twofactor/totp.py
@@ -11,8 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
-
 from cryptography.exceptions import InvalidToken
 from cryptography.hazmat.primitives import constant_time
 from cryptography.hazmat.primitives.twofactor.hotp import HOTP
@@ -22,12 +20,12 @@ class TOTP(object):
     def __init__(self, key, length, algorithm, time_step, backend):
 
         self._time_step = time_step
-        self.hotp = HOTP(key, length, algorithm, backend)
+        self._hotp = HOTP(key, length, algorithm, backend)
 
     def generate(self, time):
-        counter = int(math.floor(time/self._time_step))
-        return self.hotp.generate(counter)
+        counter = int(time/self._time_step)
+        return self._hotp.generate(counter)
 
     def verify(self, totp, time):
         if not constant_time.bytes_eq(self.generate(time), totp):
-            raise InvalidToken("Supplied HOTP value does not match")
+            raise InvalidToken("Supplied TOTP value does not match")
