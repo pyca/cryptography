8dd9713ae2a69a3e870275c088df08ce2a50dce9
Paul Kehrer <paul.l.kehrer@gmail.com>
2014-02-23 19:26:05 -0600

    incorporate review feedback. kwarg!

--- docs/hazmat/primitives/asymmetric/rsa.rst
+++ docs/hazmat/primitives/asymmetric/rsa.rst
@@ -74,7 +74,11 @@ RSA
             >>> from cryptography.hazmat.backends import default_backend
             >>> from cryptography.hazmat.primitives import hashes
             >>> from cryptography.hazmat.primitives.asymmetric import rsa, padding
-            >>> private_key = rsa.RSAPrivateKey.generate(65537, 2048, default_backend())
+            >>> private_key = rsa.RSAPrivateKey.generate(
+            ...     public_exponent=65537,
+            ...     key_size=2048,
+            ...     backend=default_backend()
+            ... )
             >>> signer = private_key.signer(padding.PKCS1(), hashes.SHA256(), default_backend())
             >>> signer.update(b"this is some data I'd like")
             >>> signer.update(b" to sign")
--- tests/hazmat/primitives/test_rsa.py
+++ tests/hazmat/primitives/test_rsa.py
@@ -397,7 +397,16 @@ class TestRSASignature(object):
     )
     def test_pkcs1v15_signing(self, pkcs1_example, backend):
         private, public, example = pkcs1_example
-        private_key = rsa.RSAPrivateKey(**private)
+        private_key = rsa.RSAPrivateKey(
+            p=private["p"],
+            q=private["q"],
+            private_exponent=private["private_exponent"],
+            dmp1=private["dmp1"],
+            dmq1=private["dmq1"],
+            iqmp=private["iqmp"],
+            public_exponent=private["public_exponent"],
+            modulus=private["modulus"]
+        )
         signer = private_key.signer(padding.PKCS1(), hashes.SHA1(), backend)
         signer.update(binascii.unhexlify(example["message"]))
         signature = signer.finalize()
