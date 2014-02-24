066718022e8cb1c9e6326d944c99280905ad0a82
koobs <koobs@users.noreply.github.com>
2014-02-24 22:33:07 +1100

    And again. Remove whitespace (pep8)

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ class PyTest(TestCommand):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
-        
+
     def run_tests(self):
         # Import here because in module scope the eggs are not loaded.
         import pytest
