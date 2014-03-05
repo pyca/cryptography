05c37cce58aeef99ae8e011c5c0b672b87724dd5
Alex Gaynor <alex.gaynor@gmail.com>
2014-03-05 14:04:53 -0800

    Simplify

--- cryptography/hazmat/bindings/openssl/ec.py
+++ cryptography/hazmat/bindings/openssl/ec.py
@@ -24,7 +24,8 @@ static const int Cryptography_HAS_EC;
 
 typedef ... EC_KEY;
 typedef struct {
-    ...;
+    int nid;
+    const char *comment;
 } EC_builtin_curve;
 
 static const int NID_X9_62_c2pnb163v1;
@@ -181,24 +182,21 @@ void EC_KEY_free(EC_KEY *);
 
 size_t EC_get_builtin_curves(EC_builtin_curve *, size_t);
 
-int Cryptography_EC_builtin_curve_get_nid(EC_builtin_curve *);
 """
 
 CUSTOMIZATIONS = """
 #ifdef OPENSSL_NO_EC
 static const long Cryptography_HAS_EC = 0;
 typedef void EC_KEY;
-typedef void EC_builtin_curve;
+typedef struct {
+    int nid;
+    const char *comment;
+} EC_builtin_curve;
 EC_KEY* (*EC_KEY_new_by_curve_name)(int) = NULL;
 void (*EC_KEY_free)(EC_KEY *) = NULL;
 size_t (*EC_get_builtin_curves)(EC_builtin_curve *, size_t) = NULL;
-int (*Cryptography_EC_builtin_curve_get_nid)(EC_builtin_curve *) = NULL;
 #else
 static const long Cryptography_HAS_EC = 1;
-
-int Cryptography_EC_builtin_curve_get_nid(EC_builtin_curve *c) {
-    return c->nid;
-}
 #endif
 """
 
@@ -207,7 +205,5 @@ CONDITIONAL_NAMES = {
         "EC_KEY_new_by_curve_name",
         "EC_KEY_free",
         "EC_get_builtin_curves",
-
-        "Cryptography_EC_builtin_curve_get_nid",
     ],
 }
