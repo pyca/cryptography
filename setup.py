7f8b277661ee1e33034743c9d3a49770fa78b2c6
Alex Gaynor <alex.gaynor@gmail.com>
2014-03-17 10:22:41 -0700

    We support 3.4

--- setup.py
+++ setup.py
@@ -119,6 +119,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Security :: Cryptography",
