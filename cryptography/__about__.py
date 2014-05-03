50a048536c5443a86e47a967dadf38bc4241b593
Paul Kehrer <paul.l.kehrer@gmail.com>
2014-05-03 08:57:13 -0500

    update versions and reopen master

--- cryptography/__about__.py
+++ cryptography/__about__.py
@@ -22,7 +22,7 @@ __summary__ = ("cryptography is a package which provides cryptographic recipes"
                " and primitives to Python developers.")
 __uri__ = "https://github.com/pyca/cryptography"
 
-__version__ = "0.4"
+__version__ = "0.5.dev1"
 
 __author__ = "The cryptography developers"
 __email__ = "cryptography-dev@python.org"
