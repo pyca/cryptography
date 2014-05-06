b6f6cdecfc7b82d15a766a13f0d5ca6c5893c82d
PhiBo <phibo@dinotools.org>
2014-05-07 00:48:43 +0200

    Fix to build without secure renegotiation support

--- cryptography/hazmat/bindings/openssl/ssl.py
+++ cryptography/hazmat/bindings/openssl/ssl.py
@@ -364,12 +364,14 @@ SSL_CIPHER *sk_SSL_CIPHER_value(Cryptography_STACK_OF_SSL_CIPHER *, int);
 """
 
 CUSTOMIZATIONS = """
-/* Secure renegotiation is supported in OpenSSL >= 0.9.8m */
-#if OPENSSL_VERSION_NUMBER < 0x009080dfL
+/** Secure renegotiation is supported in OpenSSL >= 0.9.8m
+ *  But some Linux distributions have back ported some features.
+ */
+#ifndef SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION
 static const long Cryptography_HAS_SECURE_RENEGOTIATION = 0;
-long SSL_get_secure_renegotiation_support(SSL *) = 0;
-static const SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION = 0;
-static const SSL_OP_LEGACY_SERVER_CONNECT = 0;
+long (*SSL_get_secure_renegotiation_support)(SSL *) = NULL;
+const long SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION = 0;
+const long SSL_OP_LEGACY_SERVER_CONNECT = 0;
 #else
 static const long Cryptography_HAS_SECURE_RENEGOTIATION = 1;
 #endif
