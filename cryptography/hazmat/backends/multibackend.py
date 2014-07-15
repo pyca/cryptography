ce8298e6181b1b0fa9df16cecc88096f02f91fa9
David Reid <dreid@dreid.org>
2014-07-15 13:42:38 -0700

    Uhh remove some extra lines.

--- cryptography/hazmat/backends/multibackend.py
+++ cryptography/hazmat/backends/multibackend.py
@@ -13,8 +13,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-
-
 from cryptography import utils
 from cryptography.exceptions import UnsupportedAlgorithm, _Reasons
 from cryptography.hazmat.backends.interfaces import (
