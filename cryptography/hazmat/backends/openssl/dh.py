b83e19d6ec84b052f6906cb396b2f28260ed5556
Alex Stapleton <alexs@prol.etari.at>
2014-07-21 13:11:57 +0200

    Some actual code

--- cryptography/hazmat/backends/openssl/dh.py
+++ cryptography/hazmat/backends/openssl/dh.py
@@ -16,3 +16,22 @@ from __future__ import absolute_import, division, print_function
 from cryptography import utils
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric import dh
+
+
+class _DHKeyAgreementContext(object):
+    def __init__(self, private_key, backend):
+        self._private_key = private_key
+        self._backend = backend
+
+    def agree(self, public_key):
+        lib = self._backend._lib
+        ffi = self._backend._ffi
+
+        key_size = lib.DH_size(private_key)
+
+        buf = ffi.new("char[]", key_size)
+        res = lib.DH_compute_key(
+            key_buf, public_key, private_key
+        )
+        assert res != -1
+        return ffi.buffer(buf)[:key_size]
