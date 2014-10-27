9f38540c7d0018d00305425d61b41cb54e3a560c
Terry Chia <terrycwk1994@gmail.com>
2014-10-27 10:38:44 +0800

    Make indentation use space instead of tabs.

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,7 +13,7 @@ if [[ "$DARWIN" = true ]]; then
     brew update
 
     if [[ "${OPENSSL}" == "0.9.8" ]]; then
-	brew upgrade openssl
+        brew upgrade openssl
     fi
 
     if which pyenv > /dev/null; then
@@ -21,41 +21,41 @@ if [[ "$DARWIN" = true ]]; then
     fi
 
     case "${TOX_ENV}" in
-	py26)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    ;;
-	py27)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    ;;
-	py32)
-	    brew upgrade pyenv
-	    pyenv install 3.2.6
-	    pyenv global 3.2.6
-	    ;;
-	py33)
-	    brew upgrade pyenv
-	    pyenv install 3.3.6
-	    pyenv global 3.3.6
-	    ;;
-	py34)
-	    brew upgrade pyenv
-	    pyenv install 3.4.2
-	    pyenv global 3.4.2
-	    ;;
-	py3pep8)
-	    sudo apt-get install python3.3 python3.3-dev
-	    ;;
-	pypy)
-	    brew upgrade pyenv
-	    pyenv install pypy-2.4.0
-	    pyenv global pypy-2.4.0
-	    ;;
-	docs)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    ;;
+    py26)
+        curl -O https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
+        ;;
+    py27)
+        curl -O https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
+        ;;
+    py32)
+        brew upgrade pyenv
+        pyenv install 3.2.6
+        pyenv global 3.2.6
+        ;;
+    py33)
+        brew upgrade pyenv
+        pyenv install 3.3.6
+        pyenv global 3.3.6
+        ;;
+    py34)
+        brew upgrade pyenv
+        pyenv install 3.4.2
+        pyenv global 3.4.2
+        ;;
+    py3pep8)
+        sudo apt-get install python3.3 python3.3-dev
+        ;;
+    pypy)
+        brew upgrade pyenv
+        pyenv install pypy-2.4.0
+        pyenv global pypy-2.4.0
+        ;;
+    docs)
+        curl -O https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
+        ;;
     esac
     pyenv rehash
 
@@ -63,41 +63,41 @@ else
     sudo add-apt-repository -y ppa:fkrull/deadsnakes
 
     if [[ "${TOX_ENV}" == "pypy" ]]; then
-	sudo add-apt-repository -y ppa:pypy/ppa
+        sudo add-apt-repository -y ppa:pypy/ppa
     fi
 
     if [[ "${OPENSSL}" == "0.9.8" ]]; then
-	sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ lucid main"
+        sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ lucid main"
     fi
 
     sudo apt-get -y update
 
     if [[ "${OPENSSL}" == "0.9.8" ]]; then
-	sudo apt-get install -y --force-yes libssl-dev/lucid
+        sudo apt-get install -y --force-yes libssl-dev/lucid
     fi
 
     case "${TOX_ENV}" in
-	py26)
-	    sudo apt-get install python2.6 python2.6-dev
-	    ;;
-	py32)
-	    sudo apt-get install python3.2 python3.2-dev
-	    ;;
-	py33)
-	    sudo apt-get install python3.3 python3.3-dev
-	    ;;
-	py34)
-	    sudo apt-get install python3.4 python3.4-dev
-	    ;;
-	py3pep8)
-	    sudo apt-get install python3.3 python3.3-dev
-	    ;;
-	pypy)
-	    sudo apt-get install --force-yes pypy pypy-dev
-	    ;;
-	docs)
-	    sudo apt-get install libenchant-dev
-	    ;;
+    py26)
+        sudo apt-get install python2.6 python2.6-dev
+        ;;
+    py32)
+        sudo apt-get install python3.2 python3.2-dev
+        ;;
+    py33)
+        sudo apt-get install python3.3 python3.3-dev
+        ;;
+    py34)
+        sudo apt-get install python3.4 python3.4-dev
+        ;;
+    py3pep8)
+        sudo apt-get install python3.3 python3.3-dev
+        ;;
+    pypy)
+        sudo apt-get install --force-yes pypy pypy-dev
+        ;;
+    docs)
+        sudo apt-get install libenchant-dev
+        ;;
     esac
 fi
 
