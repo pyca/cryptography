f67fcf9f9bc4d18e1ee2291d8e2b4fbe765fe68c
Terry Chia <terrycwk1994@gmail.com>
2014-10-29 10:52:21 +0800

    I fail at indentation.

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,41 +21,41 @@ if [[ "$DARWIN" = true ]]; then
     fi
 
     case "${TOX_ENV}" in
-    py26)
-        curl -O https://bootstrap.pypa.io/get-pip.py
-        sudo python get-pip.py
-        ;;
-    py27)
-        curl -O https://bootstrap.pypa.io/get-pip.py
-        sudo python get-pip.py
-        ;;
-    py32)
-        brew upgrade pyenv
-        pyenv install 3.2.6
-        pyenv global 3.2.6
-        ;;
-    py33)
-        brew upgrade pyenv
-        pyenv install 3.3.6
-        pyenv global 3.3.6
-        ;;
-    py34)
-        brew upgrade pyenv
-        pyenv install 3.4.2
-        pyenv global 3.4.2
-        ;;
-    py3pep8)
-        sudo apt-get install python3.3 python3.3-dev
-        ;;
-    pypy)
-        brew upgrade pyenv
-        pyenv install pypy-2.4.0
-        pyenv global pypy-2.4.0
-        ;;
-    docs)
-        curl -O https://bootstrap.pypa.io/get-pip.py
-        sudo python get-pip.py
-        ;;
+        py26)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            ;;
+        py27)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            ;;
+        py32)
+            brew upgrade pyenv
+            pyenv install 3.2.6
+            pyenv global 3.2.6
+            ;;
+        py33)
+            brew upgrade pyenv
+            pyenv install 3.3.6
+            pyenv global 3.3.6
+            ;;
+        py34)
+            brew upgrade pyenv
+            pyenv install 3.4.2
+            pyenv global 3.4.2
+            ;;
+        py3pep8)
+            sudo apt-get install python3.3 python3.3-dev
+            ;;
+        pypy)
+            brew upgrade pyenv
+            pyenv install pypy-2.4.0
+            pyenv global pypy-2.4.0
+            ;;
+        docs)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            ;;
     esac
     pyenv rehash
 
@@ -77,27 +77,27 @@ else
     fi
 
     case "${TOX_ENV}" in
-    py26)
-        sudo apt-get install python2.6 python2.6-dev
-        ;;
-    py32)
-        sudo apt-get install python3.2 python3.2-dev
-        ;;
-    py33)
-        sudo apt-get install python3.3 python3.3-dev
-        ;;
-    py34)
-        sudo apt-get install python3.4 python3.4-dev
-        ;;
-    py3pep8)
-        sudo apt-get install python3.3 python3.3-dev
-        ;;
-    pypy)
-        sudo apt-get install --force-yes pypy pypy-dev
-        ;;
-    docs)
-        sudo apt-get install libenchant-dev
-        ;;
+        py26)
+            sudo apt-get install python2.6 python2.6-dev
+            ;;
+        py32)
+            sudo apt-get install python3.2 python3.2-dev
+            ;;
+        py33)
+            sudo apt-get install python3.3 python3.3-dev
+            ;;
+        py34)
+            sudo apt-get install python3.4 python3.4-dev
+            ;;
+        py3pep8)
+            sudo apt-get install python3.3 python3.3-dev
+            ;;
+        pypy)
+            sudo apt-get install --force-yes pypy pypy-dev
+            ;;
+        docs)
+            sudo apt-get install libenchant-dev
+            ;;
     esac
 fi
 
