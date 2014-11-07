f77573b4c227b7dfc44661e2a46131646910e9ca
Alex Gaynor <alex.gaynor@gmail.com>
2014-11-07 16:57:45 -0300

    support conditionals?

--- cryptography/hazmat/bindings/utils.py
+++ cryptography/hazmat/bindings/utils.py
@@ -76,15 +76,7 @@ def build_ffi_for_binding(module_prefix, modules, pre_include="",
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
     )
-
-    for name in modules:
-        module_name = module_prefix + name
-        module = sys.modules[module_name]
-        for condition, names in module.CONDITIONAL_NAMES.items():
-            if not getattr(lib, condition):
-                for name in names:
-                    delattr(lib, name)
-
+    lib = LazyLibraryWithConditionals(lib._verifier, modules, module_prefix)
     return ffi, lib
 
 
@@ -99,6 +91,30 @@ class LazyLibrary(object):
             with self._lock:
                 if self._lib is None:
                     self._lib = self._verifier.load_library()
+        return getattr(self._lib, name)
+
+
+class LazyLibraryWithConditionals(object):
+    def __init__(self, verifier, modules, module_prefix):
+        self._lock = threading.Lock()
+        self._verifier = verifier
+        self._lib = None
+        self._modules = modules
+        self._module_prefix = module_prefix
+
+    def __getattr__(self, name):
+        if self._lib is None:
+            with self._lock:
+                if self._lib is None:
+                    lib = self._verifier.load_library()
+                    for name in self._modules:
+                        module_name = self._module_prefix + name
+                        module = sys.modules[module_name]
+                        for condition, names in module.CONDITIONAL_NAMES.items():
+                            if not getattr(lib, condition):
+                                for name in names:
+                                    delattr(lib, name)
+                    self._lib = lib
         return getattr(self._lib, name)
 
 
