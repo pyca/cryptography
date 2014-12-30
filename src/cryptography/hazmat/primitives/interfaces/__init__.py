6829d7e54a72590cdc0d0af09d908a795df22d0a
Gregory Haynes <greg@greghaynes.net>
2014-12-30 10:31:20 -0800

    Start splitting out interfaces with ciphers
    The interfaces module is getting unwieldy so starting to split this out.
    Beginning by creating a ciphers module.
    
    This starts implementing issue #1495

--- src/cryptography/hazmat/primitives/interfaces/__init__.py
+++ src/cryptography/hazmat/primitives/interfaces/__init__.py
@@ -4,67 +4,24 @@
 
 from __future__ import absolute_import, division, print_function
 
-import abc
-
 import six
 
+import abc
 
-@six.add_metaclass(abc.ABCMeta)
-class CipherAlgorithm(object):
-    @abc.abstractproperty
-    def name(self):
-        """
-        A string naming this mode (e.g. "AES", "Camellia").
-        """
+from cryptography.hazmat.primitives.interfaces.ciphers import (
+    BlockCipherAlgorithm,
+    CipherAlgorithm,
+    Mode,
+    ModeWithInitializationVector,
+    ModeWithNonce)
 
-    @abc.abstractproperty
-    def key_size(self):
-        """
-        The size of the key being used as an integer in bits (e.g. 128, 256).
-        """
-
-
-@six.add_metaclass(abc.ABCMeta)
-class BlockCipherAlgorithm(object):
-    @abc.abstractproperty
-    def block_size(self):
-        """
-        The size of a block as an integer in bits (e.g. 64, 128).
-        """
-
-
-@six.add_metaclass(abc.ABCMeta)
-class Mode(object):
-    @abc.abstractproperty
-    def name(self):
-        """
-        A string naming this mode (e.g. "ECB", "CBC").
-        """
-
-    @abc.abstractmethod
-    def validate_for_algorithm(self, algorithm):
-        """
-        Checks that all the necessary invariants of this (mode, algorithm)
-        combination are met.
-        """
-
-
-@six.add_metaclass(abc.ABCMeta)
-class ModeWithInitializationVector(object):
-    @abc.abstractproperty
-    def initialization_vector(self):
-        """
-        The value of the initialization vector for this mode as bytes.
-        """
-
-
-@six.add_metaclass(abc.ABCMeta)
-class ModeWithNonce(object):
-    @abc.abstractproperty
-    def nonce(self):
-        """
-        The value of the nonce for this mode as bytes.
-        """
+__all__ = [
+    "BlockCipherAlgorithm",
+    "CipherAlgorithm",
+    "Mode",
+    "ModeWithInitializationVector",
+    "ModeWithNonce"
+]
 
 
 @six.add_metaclass(abc.ABCMeta)
