f8da8e20f0d0265a4f4465eef1294ee59f3edb0e
Alex Gaynor <alex.gaynor@gmail.com>
2015-01-08 13:30:51 -0800

    Line length and typo

--- src/cryptography/hazmat/backends/__init__.py
+++ src/cryptography/hazmat/backends/__init__.py
@@ -20,8 +20,8 @@ def _available_backends():
             # setuptools 11.3 deprecated support for the require parameter to
             # load(), and introduced the new resolve() method instead.
             # This can be removed if/when we can assume setuptools>=11.3. At
-            # some point we may wish to add a warning, to push people along, but
-            # at present this would result it too many warnings.
+            # some point we may wish to add a warning, to push people along,
+            # but at present this would result in too many warnings.
             ep.resolve() if hasattr(ep, "resolve") else ep.load(require=False)
             for ep in pkg_resources.iter_entry_points(
                 "cryptography.backends"
