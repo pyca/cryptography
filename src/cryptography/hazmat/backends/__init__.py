d334e2f4c7507bb01369e5fc834163aabf1d46d6
Alex Gaynor <alex.gaynor@gmail.com>
2015-01-08 13:23:39 -0800

    Added a comment

--- src/cryptography/hazmat/backends/__init__.py
+++ src/cryptography/hazmat/backends/__init__.py
@@ -17,6 +17,11 @@ def _available_backends():
 
     if _available_backends_list is None:
         _available_backends_list = [
+            # setuptools 11.3 deprecated support for the require parameter to
+            # load(), and introduced the new resolve() method instead.
+            # This can be removed if/when we can assume setuptools>=11.3. At
+            # some point we may wish to add a warning, to push people along, but
+            # at present this would result it too many warnings.
             ep.resolve() if hasattr(ep, "resolve") else ep.load(require=False)
             for ep in pkg_resources.iter_entry_points(
                 "cryptography.backends"
