d8a16937e2e6a7ab4df51ba582286ca287ffaacd
Alex Stapleton <alex@ly.st>
2015-03-06 14:10:47 +0000

    Add DH_check_pub_key binding

--- src/cryptography/hazmat/bindings/openssl/dh.py
+++ src/cryptography/hazmat/bindings/openssl/dh.py
@@ -28,6 +28,7 @@ void DH_free(DH *);
 int DH_size(const DH *);
 DH *DH_generate_parameters(int, int, void (*)(int, int, void *), void *);
 int DH_check(const DH *, int *);
+int DH_check_pub_key(const DH *, const BIGNUM *, int *);
 int DH_generate_key(DH *);
 int DH_compute_key(unsigned char *, const BIGNUM *, DH *);
 int DH_set_ex_data(DH *, int, void *);
