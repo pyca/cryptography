c7ddf91c2b41dddf9713a0a01b80d7c850fa3da5
Andre Caron <andre.l.caron@gmail.com>
2015-05-18 23:13:28 -0400

    Fixes PEP8 issues.

--- src/cryptography/hazmat/bindings/openssl/x509v3.py
+++ src/cryptography/hazmat/bindings/openssl/x509v3.py
@@ -184,7 +184,8 @@ ACCESS_DESCRIPTION *sk_ACCESS_DESCRIPTION_value(
     Cryptography_STACK_OF_ACCESS_DESCRIPTION *, int
 );
 void sk_ACCESS_DESCRIPTION_free(Cryptography_STACK_OF_ACCESS_DESCRIPTION *);
-int sk_ACCESS_DESCRIPTION_push(Cryptography_STACK_OF_ACCESS_DESCRIPTION *, ACCESS_DESCRIPTION *);
+int sk_ACCESS_DESCRIPTION_push(Cryptography_STACK_OF_ACCESS_DESCRIPTION *,
+                               ACCESS_DESCRIPTION *);
 
 X509_EXTENSION *X509V3_EXT_conf_nid(Cryptography_LHASH_OF_CONF_VALUE *,
                                     X509V3_CTX *, int, char *);
