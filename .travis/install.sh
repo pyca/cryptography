81f327688811bd911731dd7a629dd132b02ac04d
Paul Kehrer <paul.l.kehrer@gmail.com>
2015-06-06 22:18:22 -0500

    install pypy 2.6.0 in container infra

--- .travis/install.sh
+++ .travis/install.sh
@@ -51,6 +51,15 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv rehash
     python -m pip install --user virtualenv
 else
+    # temporary pyenv installation to get pypy-2.6 before container infra upgrade
+    if [[ "${TOXENV}" == "pypy" ]]; then
+        git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+        PYENV_ROOT="$HOME/.pyenv"
+        PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        pyenv install pypy-2.6.0
+        pyenv global pypy-2.6.0
+    fi
     pip install virtualenv
 fi
 
--- .travis/run.sh
+++ .travis/run.sh
@@ -16,6 +16,13 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
         # CommonCrypto when we test against brew OpenSSL
         export TOX_FLAGS="--backend=openssl"
     fi
+else
+    if [[ "${TOXENV}" == "pypy" ]]; then
+        PYENV_ROOT="$HOME/.pyenv"
+        PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        pyenv global pypy-2.6.0
+    fi
 fi
 source ~/.venv/bin/activate
 tox -- $TOX_FLAGS
