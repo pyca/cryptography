41aabcbd2326ae154a16a1a050ee01fb9a54bd19
Alex Gaynor <alex.gaynor@gmail.com>
2015-09-09 22:16:37 -0400

    Don't use pipes
    Download the whole file first, @glyph points out that truncation attacks are a thing.

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -6,5 +6,6 @@ set -x
 NO_COVERAGE_TOXENVS=(pypy pypy3 pep8 py3pep8 docs)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
-    bash <(curl -s https://codecov.io/bash) -e TRAVIS_OS_NAME,TOXENV,OPENSSL
+    wget https://codecov.io/bash -O codecov.sh
+    bash codecov.sh -e TRAVIS_OS_NAME,TOXENV,OPENSSL
 fi
