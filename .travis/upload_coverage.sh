6879e2d76b4c8caa467fa331577c6dcec71ef2e9
Alex Gaynor <alex.gaynor@gmail.com>
2015-09-10 18:53:28 -0400

    Make some random bash changes to appease shellcheck.
    I don't understand its ways, but it seemed smart.

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 NO_COVERAGE_TOXENVS=(pypy pypy3 pep8 py3pep8 docs)
-if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
+if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ ${TOXENV} ]]; then
     source ~/.venv/bin/activate
     wget https://codecov.io/bash -O codecov.sh
     bash codecov.sh -e TRAVIS_OS_NAME,TOXENV,OPENSSL
