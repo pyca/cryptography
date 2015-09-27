42ea0d1795797c7f52ba714147e2bbc29dad9deb
Terry Chia <terrycwk1994@gmail.com>
2015-09-27 20:26:09 +0800

    Pin version of hypothesis above 1.11.4.

--- tests/test_with_hypothesis.py
+++ tests/test_with_hypothesis.py
@@ -1,11 +1,14 @@
+from hypothesis import binary
+
 import pytest
 
 from cryptography.fernet import Fernet
 
+
 hypothesis = pytest.importorskip("hypothesis")
 
 
-@hypothesis.given(bytes)
+@hypothesis.given(binary())
 def test_fernet(data):
     f = Fernet(Fernet.generate_key())
     ct = f.encrypt(data)
