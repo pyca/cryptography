9604d9e9839703636843595b52a4f01f3de8e2fe
Dominic Chen <d.c.ddcc@gmail.com>
2015-10-10 05:13:31 +0000

    add shebang for setup.py scripts

--- vectors/setup.py
+++ vectors/setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # This file is dual licensed under the terms of the Apache License, Version
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
