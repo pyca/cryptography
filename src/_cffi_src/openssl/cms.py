5653bde3e5b57e52d9be2cd284ee4855be55c50d
Paul Kehrer <paul.l.kehrer@gmail.com>
2015-11-12 16:20:23 -0600

    reorganize and rename

--- src/_cffi_src/openssl/cms.py
+++ src/_cffi_src/openssl/cms.py
@@ -17,7 +17,7 @@ INCLUDES = """
 
 TYPES = """
 static const long Cryptography_HAS_CMS;
-static const long Cryptography_HAS_MORE_CMS;
+static const long Cryptography_HAS_CMS_BIO_FUNCTIONS;
 
 typedef ... CMS_ContentInfo;
 typedef ... CMS_SignerInfo;
@@ -72,6 +72,15 @@ CMS_SignerInfo *CMS_add1_signer(CMS_ContentInfo *, X509 *, EVP_PKEY *,
 CUSTOMIZATIONS = """
 #if !defined(OPENSSL_NO_CMS) && OPENSSL_VERSION_NUMBER >= 0x0090808fL
 static const long Cryptography_HAS_CMS = 1;
+#if OPENSSL_VERSION_NUMBER < 0x1000000fL
+static const long Cryptography_HAS_CMS_BIO_FUNCTIONS = 0;
+/* These functions were added in 1.0.0 */
+BIO *(*BIO_new_CMS)(BIO *, CMS_ContentInfo *) = NULL;
+int (*i2d_CMS_bio_stream)(BIO *, CMS_ContentInfo *, BIO *, int) = NULL;
+int (*PEM_write_bio_CMS_stream)(BIO *, CMS_ContentInfo *, BIO *, int) = NULL;
+#else
+static const long Cryptography_HAS_CMS_BIO_FUNCTIONS = 1;
+#endif
 #else
 static const long Cryptography_HAS_CMS = 0;
 typedef void CMS_ContentInfo;
@@ -115,14 +124,5 @@ int (*CMS_decrypt)(CMS_ContentInfo *, EVP_PKEY *, X509 *, BIO *, BIO *,
                    unsigned int) = NULL;
 CMS_SignerInfo *(*CMS_add1_signer)(CMS_ContentInfo *, X509 *, EVP_PKEY *,
                                    const EVP_MD *, unsigned int) = NULL;
-#endif
-#if !defined(OPENSSL_NO_CMS) && OPENSSL_VERSION_NUMBER < 0x1000000fL
-static const long Cryptography_HAS_MORE_CMS = 0;
-/* These functions were added in 1.0.0 */
-BIO *(*BIO_new_CMS)(BIO *, CMS_ContentInfo *) = NULL;
-int (*i2d_CMS_bio_stream)(BIO *, CMS_ContentInfo *, BIO *, int) = NULL;
-int (*PEM_write_bio_CMS_stream)(BIO *, CMS_ContentInfo *, BIO *, int) = NULL;
-#else
-static const long Cryptography_HAS_MORE_CMS = 1;
 #endif
 """
--- src/cryptography/hazmat/bindings/openssl/_conditional.py
+++ src/cryptography/hazmat/bindings/openssl/_conditional.py
@@ -52,7 +52,7 @@ CONDITIONAL_NAMES = {
         "CMS_USE_KEYID",
         "CMS_DEBUG_DECRYPT",
     ],
-    "Cryptography_HAS_MORE_CMS": [
+    "Cryptography_HAS_CMS_BIO_FUNCTIONS": [
         "BIO_new_CMS",
         "i2d_CMS_bio_stream",
         "PEM_write_bio_CMS_stream",
