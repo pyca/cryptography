d06763d9258115b2e1199296a18d7ec773d47ac3
Nick Bastin <nick.bastin@gmail.com>
2015-12-12 18:32:59 -0800

    Allow any OID for access_method, validate OIDs at creation time, fix tests.

--- tests/test_x509_ext.py
+++ tests/test_x509_ext.py
@@ -1861,7 +1861,8 @@ class TestExtendedKeyUsageExtension(object):
 
 class TestAccessDescription(object):
     def test_invalid_access_method(self):
-        with pytest.raises(ValueError):
+        # access_method can be *any* valid OID
+        with pytest.raises(TypeError):
             x509.AccessDescription("notanoid", x509.DNSName(u"test"))
 
     def test_invalid_access_location(self):
