8e7dfdc39fe430a87c7b72f99f41b14f7e369864
Paul Kehrer <paul.l.kehrer@gmail.com>
2015-12-31 22:47:29 -0600

    these are also deprecated in 1.1.0-pre2 and unused

--- src/_cffi_src/openssl/crypto.py
+++ src/_cffi_src/openssl/crypto.py
@@ -44,8 +44,6 @@ void CRYPTO_mem_leaks(struct bio_st *);
 void CRYPTO_cleanup_all_ex_data(void);
 int CRYPTO_num_locks(void);
 void CRYPTO_set_locking_callback(void(*)(int, int, const char *, int));
-void CRYPTO_set_id_callback(unsigned long (*)(void));
-unsigned long (*CRYPTO_get_id_callback(void))(void);
 void (*CRYPTO_get_locking_callback(void))(int, int, const char *, int);
 void CRYPTO_lock(int, int, const char *, int);
 
