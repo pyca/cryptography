9d361291369784d951a7051f6ba6d0a2680156b9
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-01 13:27:03 -0500

    Bump the copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Cryptography'
-copyright = '2013-2015, Individual Contributors'
+copyright = '2013-2016, Individual Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
