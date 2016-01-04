1dafc3f9b6e7f2743d1acad876a960c9d6885a80
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-01-03 21:09:22 -0600

    add a pypy-nocoverage target and use that on OS X

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-NO_COVERAGE_TOXENVS=(pep8 py3pep8 docs)
+NO_COVERAGE_TOXENVS=(pypy-nocoverage pep8 py3pep8 docs)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
     codecov --env TRAVIS_OS_NAME,TOXENV,OPENSSL
