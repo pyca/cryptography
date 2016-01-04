6539c8fa1df0f78f4fa489ba18a3d18cd814ecc0
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-01-03 21:58:26 -0600

    install the right pypy for pypy-nocov as well

--- .travis/install.sh
+++ .travis/install.sh
@@ -53,7 +53,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 else
     # temporary pyenv installation to get latest pypy before container infra upgrade
     # now using the -latest because of a segfault bug we're encountering in 2.6.1
-    if [[ "${TOXENV}" == "pypy" ]]; then
+    if [[ "${TOXENV}" = pypy* ]]; then
         git clone https://github.com/yyuu/pyenv.git ~/.pyenv
         PYENV_ROOT="$HOME/.pyenv"
         PATH="$PYENV_ROOT/bin:$PATH"
