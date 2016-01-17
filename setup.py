e74dc4ed8608d119881273b887b8c022128846d7
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-17 11:46:25 -0500

    add this back, we couldn't dedupe there

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ else:
     requirements.append("cffi>=1.4.1")
     setup_requirements.append("cffi>=1.4.1")
 
+# If you add a new dep here you probably need to add it in the tox.ini as well
 test_requirements = [
     "pytest",
     "pretend",
