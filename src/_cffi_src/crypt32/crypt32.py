ee99d8dfc24c7a5695832ad874e84a58149e5121
Cory Benfield <lukasaoz@gmail.com>
2016-02-24 12:16:31 +0000

    Remove potentially optional struct fields.

--- src/_cffi_src/crypt32/crypt32.py
+++ src/_cffi_src/crypt32/crypt32.py
@@ -42,13 +42,7 @@ typedef struct _CERT_USAGE_MATCH {
 typedef struct _CERT_CHAIN_PARA {
   DWORD                   cbSize;
   CERT_USAGE_MATCH        RequestedUsage;
-  CERT_USAGE_MATCH        RequestedIssuancePolicy;
-  DWORD                   dwUrlRetrievalTimeout;
-  BOOL                    fCheckRevocationFreshnessTime;
-  DWORD                   dwRevocationFreshnessTime;
-  LPFILETIME              pftCacheResync;
-  PCCERT_STRONG_SIGN_PARA pStrongSignPara;
-  DWORD                   dwStrongSignFlags;
+  ...;
 } CERT_CHAIN_PARA, *PCERT_CHAIN_PARA;
 
 typedef struct _HTTPSPolicyCallbackData {
