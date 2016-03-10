b4e47f87c0d7240f072af26e8ccb40657110be5d
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-03-09 21:03:46 -0400

    opaque X509_EXTENSION

--- src/_cffi_src/openssl/x509.py
+++ src/_cffi_src/openssl/x509.py
@@ -36,11 +36,7 @@ typedef struct {
     ...;
 } X509_CINF;
 
-typedef struct {
-    ASN1_OBJECT *object;
-    ASN1_BOOLEAN critical;
-    ASN1_OCTET_STRING *value;
-} X509_EXTENSION;
+typedef ... X509_EXTENSION;
 
 typedef ... X509_EXTENSIONS;
 typedef ... X509_REQ_INFO;
