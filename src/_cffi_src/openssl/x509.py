8de767ed8b4197483b7a2abcef1e9746545e9fd1
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-03-10 21:10:13 -0400

    deopaque X509_EXTENSION so we can keep moving on 1.1.0 support

--- src/_cffi_src/openssl/x509.py
+++ src/_cffi_src/openssl/x509.py
@@ -36,7 +36,13 @@ typedef struct {
     ...;
 } X509_CINF;
 
-typedef ... X509_EXTENSION;
+/* TODO: opaque X509_EXTENSION. Cryptography no longer depends on it being
+   non-opaque but pyOpenSSL needs a release where it doesn't depend on this */
+typedef struct {
+    ASN1_OBJECT *object;
+    ASN1_BOOLEAN critical;
+    ASN1_OCTET_STRING *value;
+} X509_EXTENSION;
 
 typedef ... X509_EXTENSIONS;
 typedef ... X509_REQ_INFO;
