8148281d08b6ca0c92bd704895d07700736bba55
Todd Knarr <tknarr@silverglass.org>
2016-05-28 18:18:32 -0700

    Merge ext_fernet classes into fernet.py.

--- src/cryptography/fernet.py
+++ src/cryptography/fernet.py
@@ -175,3 +175,53 @@ class Fernet(FernetBase):
     @classmethod
     def generate_key(cls):
         return FernetBase.generate_key(128)
+
+
+class ExtFernet192(FernetBase):
+    """
+    Extended version of Fernet using AES192 encryption.
+
+    The version byte differs from standard Fernet to distinguish this format.
+    The low 5 bits indicate the version of standard Fernet the extended version
+    is based on, with 0x1 indicating Fernet version 0x80. The high 3 bits indicate
+    the encryption key length, with binary 001 (0x1) indicating 192-bit AES.
+    This yields a version byte of 0x21 (binary 00100001).
+    """
+
+    def __init__(self, key, backend=None):
+        key = base64.urlsafe_b64decode(key)
+        if len(key) != 48:
+            raise ValueError(
+                "Fernet192 key must be 48 url-safe base64-encoded bytes."
+            )
+        super(self.__class__, self).__init__(key, backend)
+        self._version = b"\x21"
+
+    @classmethod
+    def generate_key(cls):
+        return FernetBase.generate_key(192)
+
+
+class ExtFernet256(FernetBase):
+    """
+    Extended version of Fernet using AES256 encryption.
+
+    The version byte differs from standard Fernet to distinguish this format.
+    The low 5 bits indicate the version of standard Fernet the extended version
+    is based on, with 0x1 indicating Fernet version 0x80. The high 3 bits indicate
+    the encryption key length, with binary 010 (0x2) indicating 256-bit AES.
+    This yields a version byte of 0x41 (binary 01000001).
+    """
+
+    def __init__(self, key, backend=None):
+        key = base64.urlsafe_b64decode(key)
+        if len(key) != 64:
+            raise ValueError(
+                "Fernet256 key must be 64 url-safe base64-encoded bytes."
+            )
+        super(self.__class__, self).__init__(key, backend)
+        self._version = b"\x41"
+
+    @classmethod
+    def generate_key(cls):
+        return FernetBase.generate_key(256)
