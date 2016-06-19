741b2316b0bfaa107c009659dd4731602471f442
Alex Gaynor <alex.gaynor@gmail.com>
2016-06-19 01:22:07 -0400

    Removed 0.9.8 specific hmac code (#2995)

--- src/_cffi_src/openssl/hmac.py
+++ src/_cffi_src/openssl/hmac.py
@@ -13,11 +13,11 @@ typedef ... HMAC_CTX;
 """
 
 FUNCTIONS = """
-int Cryptography_HMAC_Init_ex(HMAC_CTX *, const void *, int, const EVP_MD *,
-                              ENGINE *);
-int Cryptography_HMAC_Update(HMAC_CTX *, const unsigned char *, size_t);
-int Cryptography_HMAC_Final(HMAC_CTX *, unsigned char *, unsigned int *);
-int Cryptography_HMAC_CTX_copy(HMAC_CTX *, HMAC_CTX *);
+int HMAC_Init_ex(HMAC_CTX *, const void *, int, const EVP_MD *, ENGINE *);
+int HMAC_Update(HMAC_CTX *, const unsigned char *, size_t);
+int HMAC_Final(HMAC_CTX *, unsigned char *, unsigned int *);
+int HMAC_CTX_copy(HMAC_CTX *, HMAC_CTX *);
+
 HMAC_CTX *Cryptography_HMAC_CTX_new(void);
 void Cryptography_HMAC_CTX_free(HMAC_CTX *ctx);
 """
@@ -26,60 +26,6 @@ MACROS = """
 """
 
 CUSTOMIZATIONS = """
-int Cryptography_HMAC_Init_ex(HMAC_CTX *ctx, const void *key, int key_len,
-                              const EVP_MD *md, ENGINE *impl) {
-#if OPENSSL_VERSION_NUMBER >= 0x010000000
-    return HMAC_Init_ex(ctx, key, key_len, md, impl);
-#else
-    HMAC_Init_ex(ctx, key, key_len, md, impl);
-    return 1;
-#endif
-}
-
-int Cryptography_HMAC_Update(HMAC_CTX *ctx, const unsigned char *data,
-                             size_t data_len) {
-#if OPENSSL_VERSION_NUMBER >= 0x010000000
-    return HMAC_Update(ctx, data, data_len);
-#else
-    HMAC_Update(ctx, data, data_len);
-    return 1;
-#endif
-}
-
-int Cryptography_HMAC_Final(HMAC_CTX *ctx, unsigned char *digest,
-    unsigned int *outlen) {
-#if OPENSSL_VERSION_NUMBER >= 0x010000000
-    return HMAC_Final(ctx, digest, outlen);
-#else
-    HMAC_Final(ctx, digest, outlen);
-    return 1;
-#endif
-}
-
-int Cryptography_HMAC_CTX_copy(HMAC_CTX *dst_ctx, HMAC_CTX *src_ctx) {
-#if OPENSSL_VERSION_NUMBER >= 0x010000000
-    return HMAC_CTX_copy(dst_ctx, src_ctx);
-#else
-    HMAC_CTX_init(dst_ctx);
-    if (!EVP_MD_CTX_copy_ex(&dst_ctx->i_ctx, &src_ctx->i_ctx)) {
-        goto err;
-    }
-    if (!EVP_MD_CTX_copy_ex(&dst_ctx->o_ctx, &src_ctx->o_ctx)) {
-        goto err;
-    }
-    if (!EVP_MD_CTX_copy_ex(&dst_ctx->md_ctx, &src_ctx->md_ctx)) {
-        goto err;
-    }
-    memcpy(dst_ctx->key, src_ctx->key, HMAC_MAX_MD_CBLOCK);
-    dst_ctx->key_length = src_ctx->key_length;
-    dst_ctx->md = src_ctx->md;
-    return 1;
-
-    err:
-        return 0;
-#endif
-}
-
 HMAC_CTX *Cryptography_HMAC_CTX_new(void) {
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
     return HMAC_CTX_new();
@@ -90,7 +36,6 @@ HMAC_CTX *Cryptography_HMAC_CTX_new(void) {
     return ctx;
 #endif
 }
-
 
 
 void Cryptography_HMAC_CTX_free(HMAC_CTX *ctx) {
