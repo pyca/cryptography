906bff72e71684e72548c2f7602946d208f235d7
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-06-20 19:49:35 -0500

    use DSAparams_dup now that we no longer support 0.9.8

--- src/cryptography/hazmat/backends/openssl/dsa.py
+++ src/cryptography/hazmat/backends/openssl/dsa.py
@@ -162,23 +162,11 @@ class _DSAPrivateKey(object):
         )
 
     def public_key(self):
-        dsa_cdata = self._backend._lib.DSA_new()
+        dsa_cdata = self._backend._lib.DSAparams_dup(self._dsa_cdata)
         self._backend.openssl_assert(dsa_cdata != self._backend._ffi.NULL)
         dsa_cdata = self._backend._ffi.gc(
             dsa_cdata, self._backend._lib.DSA_free
         )
-        p = self._backend._ffi.new("BIGNUM **")
-        q = self._backend._ffi.new("BIGNUM **")
-        g = self._backend._ffi.new("BIGNUM **")
-        self._backend._lib.DSA_get0_pqg(self._dsa_cdata, p, q, g)
-        self._backend.openssl_assert(p[0] != self._backend._ffi.NULL)
-        self._backend.openssl_assert(q[0] != self._backend._ffi.NULL)
-        self._backend.openssl_assert(g[0] != self._backend._ffi.NULL)
-        p_dup = self._backend._lib.BN_dup(p[0])
-        q_dup = self._backend._lib.BN_dup(q[0])
-        g_dup = self._backend._lib.BN_dup(g[0])
-        res = self._backend._lib.DSA_set0_pqg(dsa_cdata, p_dup, q_dup, g_dup)
-        self._backend.openssl_assert(res == 1)
         pub_key = self._backend._ffi.new("BIGNUM **")
         self._backend._lib.DSA_get0_key(
             self._dsa_cdata, pub_key, self._backend._ffi.NULL
