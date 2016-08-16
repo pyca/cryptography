53da67090e7063325e5407fa08ad806c8c1feb8b
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-08-16 19:17:49 +0800

    move functions that were const-ified in 1.1.0-pre6 (#3090)

--- src/_cffi_src/openssl/x509name.py
+++ src/_cffi_src/openssl/x509name.py
@@ -25,7 +25,6 @@ FUNCTIONS = """
 X509_NAME *X509_NAME_new(void);
 void X509_NAME_free(X509_NAME *);
 
-int X509_NAME_entry_count(X509_NAME *);
 X509_NAME_ENTRY *X509_NAME_get_entry(X509_NAME *, int);
 ASN1_OBJECT *X509_NAME_ENTRY_get_object(X509_NAME_ENTRY *);
 ASN1_STRING *X509_NAME_ENTRY_get_data(X509_NAME_ENTRY *);
@@ -34,8 +33,6 @@ unsigned long X509_NAME_hash(X509_NAME *);
 int i2d_X509_NAME(X509_NAME *, unsigned char **);
 int X509_NAME_add_entry_by_txt(X509_NAME *, const char *, int,
                                const unsigned char *, int, int, int);
-int X509_NAME_add_entry_by_OBJ(X509_NAME *, ASN1_OBJECT *, int,
-                               unsigned char *, int, int, int);
 int X509_NAME_add_entry_by_NID(X509_NAME *, int, int, unsigned char *,
                                int, int, int);
 X509_NAME_ENTRY *X509_NAME_delete_entry(X509_NAME *, int);
@@ -44,13 +41,20 @@ int X509_NAME_get_index_by_NID(X509_NAME *, int, int);
 int X509_NAME_cmp(const X509_NAME *, const X509_NAME *);
 char *X509_NAME_oneline(X509_NAME *, char *, int);
 X509_NAME *X509_NAME_dup(X509_NAME *);
-X509_NAME_ENTRY *X509_NAME_ENTRY_create_by_OBJ(X509_NAME_ENTRY **,
-                                               ASN1_OBJECT *, int,
-                                               const unsigned char *, int);
 int X509_NAME_add_entry(X509_NAME *, X509_NAME_ENTRY *, int, int);
 """
 
 MACROS = """
+/* This became const X509_NAME * in 1.1.0 */
+int X509_NAME_entry_count(X509_NAME *);
+
+/* These became const ASN1_OBJECT * in 1.1.0 */
+X509_NAME_ENTRY *X509_NAME_ENTRY_create_by_OBJ(X509_NAME_ENTRY **,
+                                               ASN1_OBJECT *, int,
+                                               const unsigned char *, int);
+int X509_NAME_add_entry_by_OBJ(X509_NAME *, ASN1_OBJECT *, int,
+                               unsigned char *, int, int, int);
+
 Cryptography_STACK_OF_X509_NAME *sk_X509_NAME_new_null(void);
 int sk_X509_NAME_num(Cryptography_STACK_OF_X509_NAME *);
 int sk_X509_NAME_push(Cryptography_STACK_OF_X509_NAME *, X509_NAME *);
