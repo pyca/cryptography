99c228cdfd9998a15adb1592661adb92f630b420
Alex Gaynor <alex.gaynor@gmail.com>
2016-09-04 16:43:30 -0400

    make these conditional

--- src/cryptography/hazmat/bindings/openssl/_conditional.py
+++ src/cryptography/hazmat/bindings/openssl/_conditional.py
@@ -315,5 +315,9 @@ CONDITIONAL_NAMES = {
     ],
     "Cryptography_HAS_SCRYPT": [
         "EVP_PBE_scrypt",
-    ]
+    ],
+    "Cryptography_HAS_MEM_FUNCTIONS": [
+        "CRYPTO_get_mem_functions",
+        "CRYPTO_set_mem_functions",
+    ],
 }
