59ebf0de4e5ebcd1c56aadf34c7dec5f813436f3
Alex Gaynor <alex.gaynor@gmail.com>
2016-09-04 16:48:24 -0400

    do the conditional correctly

--- src/_cffi_src/openssl/crypto.py
+++ src/_cffi_src/openssl/crypto.py
@@ -35,12 +35,14 @@ static const int CRYPTO_LOCK_SSL;
 FUNCTIONS = """
 int CRYPTO_mem_ctrl(int);
 
-void CRYPTO_get_mem_functions(void *(**)(size_t, const char *, int),
-                              void *(**)(void *, size_t, const char *, int),
-                              void (**)(void *, const char *, int));
-int CRYPTO_set_mem_functions(void *(*)(size_t, const char *, int),
-                             void *(*)(void *, size_t, const char *, int),
-                             void (*)(void *, const char *, int));
+void Cryptography_CRYPTO_get_mem_functions(
+    void *(**)(size_t, const char *, int),
+    void *(**)(void *, size_t, const char *, int),
+    void (**)(void *, const char *, int));
+int Cryptography_CRYPTO_set_mem_functions(
+    void *(*)(size_t, const char *, int),
+    void *(*)(void *, size_t, const char *, int),
+    void (*)(void *, const char *, int));
 """
 
 MACROS = """
