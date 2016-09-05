089f7cf4cdfc8d35c9598cb13bb48d304f70771e
Alex Gaynor <alex.gaynor@gmail.com>
2016-09-04 20:44:22 -0400

    first cut at memleak tests. doesn't work

---
+++ tests/hazmat/backends/test_openssl_memleak.py
@@ -0,0 +1,104 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import subprocess
+import sys
+import textwrap
+
+import pytest
+
+
+MEMORY_LEAK_SCRIPT = """
+def main():
+    from cryptography.hazmat.bindings.openssl.binding import Binding
+
+    b = Binding()
+    heap = {}
+
+    orig_malloc = b.ffi.new("void *(**)(size_t, const char *, int)")
+    orig_realloc = b.ffi.new("void *(**)(void *, size_t, const char *, int)")
+    orig_free = b.ffi.new("void(**)(void *, const char *, int)")
+    b.lib.Cryptography_CRYPTO_get_mem_functions(
+        orig_malloc, orig_realloc, orig_free
+    )
+
+    @b.ffi.callback("void *(size_t, const char *, int)")
+    def malloc(size, path, line):
+        ptr = orig_malloc[0](size, path, line)
+        heap[ptr] = (size, path, line)
+        return ptr
+
+    @b.ffi.callback("void *(void *, size_t, const char *, int)")
+    def realloc(ptr, size, path, line):
+        # TODO: this may need to be `heap.pop(ptr)`
+        del heap[ptr]
+        new_ptr = orig_realloc[0](ptr, size, path, line)
+        heap[new_ptr] = (size, path, line)
+        return new_ptr
+
+    @b.ffi.callback("void(void *, const char *, int)")
+    def free(ptr, path, line):
+        del heap[ptr]
+        orig_free[0](ptr, path, line)
+
+    result = b.lib.Cryptography_CRYPTO_set_mem_functions(malloc, realloc, free)
+    assert result == 1, "x1"
+    try:
+        func()
+    finally:
+        result = b.lib.Cryptography_CRYPTO_set_mem_functions(
+            orig_malloc, orig_realloc, orig_free
+        )
+        assert result == 1, "x2"
+
+    assert not heap, "x3"
+
+main()
+"""
+
+def assert_no_memory_leaks(s):
+    env = os.environ.copy()
+    env["PYTHONPATH"] = os.pathsep.join(sys.path)
+    subprocess.check_call(
+        [sys.executable, "-c", "{}\n\n{}".format(s, MEMORY_LEAK_SCRIPT)],
+        env=env,
+    )
+
+
+class TestAssertNoMemoryLeaks(object):
+    def test_no_leak_no_malloc(self):
+        assert_no_memory_leaks(textwrap.dedent("""
+        def func():
+            pass
+        """))
+
+    def test_no_leak_free(self):
+        assert_no_memory_leaks(textwrap.dedent("""
+        def func():
+            from cryptography.hazmat.bindings.openssl.binding import Binding
+            b = Binding()
+            name = b.lib.X509_NAME_new()
+            b.lib.X509_NAME_free(name)
+        """))
+
+    def test_no_leak_gc(self):
+        assert_no_memory_leaks(textwrap.dedent("""
+        def func():
+            from cryptography.hazmat.bindings.openssl.binding import Binding
+            b = Binding()
+            name = b.lib.X509_NAME_new()
+            b.ffi.gc(name, b.lib.X509_NAME_free)
+        """))
+
+    def test_leak(self):
+        with pytest.raises(AssertionError):
+            assert_no_memory_leaks(textwrap.dedent("""
+            def func():
+                from cryptography.hazmat.bindings.openssl.binding import Binding
+                b = Binding()
+                b.lib.X509_NAME_new()
+            """))
