bfe0b92dc5136d9909e74ea99639a396b3d75445
palaviv <palaviv@gmail.com>
2016-09-07 19:19:04 +0300

    Removed duplicate line

--- src/_cffi_src/openssl/err.py
+++ src/_cffi_src/openssl/err.py
@@ -20,7 +20,6 @@ typedef struct ERR_string_data_st ERR_STRING_DATA;
 
 static const int ERR_LIB_DH;
 static const int ERR_LIB_EVP;
-static const int ERR_LIB_EVP;
 static const int ERR_LIB_EC;
 static const int ERR_LIB_PEM;
 static const int ERR_LIB_ASN1;
