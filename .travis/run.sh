798c3823fd0b898e19c0467b3b4e78acd5fb8bc5
Alex Gaynor <alex.gaynor@gmail.com>
2016-09-26 01:35:31 -0400

    more simplify

--- .travis/run.sh
+++ .travis/run.sh
@@ -28,9 +28,7 @@ else
     fi
     if [ -n "${OPENSSL}" ]; then
         OPENSSL_DIR="ossl/${OPENSSL}"
-    fi
 
-    if [ -n "$OPENSSL_DIR" ]; then
         export PATH="$HOME/$OPENSSL_DIR/bin:$PATH"
         export CFLAGS="-I$HOME/$OPENSSL_DIR/include"
         # rpath on linux will cause it to use an absolute path so we don't need to do LD_LIBRARY_PATH
