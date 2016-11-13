d90dc939fd818b03ecf61b3cff72aedbf7658bab
Alex Gaynor <alex.gaynor@gmail.com>
2016-11-13 11:27:34 -0500

    utf8

--- src/_cffi_src/openssl/callbacks.py
+++ src/_cffi_src/openssl/callbacks.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 # This file is dual licensed under the terms of the Apache License, Version
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
