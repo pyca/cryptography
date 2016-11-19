e09754c74d75c1ba59c989cb053aaf61b519188d
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-11-19 11:30:25 +0800

    add SSL_CTX_set_ecdh_auto where supported (#3250)
    In 1.1.0 this is a noop and occurs by default, and this wasn't supported
    < 1.0.2

--- src/_cffi_src/openssl/ecdh.py
+++ src/_cffi_src/openssl/ecdh.py
@@ -12,6 +12,7 @@ INCLUDES = """
 
 TYPES = """
 static const int Cryptography_HAS_ECDH;
+static const int Cryptography_HAS_SET_ECDH_AUTO;
 """
 
 FUNCTIONS = """
@@ -20,6 +21,7 @@ FUNCTIONS = """
 MACROS = """
 int ECDH_compute_key(void *, size_t, const EC_POINT *, EC_KEY *,
                      void *(*)(const void *, size_t, void *, size_t *));
+int SSL_CTX_set_ecdh_auto(SSL_CTX *, int);
 """
 
 CUSTOMIZATIONS = """
@@ -32,5 +34,12 @@ int (*ECDH_compute_key)(void *, size_t, const EC_POINT *, EC_KEY *,
 
 #else
 static const long Cryptography_HAS_ECDH = 1;
+#endif
+
+#ifndef SSL_CTX_set_ecdh_auto
+static const long Cryptography_HAS_SET_ECDH_AUTO = 0;
+int (*SSL_CTX_set_ecdh_auto)(SSL_CTX *, int) = NULL;
+#else
+static const long Cryptography_HAS_SET_ECDH_AUTO = 1;
 #endif
 """
