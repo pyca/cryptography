a0646a0d1bba434c34b1ab26cb3c9d05a01011f3
Richard Plangger <planrichi@gmail.com>
2016-11-21 10:55:24 +0100

    missing macro and three macro functions

--- src/_cffi_src/openssl/ssl.py
+++ src/_cffi_src/openssl/ssl.py
@@ -26,6 +26,7 @@ static const long Cryptography_HAS_GET_SERVER_TMP_KEY;
 static const long Cryptography_HAS_SSL_CTX_SET_CLIENT_CERT_ENGINE;
 static const long Cryptography_HAS_SSL_CTX_CLEAR_OPTIONS;
 static const long Cryptography_HAS_NPN_NEGOTIATED;
+static const long Cryptography_OPENSSL_NO_TLSEXT;
 
 /* Internally invented symbol to tell us if SNI is supported */
 static const long Cryptography_HAS_TLSEXT_HOSTNAME;
@@ -687,6 +688,12 @@ long (*SSL_get_server_tmp_key)(SSL *, EVP_PKEY **) = NULL;
 static const long Cryptography_HAS_SSL_CTX_SET_CLIENT_CERT_ENGINE = 1;
 
 static const long Cryptography_HAS_SSL_CTX_CLEAR_OPTIONS = 1;
+
+#ifdef OPENSSL_NO_TLSEXT
+static const long Cryptography_OPENSSL_NO_TLSEXT = 1;
+#else
+static const long Cryptography_OPENSSL_NO_TLSEXT = 0;
+#endif
 
 /* in OpenSSL 1.1.0 the SSL_ST values were renamed to TLS_ST and several were
    removed */
