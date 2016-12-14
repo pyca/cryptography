a4f603e99448ea4f78769fdf7123674e8b46cead
Alex Gaynor <alex.gaynor@gmail.com>
2016-12-13 19:13:38 -0500

    try removing this

--- src/cryptography/hazmat/backends/openssl/utils.py
+++ src/cryptography/hazmat/backends/openssl/utils.py
@@ -4,29 +4,8 @@
 
 from __future__ import absolute_import, division, print_function
 
-import six
-
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.utils import Prehashed
-
-
-def _truncate_digest(digest, order_bits):
-    digest_len = len(digest)
-
-    if 8 * digest_len > order_bits:
-        digest_len = (order_bits + 7) // 8
-        digest = digest[:digest_len]
-
-    if 8 * digest_len > order_bits:
-        rshift = 8 - (order_bits & 0x7)
-        assert 0 < rshift < 8
-
-        mask = 0xFF >> rshift << rshift
-
-        # Set the bottom rshift bits to 0
-        digest = digest[:-1] + six.int2byte(six.indexbytes(digest, -1) & mask)
-
-    return digest
 
 
 def _calculate_digest_and_algorithm(backend, data, algorithm):
