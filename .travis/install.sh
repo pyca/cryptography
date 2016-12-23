31b5d784e6542a6bcac9af9b39d3feacabdba137
Alex Gaynor <alex.gaynor@gmail.com>
2016-12-23 12:20:36 -0500

    Fixed #3334 -- added Python 3.6 support (#3335)
    * Fixed #3334 -- added Python 3.6 support
    
    * install py36
    
    * empty commit to retrigger travis
    
    * this is an impressively dumb typo

--- .travis/install.sh
+++ .travis/install.sh
@@ -32,6 +32,10 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
             pyenv install 3.5.2
             pyenv global 3.5.2
             ;;
+        py36)
+            pyenv install 3.6.0
+            pyenv global 3.6.0
+            ;;
         pypy*)
             pyenv install "pypy-$PYPY_VERSION"
             pyenv global "pypy-$PYPY_VERSION"
--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -6,6 +6,8 @@ Changelog
 
 .. note:: This version is not yet released and is under active development.
 
+* Added support for Python 3.6.
+
 1.7.1 - 2016-12-13
 ~~~~~~~~~~~~~~~~~~
 
