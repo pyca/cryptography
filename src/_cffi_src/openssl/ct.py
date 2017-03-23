00e87816e6f097fca83cb5fc891cabdca44c151b
Alex Gaynor <alex.gaynor@gmail.com>
2017-03-23 10:09:18 -0400

    Two additional bindings for CT

--- src/_cffi_src/openssl/ct.py
+++ src/_cffi_src/openssl/ct.py
@@ -41,6 +41,9 @@ ct_log_entry_type_t SCT_get_log_entry_type(const SCT *);
 size_t SCT_get0_log_id(const SCT *, unsigned char **);
 
 uint64_t SCT_get_timestamp(const SCT *);
+
+int sk_SCT_num(const Cryptography_STACK_OF_SCT *);
+SCT *sk_SCT_value(const Cryptography_STACK_OF_SCT *, int);
 """
 
 CUSTOMIZATIONS = """
@@ -64,5 +67,8 @@ sct_version_t (*SCT_get_version)(const SCT *) = NULL;
 ct_log_entry_type_t (*SCT_get_log_entry_type)(const SCT *) = NULL;
 size_t (*SCT_get0_log_id)(const SCT *, unsigned char **) = NULL;
 uint64_t (*SCT_get_timestamp)(const SCT *) = NULL;
+
+int (*sk_SCT_num)(const Cryptography_STACK_OF_SCT *) = NULL;
+SCT *(*sk_SCT_value)(const Cryptography_STACK_OF_SCT *, int) = NULL;
 #endif
 """
