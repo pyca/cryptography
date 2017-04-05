08c07d46c0155ac0d5296451da9ae2631983417e
Moriyoshi Koizumi <mozo@mozo.jp>
2017-04-05 21:42:47 +0900

    Add ASN1_TIME related functions for the relevant PyOpenSSL patch.

--- src/_cffi_src/openssl/asn1.py
+++ src/_cffi_src/openssl/asn1.py
@@ -80,6 +80,8 @@ void ASN1_TIME_free(ASN1_TIME *);
 ASN1_GENERALIZEDTIME *ASN1_TIME_to_generalizedtime(ASN1_TIME *,
                                                    ASN1_GENERALIZEDTIME **);
 ASN1_TIME *ASN1_TIME_set(ASN1_TIME *, time_t);
+int ASN1_TIME_set_string(ASN1_TIME *, const char *);
+int ASN1_TIME_check(ASN1_TIME *);
 
 /*  ASN1 UTCTIME */
 ASN1_UTCTIME *ASN1_UTCTIME_new(void);
