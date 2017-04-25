1dd6c7dbc9f8872127ea1527058ad4bd4bfaf2b9
Moriyoshi Koizumi <mozo@mozo.jp>
2017-04-25 05:20:42 +0000

    More accurate LibreSSL detection

--- src/_cffi_src/openssl/x509_vfy.py
+++ src/_cffi_src/openssl/x509_vfy.py
@@ -226,9 +226,11 @@ static const long X509_V_ERR_SUITE_B_INVALID_CURVE = 0;
 static const long X509_V_ERR_SUITE_B_INVALID_SIGNATURE_ALGORITHM = 0;
 static const long X509_V_ERR_SUITE_B_LOS_NOT_ALLOWED = 0;
 static const long X509_V_ERR_SUITE_B_CANNOT_SIGN_P_384_WITH_P_256 = 0;
+#if LIBRESSL_VERSION_NUMBER < 0x20500004L
 static const long X509_V_ERR_HOSTNAME_MISMATCH = 0;
 static const long X509_V_ERR_EMAIL_MISMATCH = 0;
 static const long X509_V_ERR_IP_ADDRESS_MISMATCH = 0;
+#endif
 
 /* X509_V_FLAG_TRUSTED_FIRST is also new in 1.0.2+, but it is added separately
    below because it shows up in some earlier 3rd party OpenSSL packages. */
