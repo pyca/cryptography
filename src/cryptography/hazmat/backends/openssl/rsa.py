f624f3a1f6eebecf147d5a3642e2fa092fb59274
hackers-terabit <terabit@me.com>
2017-05-11 04:53:43 +0000

    ../parrot/rsatestcase.py

--- src/cryptography/hazmat/backends/openssl/rsa.py
+++ src/cryptography/hazmat/backends/openssl/rsa.py
@@ -300,7 +300,8 @@ def _rsa_sig_verify(backend, padding, padding_enum, algorithm, public_key,
         errors = backend._consume_errors()
         assert errors
         raise InvalidSignature
-
+    else:
+        return res
 
 @utils.register_interface(AsymmetricSignatureContext)
 class _RSASignatureContext(object):
