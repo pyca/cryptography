389b73bdd6234627865b5f500f3e572ac8aec709
Alex Gaynor <alex.gaynor@gmail.com>
2017-05-25 17:45:02 -0400

    This is only true on 1.1.0

--- tests/test_x509.py
+++ tests/test_x509.py
@@ -1462,8 +1462,9 @@ class TestRSACertificateRequest(object):
         # Check that each value was encoded as an ASN.1 PRINTABLESTRING.
         assert parsed.subject.chosen[0][0]['value'].chosen.tag == 19
         assert parsed.issuer.chosen[0][0]['value'].chosen.tag == 19
-        assert parsed.subject.chosen[1][0]['value'].chosen.tag == 19
-        assert parsed.issuer.chosen[1][0]['value'].chosen.tag == 19
+        if backend._lib.CRYPTOGRAPHY_OPENSSL_110_OR_GREATER:
+            assert parsed.subject.chosen[1][0]['value'].chosen.tag == 19
+            assert parsed.issuer.chosen[1][0]['value'].chosen.tag == 19
 
 
 class TestCertificateBuilder(object):
