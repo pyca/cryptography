349923379f1c1baf51ff1709abaa19559a59ad69
Alex Gaynor <alex.gaynor@gmail.com>
2017-05-25 23:59:14 -0400

    Document when OpenSSL changed its default (#3611)
    * Figure out when this came into existance by deleting it
    
    * Revert "Figure out when this came into existance by deleting it"
    
    This reverts commit 2fb9b8e104742bfa12eb5feedccebacfc23c66bb.
    
    * Clearly document when this became true

--- src/cryptography/hazmat/backends/openssl/backend.py
+++ src/cryptography/hazmat/backends/openssl/backend.py
@@ -91,7 +91,7 @@ class Backend(object):
         self._lib = self._binding.lib
 
         # Set the default string mask for encoding ASN1 strings to UTF8. This
-        # is the default for newer OpenSSLs for several years and is
+        # is the default for newer OpenSSLs for several years (1.0.1h+) and is
         # recommended in RFC 2459.
         res = self._lib.ASN1_STRING_set_default_mask_asc(b"utf8only")
         self.openssl_assert(res == 1)
