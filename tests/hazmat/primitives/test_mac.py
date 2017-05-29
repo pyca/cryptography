8ff0168073395de4b7adeee2c416389366093597
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-05-29 09:47:05 -0500

    use pytest.warns instead of deprecated_call
    The pytest docs insist that deprecation warnings are handled differently
    and that you should use deprecated_call, but this works so okay then

--- tests/hazmat/primitives/test_mac.py
+++ tests/hazmat/primitives/test_mac.py
@@ -6,8 +6,10 @@ from __future__ import absolute_import, division, print_function
 
 import pytest
 
+from cryptography import utils
+
 
 def test_deprecated_maccontext():
-    with pytest.deprecated_call():
+    with pytest.warns(utils.DeprecatedIn19):
         from cryptography.hazmat.primitives.interfaces import MACContext
         assert MACContext
