16f8aae41a7c7297454860a9bb55372cc39dd9c7
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-06-06 17:10:07 -1000

    of course

--- .travis/run.sh
+++ .travis/run.sh
@@ -13,11 +13,12 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     # if the build is static we need different LDFLAGS
     if [[ "${CRYPTOGRAPHY_SUPPRESS_LINK_FLAGS}" == "1" ]]; then
         export LDFLAGS="/usr/local/opt/openssl\@1.1/lib/libssl.a /usr/local/opt/openssl\@1.1/lib/libcrypto.a"
+        export CFLAGS="-I/usr/local/opt/openssl\@1.1/include"
     else
         # Compile the dynamic link build against 1.0.2 because the linker refuses to properly load 1.1.0
         export LDFLAGS="-L/usr/local/opt/openssl/lib"
+        export CFLAGS="-I/usr/local/opt/openssl/include"
     fi
-    export CFLAGS="-I/usr/local/opt/openssl\@1.1/include"
 else
     if [[ "${TOXENV}" == "pypy" ]]; then
         PYENV_ROOT="$HOME/.pyenv"
