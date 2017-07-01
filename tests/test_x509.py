2ab0444e35dbe835a389f5e665c301e17dacc047
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-06-30 19:24:21 -0500

    pep8

--- tests/test_x509.py
+++ tests/test_x509.py
@@ -34,8 +34,8 @@ from cryptography.x509.oid import (
 )
 
 from .hazmat.primitives.fixtures_dsa import DSA_KEY_2048
-from .hazmat.primitives.fixtures_rsa import RSA_KEY_2048, RSA_KEY_512
 from .hazmat.primitives.fixtures_ec import EC_KEY_SECP256R1
+from .hazmat.primitives.fixtures_rsa import RSA_KEY_2048, RSA_KEY_512
 from .hazmat.primitives.test_ec import _skip_curve_unsupported
 from .utils import load_vectors_from_file
 
