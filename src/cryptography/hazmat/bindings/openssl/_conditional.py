b53b08d2de02c29463f76fd7e057a788d8a3f624
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-07-08 19:42:33 -0500

    oops

--- src/cryptography/hazmat/bindings/openssl/_conditional.py
+++ src/cryptography/hazmat/bindings/openssl/_conditional.py
@@ -257,7 +257,6 @@ CONDITIONAL_NAMES = {
     "Cryptography_HAS_RSA_R_PKCS_DECODING_ERROR": (
         cryptography_has_rsa_r_pkcs_decoding_error
     ),
-    "Cryptography_HAS_EGD": cryptography_has_egd,
     "Cryptography_HAS_RSA_OAEP_MD": cryptography_has_rsa_oaep_md,
     "Cryptography_HAS_SSL3_METHOD": cryptography_has_ssl3_method,
     "Cryptography_HAS_ALPN": cryptography_has_alpn,
