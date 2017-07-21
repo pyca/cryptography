0a8bdff51330a380d2ae4c9dd85b7798111b3528
Alex Gaynor <alex.gaynor@gmail.com>
2017-07-21 13:57:50 -0400

    simplify pyenchant requirement (#3805)

--- setup.py
+++ setup.py
@@ -293,7 +293,7 @@ setup(
         "test": test_requirements,
         "docstest": [
             "doc8",
-            "pyenchant != 1.6.9, != 1.6.10",
+            "pyenchant >= 1.6.11",
             "readme_renderer >= 16.0",
             "sphinx != 1.6.1, != 1.6.2, != 1.6.3",
             "sphinx_rtd_theme",
