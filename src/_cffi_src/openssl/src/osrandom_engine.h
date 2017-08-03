b5e71319a5f1214ad50331ff0b074875e51a19c0
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-08-03 11:00:57 -0500

    update a comment and a style nit

--- src/_cffi_src/openssl/src/osrandom_engine.h
+++ src/_cffi_src/openssl/src/osrandom_engine.h
@@ -14,8 +14,8 @@
   #ifdef __APPLE__
     #include <sys/random.h>
     /* To support weak linking we need to declare this as a weak import even if
-     * it's not present in sys/random. */
-    extern int getentropy(void* buffer, size_t size) __attribute((weak_import));
+     * it's not present in sys/random (e.g. macOS < 10.12). */
+    extern int getentropy(void *buffer, size_t size) __attribute((weak_import));
   #endif
 
   #ifdef __linux__
