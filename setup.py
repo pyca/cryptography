8ed9fc77e309fbf3de39bf5911dedd03441a5d65
Alex Gaynor <alex.gaynor@gmail.com>
2017-12-26 11:41:55 -0500

    Fixed #4039 -- added a python_requires to setup.py

--- setup.py
+++ setup.py
@@ -283,6 +283,8 @@ setup(
     packages=find_packages(where="src", exclude=["_cffi_src", "_cffi_src.*"]),
     include_package_data=True,
 
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+
     install_requires=[
         "idna >= 2.1",
         "asn1crypto >= 0.21.0",
