110398ee2e28a164ad6e564587d392bfb870eb16
Tux <tuxxy@users.noreply.github.com>
2018-01-05 17:53:45 -0700

    Use `BN_clear_free` in places where `BN_free` is being used (#4072)
    * Expose BN_clear_free
    
    * Use BN_clear_free in test_int_to_bn
    
    * Use BN_clear_free in lieu of BN_free
    
    * Use BN_free on public values

--- src/cryptography/hazmat/backends/openssl/backend.py
+++ src/cryptography/hazmat/backends/openssl/backend.py
@@ -1359,7 +1359,7 @@ class Backend(object):
             ec_cdata, public.x, public.y)
 
         private_value = self._ffi.gc(
-            self._int_to_bn(numbers.private_value), self._lib.BN_free
+            self._int_to_bn(numbers.private_value), self._lib.BN_clear_free
         )
         res = self._lib.EC_KEY_set_private_key(ec_cdata, private_value)
         self.openssl_assert(res == 1)
@@ -1394,7 +1394,7 @@ class Backend(object):
         point = self._ffi.gc(point, self._lib.EC_POINT_free)
 
         value = self._int_to_bn(private_value)
-        value = self._ffi.gc(value, self._lib.BN_free)
+        value = self._ffi.gc(value, self._lib.BN_clear_free)
 
         with self._tmp_bn_ctx() as bn_ctx:
             res = self._lib.EC_POINT_mul(group, point, value, self._ffi.NULL,
