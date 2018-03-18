3592a7fe8b3ee3e58acca0f6a2dfd6ddbb8ae173
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-03-18 09:49:28 -0400

    oxford comma

--- src/cryptography/hazmat/primitives/asymmetric/dsa.py
+++ src/cryptography/hazmat/primitives/asymmetric/dsa.py
@@ -131,7 +131,7 @@ def _check_dsa_parameters(parameters):
     if parameters.p.bit_length() not in [1024, 2048, 3072]:
         raise ValueError("p must be exactly 1024, 2048, or 3072 bits long")
     if parameters.q.bit_length() not in [160, 224, 256]:
-        raise ValueError("q must be exactly 160, 224 or 256 bits long")
+        raise ValueError("q must be exactly 160, 224, or 256 bits long")
 
     if not (1 < parameters.g < parameters.p):
         raise ValueError("g, p don't satisfy 1 < g < p.")
