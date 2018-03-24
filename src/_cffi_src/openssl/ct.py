34f55aeb54d67c70833ae1597229d8aac7273921
Alex Gaynor <alex.gaynor@gmail.com>
2018-03-24 15:08:35 -0400

    CT is never available on libressl

--- src/_cffi_src/openssl/ct.py
+++ src/_cffi_src/openssl/ct.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import, division, print_function
 
 INCLUDES = """
-#if CRYPTOGRAPHY_OPENSSL_110_OR_GREATER
+#if CRYPTOGRAPHY_OPENSSL_110_OR_GREATER && !CRYPTOGRAPHY_IS_LIBRESSL
 #include <openssl/ct.h>
 
 typedef STACK_OF(SCT) Cryptography_STACK_OF_SCT;
