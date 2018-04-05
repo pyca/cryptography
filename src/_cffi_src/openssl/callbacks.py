cbdc9baafd4cc919abc7aed070d08d8ea847aa33
Thierry Bastian <thierryb@filewave.com>
2018-04-05 16:15:53 +0200

    OpenSSL 1.0.2o has switched to winsock2 (#4184)
    So here we need to make sure we don't simply include windows but only the parts that we want

--- src/_cffi_src/openssl/callbacks.py
+++ src/_cffi_src/openssl/callbacks.py
@@ -11,7 +11,10 @@ INCLUDES = """
 #include <openssl/crypto.h>
 
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <Wincrypt.h>
+#include <Winsock2.h>
 #else
 #include <stdio.h>
 #include <stdlib.h>
