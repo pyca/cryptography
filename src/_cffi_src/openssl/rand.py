db131983f4e6ae6c2ec34072fbff0e30d967c8c7
David Benjamin <davidben@davidben.net>
2018-05-14 16:09:36 -0400

    Remove some unused RAND bindings. (#4239)
    These are unused. (And not especially useful.)

--- src/_cffi_src/openssl/rand.py
+++ src/_cffi_src/openssl/rand.py
@@ -13,12 +13,8 @@ static const long Cryptography_HAS_EGD;
 """
 
 FUNCTIONS = """
-void RAND_seed(const void *, int);
 void RAND_add(const void *, int, double);
 int RAND_status(void);
-const char *RAND_file_name(char *, size_t);
-int RAND_load_file(const char *, long);
-int RAND_write_file(const char *);
 int RAND_bytes(unsigned char *, int);
 /* ERR_load_RAND_strings started returning an int in 1.1.0. Unfortunately we
    can't declare a conditional signature like that. Since it always returns
