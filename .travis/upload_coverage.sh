b6cfb331db882a0c2a2d66714e2bd653716959f9
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-16 10:27:07 -0400

    Only send coverage for tox builds, in prep for #4228 (#4248)

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -3,13 +3,15 @@
 set -e
 set -x
 
-case "${TOXENV}" in
-    pypy-nocoverage);;
-    pep8);;
-    py3pep8);;
-    docs);;
-    *)
-        source ~/.venv/bin/activate
-        codecov --env TRAVIS_OS_NAME,TOXENV,OPENSSL
-        ;;
-esac
+if [ -n "${TOXENV}" ]; then
+    case "${TOXENV}" in
+        pypy-nocoverage);;
+        pep8);;
+        py3pep8);;
+        docs);;
+        *)
+            source ~/.venv/bin/activate
+            codecov --env TRAVIS_OS_NAME,TOXENV,OPENSSL
+            ;;
+    esac
+fi
