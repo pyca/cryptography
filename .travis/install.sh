99bd1556d95dfed131c7c6ec3de8f90a7e19690d
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-05-21 14:35:02 +0800

    build and test libre on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+shlib_sed() {
+    # modify the shlib version to a unique one to make sure the dynamic
+    # linker doesn't load the system one.
+    sed -i "s/^SHLIB_MAJOR=.*/SHLIB_MAJOR=100/" Makefile
+    sed -i "s/^SHLIB_MINOR=.*/SHLIB_MINOR=0.0/" Makefile
+    sed -i "s/^SHLIB_VERSION_NUMBER=.*/SHLIB_VERSION_NUMBER=100.0.0/" Makefile
+}
+
 # download, compile, and install if it's not already present via travis
 # cache
 if [ -n "${OPENSSL}" ]; then
@@ -12,14 +20,19 @@ if [ -n "${OPENSSL}" ]; then
         tar zxf "openssl-${OPENSSL}.tar.gz"
         cd "openssl-${OPENSSL}"
         ./config shared no-asm no-ssl2 no-ssl3 -fPIC --prefix="$HOME/$OPENSSL_DIR"
-        # modify the shlib version to a unique one to make sure the dynamic
-        # linker doesn't load the system one. This isn't required for 1.1.0 at the
-        # moment since our Travis builders have a diff shlib version, but it doesn't hurt
-        sed -i "s/^SHLIB_MAJOR=.*/SHLIB_MAJOR=100/" Makefile
-        sed -i "s/^SHLIB_MINOR=.*/SHLIB_MINOR=0.0/" Makefile
-        sed -i "s/^SHLIB_VERSION_NUMBER=.*/SHLIB_VERSION_NUMBER=100.0.0/" Makefile
+        shlib_sed
         make depend
         make install
+    fi
+elif [ -n "${LIBRESSL}" ]; then
+    LIBRESSL_DIR="ossl-1/${LIBRESSL}"
+    if [[ ! -f "$HOME/$LIBRESSL_DIR/bin/openssl" ]]; then
+        curl -O "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL}.tar.gz"
+        tar zxf "libressl-${LIBRESSL}.tar.gz"
+        cd "libressl-${LIBRESSL}"
+        ./config -Wl -Wl,-Bsymbolic-functions -fPIC shared --prefix="$HOME/$LIBRESSL_DIR"
+        shlib_sed
+        make -j"$(nproc)" install
     fi
 fi
 pip install virtualenv
