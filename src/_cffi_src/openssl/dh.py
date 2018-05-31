18e7cc7850c9602fffae03d1933e4f2d7b9e7fbf
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-30 21:20:51 -0400

    Update dh.py

--- src/_cffi_src/openssl/dh.py
+++ src/_cffi_src/openssl/dh.py
@@ -45,8 +45,8 @@ int Cryptography_i2d_DHxparams_bio(BIO *bp, DH *x);
 """
 
 CUSTOMIZATIONS = """
-/* These functions were added in OpenSSL 1.1.0-pre5 (beta2) */
-#if CRYPTOGRAPHY_OPENSSL_LESS_THAN_110PRE5
+/* These functions were added in OpenSSL 1.1.0 */
+#if CRYPTOGRAPHY_OPENSSL_LESS_THAN_110
 void DH_get0_pqg(const DH *dh,
                  const BIGNUM **p, const BIGNUM **q, const BIGNUM **g)
 {
