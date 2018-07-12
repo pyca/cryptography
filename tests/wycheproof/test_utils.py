289c89946af1514f41fb5eb541b11c4b2355e235
Alex Gaynor <alex.gaynor@gmail.com>
2018-07-12 20:08:51 +0000

    added additional test coverage

---
+++ tests/wycheproof/test_utils.py
@@ -0,0 +1,13 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+from __future__ import absolute_import, division, print_function
+
+
+from ..utils import WycheproofTest
+
+
+def test_wycheproof_test_repr():
+    wycheproof = WycheproofTest({}, {"tcId": 3})
+    assert repr(wycheproof) == "<WycheproofTest({}, {'tcId': 3}, tcId=3)>"
