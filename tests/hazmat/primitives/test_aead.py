db62ec9967d95e666eb6898766944d9e50532b2d
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-07-18 00:06:10 +0800

    also check iv length for GCM nonce in AEAD (#4350)
    * also check iv length for GCM nonce in AEAD
    
    * ugh

--- tests/hazmat/primitives/test_aead.py
+++ tests/hazmat/primitives/test_aead.py
@@ -383,6 +383,12 @@ class TestAESGCM(object):
         with pytest.raises(TypeError):
             aesgcm.decrypt(nonce, data, associated_data)
 
+    def test_invalid_nonce_length(self, backend):
+        key = AESGCM.generate_key(128)
+        aesgcm = AESGCM(key)
+        with pytest.raises(ValueError):
+            aesgcm.encrypt(b"", b"hi", None)
+
     def test_bad_key(self, backend):
         with pytest.raises(TypeError):
             AESGCM(object())
