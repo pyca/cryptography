f63b824bfc2f261643d0118295be15052ae28e73
mattsb42-aws <bullocm@amazon.com>
2018-08-28 16:17:34 -0700

    fix run.sh syntax

--- .travis/run.sh
+++ .travis/run.sh
@@ -26,11 +26,11 @@ source ~/.venv/bin/activate
 if [ -n "${TOXENV}" ]; then
     tox -- --wycheproof-root=$HOME/wycheproof
 else
-    downstream_script = "${0}/downstream.d/${DOWNSTREAM}.sh"
+    downstream_script="${TRAVIS_BUILD_DIR}/.travis/downstream.d/${DOWNSTREAM}.sh"
     if [ ! -x $downstream_script ]; then
         exit 1
     fi
-    $(downstream_script) install
+    $downstream_script install
     pip install .
-    $(downstream_script) run
+    $downstream_script run
 fi
