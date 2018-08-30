bd8b90857977c17612959f002c887902d46f3d47
mattsb42-aws <bullocm@amazon.com>
2018-08-30 13:01:58 -0700

    explicitly identify test directory in dynanmodb-encryption-sdk downstream tests

--- .travis/downstream.d/dynamodb-encryption-sdk.sh
+++ .travis/downstream.d/dynamodb-encryption-sdk.sh
@@ -9,7 +9,7 @@ case "${1}" in
         ;;
     run)
         cd aws-dynamodb-encryption-python
-        pytest -m "local and not slow and not veryslow and not nope"
+        pytest test/ -m "local and not slow and not veryslow and not nope"
         ;;
     *)
         exit 1
