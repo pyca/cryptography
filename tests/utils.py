9682a67d78e08f43b8655d50d1e6b4cf0a5148de
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-09-06 16:35:02 -0500

    don't need this any more

--- tests/utils.py
+++ tests/utils.py
@@ -28,13 +28,11 @@ KeyedHashVector = collections.namedtuple(
 
 
 def check_backend_support(backend, item):
-    supported = item.node.iter_markers("supported")
-    if supported:
-        for mark in supported:
-            if not mark.kwargs["only_if"](backend):
-                pytest.skip("{0} ({1})".format(
-                    mark.kwargs["skip_message"], backend
-                ))
+    for mark in item.node.iter_markers("supported"):
+        if not mark.kwargs["only_if"](backend):
+            pytest.skip("{0} ({1})".format(
+                mark.kwargs["skip_message"], backend
+            ))
 
 
 @contextmanager
