25566ac5dbbad5cf69e67973d44794a50ca6ccb1
erik <erik@getvida.io>
2018-09-13 16:26:26 -0400

    remove blank lines

--- src/cryptography/hazmat/backends/openssl/ec.py
+++ src/cryptography/hazmat/backends/openssl/ec.py
@@ -296,5 +296,3 @@ class _EllipticCurvePublicKey(object):
             self._backend, data, signature_algorithm._algorithm
         )
         _ecdsa_sig_verify(self._backend, self, signature, data)
-
-
