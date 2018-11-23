d7aba7c084b5376c46f872c36650ce7c51887565
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-11-23 08:41:50 +0800

    refactor x25519 exchange into utils

--- src/cryptography/hazmat/backends/openssl/utils.py
+++ src/cryptography/hazmat/backends/openssl/utils.py
@@ -11,6 +11,30 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.utils import Prehashed
 
 
+def _evp_pkey_derive(backend, evp_pkey, peer_public_key):
+    ctx = backend._lib.EVP_PKEY_CTX_new(evp_pkey, backend._ffi.NULL)
+    backend.openssl_assert(ctx != backend._ffi.NULL)
+    ctx = backend._ffi.gc(ctx, backend._lib.EVP_PKEY_CTX_free)
+    res = backend._lib.EVP_PKEY_derive_init(ctx)
+    backend.openssl_assert(res == 1)
+    res = backend._lib.EVP_PKEY_derive_set_peer(
+        ctx, peer_public_key._evp_pkey
+    )
+    backend.openssl_assert(res == 1)
+    keylen = backend._ffi.new("size_t *")
+    res = backend._lib.EVP_PKEY_derive(ctx, backend._ffi.NULL, keylen)
+    backend.openssl_assert(res == 1)
+    backend.openssl_assert(keylen[0] > 0)
+    buf = backend._ffi.new("unsigned char[]", keylen[0])
+    res = backend._lib.EVP_PKEY_derive(ctx, buf, keylen)
+    if res != 1:
+        raise ValueError(
+            "Null shared key derived from public/private pair."
+        )
+
+    return backend._ffi.buffer(buf, keylen[0])[:]
+
+
 def _calculate_digest_and_algorithm(backend, data, algorithm):
     if not isinstance(algorithm, Prehashed):
         hash_ctx = hashes.Hash(algorithm, backend)
