49b3fdd6f80eb7555992a413ec39199c3a2d8b42
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-11-26 09:30:31 +0800

    a PKCS12 interface to discuss

---
+++ src/cryptography/hazmat/primitives/serialization/pkcs12.py
@@ -0,0 +1,36 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+from __future__ import absolute_import, division, print_function
+
+import abc
+
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class PKCS12(object):
+    def __init__(self, certificate, private_key, additional_certificates):
+        """
+        Create a PKCS12 object with a certificate, private key, and
+        additional certificates.
+        """
+
+    @abc.abstractproperty
+    def private_key(self):
+        """
+        The private key inside the PKCS12 object.
+        """
+
+    @abc.abstractproperty
+    def certificate(self):
+        """
+        The certificate associated with the private key.
+        """
+
+    @abc.abstractproperty
+    def additional_certificates(self):
+        """
+        Additional certificates inside the PKCS12 object.
+        """
