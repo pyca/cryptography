b29eca78edcab57e0630822df934f7500d7f06f2
Alex Gaynor <alex.gaynor@gmail.com>
2019-02-25 12:56:15 +0000

    Polish off removal of unused engine bindings

--- src/_cffi_src/openssl/engine.py
+++ src/_cffi_src/openssl/engine.py
@@ -11,12 +11,10 @@ INCLUDES = """
 TYPES = """
 typedef ... ENGINE;
 
-static const int ENGINE_R_CONFLICTING_ENGINE_ID;
 static const long Cryptography_HAS_ENGINE;
 """
 
 FUNCTIONS = """
-int ENGINE_add(ENGINE *);
 ENGINE *ENGINE_by_id(const char *);
 int ENGINE_init(ENGINE *);
 int ENGINE_finish(ENGINE *);
@@ -27,35 +25,25 @@ int ENGINE_ctrl_cmd(ENGINE *, const char *, long, void *, void (*)(void), int);
 int ENGINE_free(ENGINE *);
 const char *ENGINE_get_name(const ENGINE *);
 
-/* these became macros in 1.1.0 */
-void ENGINE_load_dynamic(void);
 """
 
 CUSTOMIZATIONS = """
 #ifdef OPENSSL_NO_ENGINE
 static const long Cryptography_HAS_ENGINE = 0;
 
-static const int ENGINE_R_CONFLICTING_ENGINE_ID = 0;
-
-int (*ENGINE_add)(ENGINE *) = NULL;
 ENGINE *(*ENGINE_by_id)(const char *) = NULL;
 int (*ENGINE_init)(ENGINE *) = NULL;
 int (*ENGINE_finish)(ENGINE *) = NULL;
 ENGINE *(*ENGINE_get_default_RAND)(void) = NULL;
 int (*ENGINE_set_default_RAND)(ENGINE *) = NULL;
 void (*ENGINE_unregister_RAND)(ENGINE *) = NULL;
-int (*ENGINE_ctrl)(ENGINE *, int, long, void *, void (*)(void)) = NULL;
 int (*ENGINE_ctrl_cmd)(ENGINE *, const char *, long, void *,
                        void (*)(void), int) = NULL;
-int (*ENGINE_ctrl_cmd_string)(ENGINE *, const char *, const char *,
-                              int) = NULL;
 
 int (*ENGINE_free)(ENGINE *) = NULL;
 const char *(*ENGINE_get_id)(const ENGINE *) = NULL;
 const char *(*ENGINE_get_name)(const ENGINE *) = NULL;
 
-/* these became macros in 1.1.0 */
-void (*ENGINE_load_dynamic)(void) = NULL;
 #else
 static const long Cryptography_HAS_ENGINE = 1;
 #endif
