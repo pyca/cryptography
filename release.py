4c939d62bbc50b22982e6473ca80e33010611574
Alex Gaynor <alex.gaynor@gmail.com>
2019-05-25 10:34:41 -0400

    Adapt release.py to combine azure and jenkins wheels

--- release.py
+++ release.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function
 
 import getpass
 import glob
+import io
 import json
 import os
 import subprocess
@@ -20,6 +21,8 @@ import click
 
 from msrest.authentication import BasicAuthentication
 
+import requests
+
 
 JENKINS_URL = (
     "https://ci.cryptography.io/job/cryptography-support-jobs/"
@@ -32,7 +35,7 @@ def run(*args, **kwargs):
     subprocess.check_call(list(args), **kwargs)
 
 
-def wait_for_build_completed(build_client, build_id):
+def wait_for_build_completed_azure(build_client, build_id):
     while True:
         build = build_client.get_build("cryptography", build_id)
         if build.finish_time is not None:
@@ -40,7 +43,7 @@ def wait_for_build_completed(build_client, build_id):
         time.sleep(3)
 
 
-def download_artifacts(build_client, build_id):
+def download_artifacts_azure(build_client, build_id):
     artifacts = build_client.get_artifacts("cryptography", build_id)
     paths = []
     for artifact in artifacts:
@@ -67,7 +70,7 @@ def download_artifacts(build_client, build_id):
     return paths
 
 
-def build_wheels(version):
+def build_wheels_azure(version):
     token = getpass.getpass("Azure personal access token: ")
     credentials = BasicAuthentication("", token)
     connection = Connection(
@@ -84,8 +87,78 @@ def build_wheels(version):
     build = build_client.queue_build(
         project="cryptography", build=build_description
     )
-    wait_for_build_completed(build_client, build.id)
-    return download_artifacts(build_client, build.id)
+    wait_for_build_completed_azure(build_client, build.id)
+    return download_artifacts_azure(build_client, build.id)
+
+
+def wait_for_build_completed_jenkins(session):
+    # Wait 20 seconds before actually checking if the build is complete, to
+    # ensure that it had time to really start.
+    time.sleep(20)
+    while True:
+        response = session.get(
+            "{0}/lastBuild/api/json/".format(JENKINS_URL),
+            headers={
+                "Accept": "application/json",
+            }
+        )
+        response.raise_for_status()
+        if not response.json()["building"]:
+            assert response.json()["result"] == "SUCCESS"
+            break
+        time.sleep(0.1)
+
+
+def download_artifacts_jenkins(session):
+    response = session.get(
+        "{0}/lastBuild/api/json/".format(JENKINS_URL),
+        headers={
+            "Accept": "application/json"
+        }
+    )
+    response.raise_for_status()
+    json_response = response.json()
+    assert not json_response["building"]
+    assert json_response["result"] == "SUCCESS"
+
+    paths = []
+
+    for artifact in json_response["artifacts"]:
+        response = session.get(
+            "{0}artifact/{1}".format(
+                json_response["url"], artifact["relativePath"]
+            ), stream=True
+        )
+        assert response.headers["content-length"]
+        print("Downloading {0}".format(artifact["fileName"]))
+        content = io.BytesIO()
+        for data in response.iter_content(chunk_size=8192):
+            content.write(data)
+        out_path = os.path.join(
+            os.path.dirname(__file__),
+            "dist",
+            artifact["fileName"],
+        )
+        with open(out_path, "wb") as f:
+            f.write(content.getvalue())
+        paths.append(out_path)
+    return paths
+
+
+def build_wheels_jenkins(version):
+    token = getpass.getpass("Input the Jenkins token: ")
+    session = requests.Session()
+    response = session.get(
+        "{0}/buildWithParameters".format(JENKINS_URL),
+        params={
+            "token": token,
+            "BUILD_VERSION": version,
+            "cause": "Building wheels for {0}".format(version)
+        }
+    )
+    response.raise_for_status()
+    wait_for_build_completed_jenkins(session)
+    return download_artifacts_jenkins(session)
 
 
 @click.command()
@@ -106,8 +179,9 @@ def release(version):
     )
     run("twine", "upload", "-s", *packages)
 
-    paths = build_wheels(version)
-    run("twine", "upload", *paths)
+    azure_wheel_paths = build_wheels_azure(version)
+    jenkins_wheel_paths = build_wheels_jenkins(version)
+    run("twine", "upload", *(azure_wheel_paths + jenkins_wheel_paths))
 
 
 if __name__ == "__main__":
