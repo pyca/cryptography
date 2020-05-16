84be0741033f573cc17590e36f1c3468450fd4aa
Alex Gaynor <alex.gaynor@gmail.com>
2020-05-16 15:07:12 -0400

    Deprecate support for Python 2

--- src/cryptography/__init__.py
+++ src/cryptography/__init__.py
@@ -4,13 +4,25 @@
 
 from __future__ import absolute_import, division, print_function
 
+import sys
+import warnings
+
 from cryptography.__about__ import (
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__
 )
+from cryptography.utils import CryptographyDeprecationWarning
 
 
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
 ]
+
+if sys.version_info[0] == 2:
+    warnings.warn(
+        "Python 2 is no longer supported by the Python core team. Support for "
+        "it is now deprecated in cryptography, and will be removed in a "
+        "future release.",
+        CryptographyDeprecationWarning
+    )
