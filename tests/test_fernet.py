a0bd42c11ef4c426432b94f2ed8614b4e4d83c79
Jakub Stasiak <jakub@stasiak.at>
2020-05-25 21:21:29 +0200

    Trigger a Fernet.encrypt() branch missing from coverage

--- tests/test_fernet.py
+++ tests/test_fernet.py
@@ -134,8 +134,7 @@ class TestFernet(object):
     def test_extract_timestamp(self, monkeypatch, backend):
         f = Fernet(base64.urlsafe_b64encode(b"\x00" * 32), backend=backend)
         current_time = 1526138327
-        monkeypatch.setattr(time, "time", lambda: current_time)
-        token = f.encrypt(b'encrypt me')
+        token = f.encrypt(b'encrypt me', current_time)
         assert f.extract_timestamp(token) == current_time
         with pytest.raises(InvalidToken):
             f.extract_timestamp(b"nonsensetoken")
