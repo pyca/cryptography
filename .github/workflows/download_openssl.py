371ef3782735ee467d8dcb88f892b06403dfaa97
Alex Gaynor <alex.gaynor@gmail.com>
2020-07-30 16:08:55 -0400

    Attempt to retry when downloading OpenSSL

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -9,6 +9,12 @@ from urllib3.util.retry import Retry
 
 
 def get_response(session, url, token):
+    # Retry on non-502s
+    for i in range(5):
+        response = session.get(url, headers={"Authorization": "token " + token})
+        if response.status_code != 200:
+            continue
+        return response
     response = session.get(url, headers={"Authorization": "token " + token})
     if response.status_code != 200:
         raise ValueError(
