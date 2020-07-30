241f845071a8747d0986ed60575e28840f096b79
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-07-30 15:03:34 -0500

    handle unnamed-but-really-named curves in 1.0.2u (#5362)
    * handle unnamed-but-really-named curves in 1.0.2u
    
    * handle openssl 1.0.2 not supporting better install commands on make
    
    * do what openssl didn't feel was necessary in 1.0.2t/u
    
    I didn't bind the named curve constant, fight me.

--- src/cryptography/hazmat/backends/openssl/backend.py
+++ src/cryptography/hazmat/backends/openssl/backend.py
@@ -1578,8 +1578,19 @@ class Backend(object):
 
     def _ec_key_new_by_curve(self, curve):
         curve_nid = self._elliptic_curve_to_nid(curve)
+        return self._ec_key_new_by_curve_nid(curve_nid)
+
+    def _ec_key_new_by_curve_nid(self, curve_nid):
         ec_cdata = self._lib.EC_KEY_new_by_curve_name(curve_nid)
         self.openssl_assert(ec_cdata != self._ffi.NULL)
+        # Setting the ASN.1 flag to OPENSSL_EC_NAMED_CURVE is
+        # only necessary on OpenSSL 1.0.2t/u. Once we drop support for 1.0.2
+        # we can remove this as it's done automatically when getting an EC_KEY
+        # from new_by_curve_name
+        # CRYPTOGRAPHY_OPENSSL_102U_OR_GREATER
+        self._lib.EC_KEY_set_asn1_flag(
+            ec_cdata, backend._lib.OPENSSL_EC_NAMED_CURVE
+        )
         return self._ffi.gc(ec_cdata, self._lib.EC_KEY_free)
 
     def load_der_ocsp_request(self, data):
