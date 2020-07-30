603b6ddd3e18c391e92fc5346e3181e82548d10d
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-07-30 09:29:01 -0500

    handle unnamed-but-really-named curves in 1.0.2u

--- src/cryptography/hazmat/backends/openssl/ec.py
+++ src/cryptography/hazmat/backends/openssl/ec.py
@@ -47,7 +47,7 @@ def _ec_key_curve_sn(backend, ec_key):
     # explicitly encoded a curve with the same parameters as a named curve.
     # Don't do that.
     if (
-        backend._lib.CRYPTOGRAPHY_OPENSSL_110_OR_GREATER
+        backend._lib.CRYPTOGRAPHY_OPENSSL_102U_OR_GREATER
         and backend._lib.EC_GROUP_get_asn1_flag(group) == 0
     ):
         raise NotImplementedError(
