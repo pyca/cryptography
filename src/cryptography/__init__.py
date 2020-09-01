pyca/cryptography

commit e597fa019d7aa335386fcf63b086012dc4b4f87f

deprecate support for python 3.5 (#5387)

src/cryptography/__init__.py
@@ -39,3 +39,10 @@ if sys.version_info[0] == 2:
         CryptographyDeprecationWarning,
         stacklevel=2,
     )
+if sys.version_info[:2] == (3, 5):
+    warnings.warn(
+        "Python 3.5 support will be dropped in the next release of"
+        "cryptography. Please upgrade your Python.",
+        CryptographyDeprecationWarning,
+        stacklevel=2,
+    )

commit 951a451aa3c5e7d4e654f482afc6cb80d720fcd9

Add a missing space to py35 deprecation warning
This fixes a typo that's been introduced in #5387.

src/cryptography/__init__.py
@@ -41,7 +41,7 @@ if sys.version_info[0] == 2:
     )
 if sys.version_info[:2] == (3, 5):
     warnings.warn(
-        "Python 3.5 support will be dropped in the next release of"
+        "Python 3.5 support will be dropped in the next release of "
         "cryptography. Please upgrade your Python.",
         CryptographyDeprecationWarning,
         stacklevel=2,