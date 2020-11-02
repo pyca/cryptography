2ab719c38cff64c4afc0cd4c61c878020fcde83b
Benjamin Peterson <benjamin@python.org>
2020-11-02 15:03:43 -0600

    padding: Tip-toe around bytes subclasses.
    This change allows future's newbytes class to be padded again.
    
    Fixes https://github.com/pyca/cryptography/issues/5547.

--- src/cryptography/hazmat/primitives/padding.py
+++ src/cryptography/hazmat/primitives/padding.py
@@ -42,7 +42,10 @@ def _byte_padding_update(buffer_, data, block_size):
 
     utils._check_byteslike("data", data)
 
-    buffer_ += bytes(data)
+    # six.PY2: Only coerce non-bytes objects to avoid triggering bad behavior
+    # of future's newbytes type. Unconditionally call bytes() after Python 2
+    # support is gone.
+    buffer_ += data if isinstance(data, bytes) else bytes(data)
 
     finished_blocks = len(buffer_) // (block_size // 8)
 
@@ -66,7 +69,10 @@ def _byte_unpadding_update(buffer_, data, block_size):
 
     utils._check_byteslike("data", data)
 
-    buffer_ += bytes(data)
+    # six.PY2: Only coerce non-bytes objects to avoid triggering bad behavior
+    # of future's newbytes type. Unconditionally call bytes() after Python 2
+    # support is gone.
+    buffer_ += data if isinstance(data, bytes) else bytes(data)
 
     finished_blocks = max(len(buffer_) // (block_size // 8) - 1, 0)
 
