6280ed2633d67a6e3699b65ff1adcd77fd4910fb
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-11-11 13:38:50 -0600

    remove unused things in travis

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,23 +1,5 @@
 #!/bin/bash -ex
 
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-
-if [ -n "${LIBRESSL}" ]; then
-    LIBRESSL_DIR="ossl-2/${LIBRESSL}"
-    export CFLAGS="-Werror -Wno-error=deprecated-declarations -Wno-error=discarded-qualifiers -Wno-error=unused-function -I$HOME/$LIBRESSL_DIR/include"
-    export PATH="$HOME/$LIBRESSL_DIR/bin:$PATH"
-    export LDFLAGS="-L$HOME/$LIBRESSL_DIR/lib -Wl,-rpath=$HOME/$LIBRESSL_DIR/lib"
-fi
-
-if [ -n "${OPENSSL}" ]; then
-    . "$SCRIPT_DIR/openssl_config.sh"
-    export PATH="$HOME/$OPENSSL_DIR/bin:$PATH"
-    export CFLAGS="${CFLAGS} -I$HOME/$OPENSSL_DIR/include"
-    # rpath on linux will cause it to use an absolute path so we don't need to
-    # do LD_LIBRARY_PATH
-    export LDFLAGS="-L$HOME/$OPENSSL_DIR/lib -Wl,-rpath=$HOME/$OPENSSL_DIR/lib"
-fi
-
 source ~/.venv/bin/activate
 
 if [ -n "${TOXENV}" ]; then
