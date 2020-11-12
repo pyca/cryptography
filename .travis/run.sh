fc169da5a2bf3df5163458519d02bdb2ff56d533
Alex Gaynor <alex.gaynor@gmail.com>
2020-11-11 19:01:51 -0500

    Garbage collect more CI code

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,14 +2,10 @@
 
 source ~/.venv/bin/activate
 
-if [ -n "${TOXENV}" ]; then
-    tox -- --wycheproof-root="$HOME/wycheproof"
-else
-    downstream_script="${TRAVIS_BUILD_DIR}/.travis/downstream.d/${DOWNSTREAM}.sh"
-    if [ ! -x "$downstream_script" ]; then
-        exit 1
-    fi
-    $downstream_script install
-    pip install .
-    $downstream_script run
+downstream_script="${TRAVIS_BUILD_DIR}/.travis/downstream.d/${DOWNSTREAM}.sh"
+if [ ! -x "$downstream_script" ]; then
+    exit 1
 fi
+$downstream_script install
+pip install .
+$downstream_script run
