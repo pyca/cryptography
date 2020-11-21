fd948026e1d1a3b8d49072d6cd0e2b234484927d
Alex Gaynor <alex.gaynor@gmail.com>
2020-11-21 13:23:26 -0500

    Simplify CI scripts

--- .github/workflows/build_openssl.sh
+++ .github/workflows/build_openssl.sh
@@ -10,18 +10,16 @@ shlib_sed() {
   sed -i "s/^SHLIB_VERSION_NUMBER=.*/SHLIB_VERSION_NUMBER=100.0.0/" Makefile
 }
 
-# CONFIG_HASH is a global coming from a previous step
-OPENSSL_DIR="${GITHUB_WORKSPACE}/osslcache/${TYPE}-${VERSION}-${CONFIG_HASH}"
 if [[ "${TYPE}" == "openssl" ]]; then
   curl -O "https://www.openssl.org/source/openssl-${VERSION}.tar.gz"
   tar zxf "openssl-${VERSION}.tar.gz"
   pushd "openssl-${VERSION}"
   # CONFIG_FLAGS is a global coming from a previous step
-  ./config ${CONFIG_FLAGS} -fPIC --prefix="${OPENSSL_DIR}"
+  ./config ${CONFIG_FLAGS} -fPIC --prefix="${OSSL_PATH}"
   shlib_sed
   make depend
   make -j"$(nproc)"
-  # avoid installing the docs on versions of OpenSSL that aren't ancient.
+  # avoid installing the docs (for performance)
   # https://github.com/openssl/openssl/issues/6685#issuecomment-403838728
   make install_sw install_ssldirs
   popd
@@ -29,7 +27,7 @@ elif [[ "${TYPE}" == "libressl" ]]; then
   curl -O "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"
   tar zxf "libressl-${VERSION}.tar.gz"
   pushd "libressl-${VERSION}"
-  ./config -Wl -Wl,-Bsymbolic-functions -fPIC shared --prefix="${OPENSSL_DIR}"
+  ./config -Wl -Wl,-Bsymbolic-functions -fPIC shared --prefix="${OSSL_PATH}"
   shlib_sed
   make -j"$(nproc)" install
   popd
