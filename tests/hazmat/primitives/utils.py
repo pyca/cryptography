4ca101db5fb6e2f3eddb31d3f8de5d8c2fce603a
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-12-09 10:01:57 -0600

    remove AES tests we will always skip

--- tests/hazmat/primitives/utils.py
+++ tests/hazmat/primitives/utils.py
@@ -76,6 +76,8 @@ def generate_aead_test(
     param_loader, path, file_names, cipher_factory, mode_factory
 ):
     all_params = _load_all_params(path, file_names, param_loader)
+    # We don't support IVs < 64-bit so just strip them out
+    all_params = [i for i in all_params if len(i["iv"]) >= 16]
 
     def test_aead(self, backend, subtests):
         for params in all_params:
@@ -86,10 +88,6 @@ def generate_aead_test(
 
 
 def aead_test(backend, cipher_factory, mode_factory, params):
-    if mode_factory is GCM and len(params["iv"]) < 16:
-        # 16 because this is hex encoded data
-        pytest.skip("Less than 64-bit IVs are no longer supported")
-
     if (
         mode_factory is GCM
         and backend._fips_enabled
