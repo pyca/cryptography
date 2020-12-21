ea5b1576ebf7cd75a7d510b67d7a68a8c729158a
Maximilian Hils <git@maximilianhils.com>
2020-12-21 15:02:37 +0100

    make linter happy

--- src/_cffi_src/openssl/ssl.py
+++ src/_cffi_src/openssl/ssl.py
@@ -590,8 +590,11 @@ static const long TLS_ST_OK = 0;
 #if CRYPTOGRAPHY_IS_LIBRESSL
 static const long SSL_OP_NO_DTLSv1 = 0;
 static const long SSL_OP_NO_DTLSv1_2 = 0;
-/* it's not really clear why DTLS1_VERSION is defined, but it is */
-/* don't set this to 0, SSL_CTX_set_min_proto_version(DTLS1_2_VERSION) would silently allow all versions */
+/* it's not really clear why DTLS1_VERSION is defined, but it is. */
+/*
+don't set this to 0,
+SSL_CTX_set_min_proto_version(TLS1_3_VERSION) would silently allow all version
+*/
 static const long DTLS1_2_VERSION = -1;
 long (*DTLS_set_link_mtu)(SSL *, long) = NULL;
 long (*DTLS_get_link_min_mtu)(SSL *) = NULL;
@@ -688,7 +691,10 @@ SRTP_PROTECTION_PROFILE * (*SSL_get_selected_srtp_profile)(SSL *) = NULL;
 
 #if CRYPTOGRAPHY_OPENSSL_LESS_THAN_111
 static const long Cryptography_HAS_TLSv1_3 = 0;
-/* don't set this to 0, SSL_CTX_set_min_proto_version(TLS1_3_VERSION) would silently allow all version */
+/*
+don't set this to 0,
+SSL_CTX_set_min_proto_version(TLS1_3_VERSION) would silently allow all version
+*/
 static const long TLS1_3_VERSION = -1;
 static const long SSL_OP_NO_TLSv1_3 = 0;
 static const long SSL_VERIFY_POST_HANDSHAKE = 0;
