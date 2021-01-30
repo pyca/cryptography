902066343b57b37262fcde108bc3de22f1d805fb
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-01-30 15:25:10 -0600

    add typing to padding and constant time modules

--- src/cryptography/hazmat/primitives/constant_time.py
+++ src/cryptography/hazmat/primitives/constant_time.py
@@ -6,7 +6,7 @@
 import hmac
 
 
-def bytes_eq(a, b):
+def bytes_eq(a: bytes, b: bytes):
     if not isinstance(a, bytes) or not isinstance(b, bytes):
         raise TypeError("a and b must be bytes.")
 
