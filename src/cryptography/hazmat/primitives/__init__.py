8a9e14a2401c93a67831b0b19836a38f622d183b
Christian Heimes <cheimes@redhat.com>
2021-02-09 11:46:44 +0100

    Import serialization early to break cyclic import
    Fixes: https://github.com/pyca/cryptography/issues/5794
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- src/cryptography/hazmat/primitives/__init__.py
+++ src/cryptography/hazmat/primitives/__init__.py
@@ -1,3 +1,6 @@
 # This file is dual licensed under the terms of the Apache License, Version
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
+
+# break import cycle, see https://github.com/pyca/cryptography/issues/5794
+from . import serialization
