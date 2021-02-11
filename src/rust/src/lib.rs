9efc6d46fb98a52a9bb956096a9389f21bd8de92
Alex Gaynor <alex.gaynor@gmail.com>
2021-02-11 11:47:20 -0500

    fix a false positive from the latest clippy (#5813)

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -3,6 +3,8 @@
 // for complete details.
 
 #[pyo3::prelude::pymodule]
+// False positive: https://github.com/rust-lang/rust-clippy/issues/6721
+#[allow(clippy::unnecessary_wraps)]
 fn _rust(_py: pyo3::Python<'_>, _m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {
     Ok(())
 }
