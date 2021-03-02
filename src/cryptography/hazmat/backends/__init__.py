fc4e9adc2b229090210973e812e65f147f6360cd
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-03-02 10:58:48 -0600

    no generic typing for _get_backend

--- src/cryptography/hazmat/backends/__init__.py
+++ src/cryptography/hazmat/backends/__init__.py
@@ -4,7 +4,9 @@
 
 import typing
 
-_default_backend: typing.Any = None
+from cryptography.hazmat.backends.interfaces import Backend
+
+_default_backend: typing.Optional[Backend] = None
 
 
 def default_backend():
@@ -18,10 +20,7 @@ def default_backend():
     return _default_backend
 
 
-T = typing.TypeVar("T")
-
-
-def _get_backend(backend: typing.Optional[T]) -> T:
+def _get_backend(backend: typing.Optional[Backend]) -> Backend:
     if backend is None:
         return default_backend()
     else:
