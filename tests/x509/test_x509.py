88e5acb6d5d99dc16e0799a2b58d54f67bad7bdf
Mathias Ertl <mati@er.tl>
2021-03-08 17:32:01 +0100

    cast extension class

--- tests/x509/test_x509.py
+++ tests/x509/test_x509.py
@@ -10,6 +10,7 @@ import copy
 import datetime
 import ipaddress
 import os
+import typing
 
 import pytest
 
@@ -3615,8 +3616,11 @@ class TestCertificateSigningRequestBuilder(object):
         assert list(subject) == [
             x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Texas"),
         ]
-        basic_constraints = request.extensions.get_extension_for_oid(
-            ExtensionOID.BASIC_CONSTRAINTS
+        basic_constraints = typing.cast(
+            "x509.Extension[x509.BasicConstraints]",
+            request.extensions.get_extension_for_oid(
+                ExtensionOID.BASIC_CONSTRAINTS
+            ),
         )
         assert basic_constraints.value.ca is True
         assert basic_constraints.value.path_length == 2
@@ -3653,8 +3657,11 @@ class TestCertificateSigningRequestBuilder(object):
         assert list(subject) == [
             x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Texas"),
         ]
-        basic_constraints = request.extensions.get_extension_for_oid(
-            ExtensionOID.BASIC_CONSTRAINTS
+        basic_constraints = typing.cast(
+            "x509.Extension[x509.BasicConstraints]",
+            request.extensions.get_extension_for_oid(
+                ExtensionOID.BASIC_CONSTRAINTS
+            ),
         )
         assert basic_constraints.value.ca is True
         assert basic_constraints.value.path_length == 2
