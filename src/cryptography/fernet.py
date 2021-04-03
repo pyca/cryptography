2839b4b6a2e6c98023748ebccf58649afe6038e4
Sam Bull <aa6bs0@sambull.org>
2021-04-04 00:02:22 +0100

    Update annotation for key (#5951)
    * Update annotation for key
    
    * Update fernet.py
    
    * Update fernet.py

--- src/cryptography/fernet.py
+++ src/cryptography/fernet.py
@@ -27,7 +27,11 @@ _MAX_CLOCK_SKEW = 60
 
 
 class Fernet(object):
-    def __init__(self, key: bytes, backend: typing.Optional[Backend] = None):
+    def __init__(
+        self,
+        key: typing.Union[bytes, str],
+        backend: typing.Optional[Backend] = None,
+    ):
         backend = _get_backend(backend)
 
         key = base64.urlsafe_b64decode(key)
