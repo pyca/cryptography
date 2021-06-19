7a1e82dc7fb9c52e671accdcd3e4f4b2c1fb6b9e
Alex Gaynor <alex.gaynor@gmail.com>
2021-06-19 14:24:00 -0400

    Add another linkcheck ignore

--- docs/conf.py
+++ docs/conf.py
@@ -198,6 +198,8 @@ linkcheck_ignore = [
     r"https://info.isl.ntt.co.jp/crypt/eng/camellia/",
     # Inconsistent small DH params they seem incapable of fixing
     r"https://www.secg.org/sec1-v2.pdf",
+    # Incomplete cert chain
+    r"https://e-trust.gosuslugi.ru",
 ]
 
 autosectionlabel_prefix_document = True
