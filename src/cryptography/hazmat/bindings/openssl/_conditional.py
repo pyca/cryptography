ad45530c3bd1bba3b768d11ef83e58430daa5d1a
Nathaniel J. Smith <njs@pobox.com>
2021-06-28 07:04:17 -0700

    Attempt to hide my new undefined symbols

--- src/cryptography/hazmat/bindings/openssl/_conditional.py
+++ src/cryptography/hazmat/bindings/openssl/_conditional.py
@@ -244,6 +244,18 @@ def cryptography_has_providers():
     ]
 
 
+def cryptography_has_op_no_renegotiation():
+    return [
+        "SSL_OP_NO_RENEGOTIATION",
+    ]
+
+
+def DTLS_get_data_mtu():
+    return [
+        "DTLS_get_data_mtu",
+    ]
+
+
 # This is a mapping of
 # {condition: function-returning-names-dependent-on-that-condition} so we can
 # loop over them and delete unsupported names at runtime. It will be removed
@@ -291,4 +303,6 @@ CONDITIONAL_NAMES = {
     "Cryptography_HAS_SRTP": cryptography_has_srtp,
     "Cryptography_HAS_GET_PROTO_VERSION": cryptography_has_get_proto_version,
     "Cryptography_HAS_PROVIDERS": cryptography_has_providers,
+    "Cryptography_HAS_OP_NO_RENEGOTIATION": cryptography_has_op_no_renegotiation,
+    "DTLS_get_data_mtu": DTLS_get_data_mtu,
 }
