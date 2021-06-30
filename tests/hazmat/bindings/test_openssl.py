82b3dd14c87c81323fc8b09b1a5dddff0628afff
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-06-30 06:14:42 -0500

    3.0.0 deprecated func and it isn't useful to us in general (#6148)
    remove it everywhere and assert on the code/lib/reason

--- tests/hazmat/bindings/test_openssl.py
+++ tests/hazmat/bindings/test_openssl.py
@@ -91,11 +91,10 @@ class TestOpenSSL(object):
             _openssl_assert(b.lib, False)
 
         error = exc_info.value.err_code[0]
-        # As of 3.0.0 OpenSSL sets func codes to 0, so the combined
-        # code is a different value
+        # As of 3.0.0 OpenSSL no longer sets func codes (which we now also
+        # ignore), so the combined code is a different value
         assert error.code in (101183626, 50331786)
         assert error.lib == b.lib.ERR_LIB_EVP
-        assert error.func == b.lib.EVP_F_EVP_ENCRYPTFINAL_EX
         assert error.reason == b.lib.EVP_R_DATA_NOT_MULTIPLE_OF_BLOCK_LENGTH
         assert b"data not multiple of block length" in error.reason_text
 
