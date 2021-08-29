338508b39c12032d2b13b3ba203524dbb089f023
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-08-29 16:37:33 -0500

    convert cryptography_vectors to setup.cfg

--- vectors/cryptography_vectors/__about__.py
+++ vectors/cryptography_vectors/__about__.py
@@ -3,25 +3,7 @@
 # for complete details.
 
 __all__ = [
-    "__title__",
-    "__summary__",
-    "__uri__",
     "__version__",
-    "__author__",
-    "__email__",
-    "__license__",
-    "__copyright__",
 ]
 
-__title__ = "cryptography_vectors"
-__summary__ = "Test vectors for the cryptography package."
-
-__uri__ = "https://github.com/pyca/cryptography"
-
 __version__ = "35.0.0.dev1"
-
-__author__ = "The Python Cryptographic Authority and individual contributors"
-__email__ = "cryptography-dev@python.org"
-
-__license__ = "BSD or Apache License, Version 2.0"
-__copyright__ = "Copyright 2013-2021 %s" % __author__
