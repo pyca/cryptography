efc37b4a23eb51f6ae80c0ac496ab25911c2a714
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-08-29 17:07:54 -0500

    convert cryptography_vectors to setup.cfg (#6243)
    * convert cryptography_vectors to setup.cfg
    
    * put it in the right section

--- vectors/setup.py
+++ vectors/setup.py
@@ -4,27 +4,7 @@
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
 
-import os
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-base_dir = os.path.dirname(__file__)
-
-about = {}
-with open(os.path.join(base_dir, "cryptography_vectors", "__about__.py")) as f:
-    exec(f.read(), about)
-
-
-setup(
-    name=about["__title__"],
-    version=about["__version__"],
-    description=about["__summary__"],
-    license=about["__license__"],
-    url=about["__uri__"],
-    author=about["__author__"],
-    author_email=about["__email__"],
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-)
+setup()
