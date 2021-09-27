92b4c035951f932da3a091a31b8da6c854adcf59
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-09-27 13:12:23 +0800

    remove unneeded bindings

--- src/_cffi_src/openssl/x509name.py
+++ src/_cffi_src/openssl/x509name.py
@@ -32,7 +32,6 @@ void X509_NAME_ENTRY_free(X509_NAME_ENTRY *);
 int X509_NAME_get_index_by_NID(X509_NAME *, int, int);
 int X509_NAME_cmp(const X509_NAME *, const X509_NAME *);
 X509_NAME *X509_NAME_dup(X509_NAME *);
-int X509_NAME_ENTRY_set(X509_NAME_ENTRY *);
 /* These became const X509_NAME * in 1.1.0 */
 int X509_NAME_entry_count(X509_NAME *);
 X509_NAME_ENTRY *X509_NAME_get_entry(X509_NAME *, int);
