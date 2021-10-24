ab0298a1184c8b38cb032a497c6ebf847c5081af
Alex Gaynor <alex.gaynor@gmail.com>
2021-10-24 09:07:56 -0400

    Added a test for signing an OCSP response with an invalid hash

--- tests/x509/test_ocsp.py
+++ tests/x509/test_ocsp.py
@@ -778,6 +778,29 @@ class TestOCSPResponseBuilder(object):
                 ocsp.OCSPResponseStatus.SUCCESSFUL
             )
 
+    def test_sign_unrecognized_hash_algorithm(self):
+        builder = ocsp.OCSPResponseBuilder()
+        cert, issuer = _cert_and_issuer()
+        root_cert, private_key = _generate_root()
+        current_time = datetime.datetime.utcnow().replace(microsecond=0)
+        this_update = current_time - datetime.timedelta(days=1)
+        next_update = this_update + datetime.timedelta(days=7)
+        builder = builder.responder_id(
+            ocsp.OCSPResponderEncoding.NAME, root_cert
+        ).add_response(
+            cert,
+            issuer,
+            hashes.SHA1(),
+            ocsp.OCSPCertStatus.GOOD,
+            this_update,
+            next_update,
+            None,
+            None,
+        )
+
+        with pytest.raises(ValueError):
+            builder.sign(private_key, hashes.BLAKE2b(digest_size=64))
+
 
 class TestSignedCertificateTimestampsExtension(object):
     def test_init(self):
