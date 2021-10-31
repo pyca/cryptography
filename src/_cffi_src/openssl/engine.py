8b3affcca796f1a49c9a655e9cd030d387d79627
Alex Gaynor <alex.gaynor@gmail.com>
2021-10-31 19:17:53 -0400

    Check for BoringSSL in the engine bindings (#6509)

--- src/_cffi_src/openssl/engine.py
+++ src/_cffi_src/openssl/engine.py
@@ -37,6 +37,16 @@ CUSTOMIZATIONS = """
 #ifdef OPENSSL_NO_ENGINE
 static const long Cryptography_HAS_ENGINE = 0;
 
+#if CRYPTOGRAPHY_IS_BORINGSSL
+typedef void UI_METHOD;
+#endif
+
+/* Despite being OPENSSL_NO_ENGINE, BoringSSL defines these symbols. */
+#if !CRYPTOGRAPHY_IS_BORINGSSL
+int (*ENGINE_free)(ENGINE *) = NULL;
+void (*ENGINE_load_builtin_engines)(void) = NULL;
+#endif
+
 ENGINE *(*ENGINE_by_id)(const char *) = NULL;
 int (*ENGINE_init)(ENGINE *) = NULL;
 int (*ENGINE_finish)(ENGINE *) = NULL;
@@ -46,13 +56,11 @@ void (*ENGINE_unregister_RAND)(ENGINE *) = NULL;
 int (*ENGINE_ctrl_cmd)(ENGINE *, const char *, long, void *,
                        void (*)(void), int) = NULL;
 
-int (*ENGINE_free)(ENGINE *) = NULL;
 const char *(*ENGINE_get_id)(const ENGINE *) = NULL;
 const char *(*ENGINE_get_name)(const ENGINE *) = NULL;
 
 int (*ENGINE_ctrl_cmd_string)(ENGINE *, const char *, const char *,
                               int) = NULL;
-void (*ENGINE_load_builtin_engines)(void) = NULL;
 EVP_PKEY *(*ENGINE_load_private_key)(ENGINE *, const char *, UI_METHOD *,
                                      void *) = NULL;
 EVP_PKEY *(*ENGINE_load_public_key)(ENGINE *, const char *,
