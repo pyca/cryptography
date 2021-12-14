a9aa151c928d591fd25e476cd7580922e575636a
messense <messense@icloud.com>
2021-12-14 09:06:21 +0800

    Link against clang runtime on static builds on macOS

--- src/rust/build.rs
+++ src/rust/build.rs
@@ -4,6 +4,21 @@ use std::path::{Path, MAIN_SEPARATOR};
 use std::process::{Command, Stdio};
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
+    let openssl_static = env::var("OPENSSL_STATIC")
+        .map(|x| x == "1")
+        .unwrap_or(false);
+    if target.contains("apple") && openssl_static {
+        // On (older) OSX we need to link against the clang runtime,
+        // which is hidden in some non-default path.
+        //
+        // More details at https://github.com/alexcrichton/curl-rust/issues/279.
+        if let Some(path) = macos_link_search_path() {
+            println!("cargo:rustc-link-lib=clang_rt.osx");
+            println!("cargo:rustc-link-search={}", path);
+        }
+    }
+
     let out_dir = env::var("OUT_DIR").unwrap();
     // FIXME: maybe pyo3-build-config should provide a way to do this?
     let python = env::var("PYO3_PYTHON").unwrap_or_else(|_| "python3".to_string());
@@ -83,4 +98,28 @@ fn run_python_script(interpreter: impl AsRef<Path>, script: &str) -> Result<Stri
             String::from_utf8(ok.stdout).expect("failed to parse Python script output as utf-8")
         ),
     }
+}
+
+fn macos_link_search_path() -> Option<String> {
+    let output = Command::new("clang")
+        .arg("--print-search-dirs")
+        .output()
+        .ok()?;
+    if !output.status.success() {
+        println!(
+            "failed to run 'clang --print-search-dirs', continuing without a link search path"
+        );
+        return None;
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    for line in stdout.lines() {
+        if line.contains("libraries: =") {
+            let path = line.split('=').nth(1)?;
+            return Some(format!("{}/lib/darwin", path));
+        }
+    }
+
+    println!("failed to determine link search path, continuing without it");
+    None
 }
