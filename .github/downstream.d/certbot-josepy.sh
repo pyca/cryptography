e8d4d4cdb0b0a89fbab5e346bd95e5b421e600f7
Adrien Ferrand <ferrand.ad@gmail.com>
2022-04-12 21:30:57 +0200

    Pin josepy to 1.13.0 in downstream tests

--- .github/downstream.d/certbot-josepy.sh
+++ .github/downstream.d/certbot-josepy.sh
@@ -2,7 +2,12 @@
 
 case "${1}" in
     install)
-        git clone --depth=1 https://github.com/certbot/josepy
+        # Josepy is pinned to 1.13.0 because the project is migrating to Poetry, and
+        # this test is not compatible with it yet.
+        #
+        # TODO: Update this test with Poetry once a new release of Josepy includes
+        #       https://github.com/certbot/josepy/pull/129
+        git clone --depth=1 --branch v1.13.0 https://github.com/certbot/josepy
         cd josepy
         git rev-parse HEAD
         pip install -e ".[tests]" -c constraints.txt
