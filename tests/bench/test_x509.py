d3b95f7d765fe0624ab90e33e217e50345dfff8e
Alex Gaynor <alex.gaynor@gmail.com>
2022-04-29 21:52:59 -0600

    Update test_x509.py

--- tests/bench/test_x509.py
+++ tests/bench/test_x509.py
@@ -9,7 +9,7 @@ from cryptography import x509
 def test_object_identier_constructor(benchmark):
     benchmark(x509.ObjectIdentifier, "1.3.6.1.4.1.11129.2.4.5")
 
-    
+
 def test_aki_public_bytes(benchmark):
     aki = x509.AuthorityKeyIdentifier(
         key_identifier=b"\x00" * 16,
