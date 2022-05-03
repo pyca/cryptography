687804338a9fc0ade6cb533e58e7956941bfe826
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-05-03 06:31:44 -0600

    allocate our hash algorithms in a better spot (#7188)
    * allocate our hash algorithms in a better spot
    
    this is basically pointless from a performance perspective, but it's
    better conceptually so why not
    
    * fix typing

--- tests/hazmat/primitives/test_dsa.py
+++ tests/hazmat/primitives/test_dsa.py
@@ -26,12 +26,12 @@ from ...utils import (
     load_vectors_from_file,
 )
 
-_ALGORITHMS_DICT: typing.Dict[str, typing.Type[hashes.HashAlgorithm]] = {
-    "SHA1": hashes.SHA1,
-    "SHA224": hashes.SHA224,
-    "SHA256": hashes.SHA256,
-    "SHA384": hashes.SHA384,
-    "SHA512": hashes.SHA512,
+_ALGORITHMS_DICT: typing.Dict[str, hashes.HashAlgorithm] = {
+    "SHA1": hashes.SHA1(),
+    "SHA224": hashes.SHA224(),
+    "SHA256": hashes.SHA256(),
+    "SHA384": hashes.SHA384(),
+    "SHA512": hashes.SHA512(),
 }
 
 
@@ -397,7 +397,7 @@ class TestDSAVerification:
         for vector in vectors:
             with subtests.test():
                 digest_algorithm = vector["digest_algorithm"].replace("-", "")
-                algorithm = _ALGORITHMS_DICT[digest_algorithm]()
+                algorithm = _ALGORITHMS_DICT[digest_algorithm]
 
                 _skip_if_dsa_not_supported(
                     backend, algorithm, vector["p"], vector["q"], vector["g"]
@@ -466,7 +466,7 @@ class TestDSASignature:
         for vector in vectors:
             with subtests.test():
                 digest_algorithm = vector["digest_algorithm"].replace("-", "")
-                algorithm = _ALGORITHMS_DICT[digest_algorithm]()
+                algorithm = _ALGORITHMS_DICT[digest_algorithm]
 
                 _skip_if_dsa_not_supported(
                     backend, algorithm, vector["p"], vector["q"], vector["g"]
