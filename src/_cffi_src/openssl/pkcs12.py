0be4628ff4d51779151db8d898ac52bcfa938671
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-05-13 06:28:25 +0800

    add binding for PKCS12_set_mac (#7217)
    OpenSSL 3 changed the default MAC to sha256, which is fine and good
    except Windows Server 2016 can't handle that so we need to build some
    APIs allowing worse things and name them scary legacy names.

--- src/_cffi_src/openssl/pkcs12.py
+++ src/_cffi_src/openssl/pkcs12.py
@@ -8,6 +8,8 @@ INCLUDES = """
 """
 
 TYPES = """
+static const long Cryptography_HAS_PKCS12_SET_MAC;
+
 typedef ... PKCS12;
 """
 
@@ -20,7 +22,16 @@ int PKCS12_parse(PKCS12 *, const char *, EVP_PKEY **, X509 **,
                  Cryptography_STACK_OF_X509 **);
 PKCS12 *PKCS12_create(char *, char *, EVP_PKEY *, X509 *,
                       Cryptography_STACK_OF_X509 *, int, int, int, int, int);
+int PKCS12_set_mac(PKCS12 *, const char *, int, unsigned char *, int, int,
+                   const EVP_MD *);
 """
 
 CUSTOMIZATIONS = """
+#if CRYPTOGRAPHY_IS_BORINGSSL
+static const long Cryptography_HAS_PKCS12_SET_MAC = 0;
+int (*PKCS12_set_mac)(PKCS12 *, const char *, int, unsigned char *, int, int,
+                      const EVP_MD *) = NULL;
+#else
+static const long Cryptography_HAS_PKCS12_SET_MAC = 1;
+#endif
 """
