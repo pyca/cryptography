2adfeb56841dbd8d991e28ab23fe9af9fef3e7d5
Alexandre Machado <axmachado@gmail.com>
2022-08-16 16:57:20 -0300

    Fix issue #7513 - parsing of RFC4514 strings should reverse the order of RDNs (#7514)
    * Fix issue #7513 - parsing of RFC4514 strings should reverse the order of RDNs
    
    * Solving code formating issues
    
    * Adding comment justifing the reversal of rdns on the RFC4514 parser
    
    * Comment changes on CHANGELOG.rst
    
    * fixing a typo on method link at CHANGELOG.rst
    
    * Removing reference to protected class _RFC4514NameParser from CHANGELOG.rst
    
    Co-authored-by: Alexandre Machado <alexandre@softnex.com.br>

--- src/cryptography/x509/name.py
+++ src/cryptography/x509/name.py
@@ -414,13 +414,21 @@ class _RFC4514NameParser:
         return val
 
     def parse(self) -> Name:
+        """
+        Parses the `data` string and converts it to a Name.
+
+        According to RFC4514 section 2.1 the RDNSequence must be
+        reversed when converting to string representation. So, when
+        we parse it, we need to reverse again to get the RDNs on the
+        correct order.
+        """
         rdns = [self._parse_rdn()]
 
         while self._has_data():
             self._read_char(",")
             rdns.append(self._parse_rdn())
 
-        return Name(rdns)
+        return Name(reversed(rdns))
 
     def _parse_rdn(self) -> RelativeDistinguishedName:
         nas = [self._parse_na()]
