fb6014eb1e7cd3fabb5c1ccf48f92627a4ab874a
Alex Gaynor <alex.gaynor@gmail.com>
2022-08-23 07:03:51 -0400

    try with glyph's branch

--- .github/downstream.d/twisted.sh
+++ .github/downstream.d/twisted.sh
@@ -2,7 +2,7 @@
 
 case "${1}" in
     install)
-        git clone --depth=1 https://github.com/twisted/twisted
+        git clone --depth=1 -b 10275-cryptography-latest https://github.com/twisted/twisted
         cd twisted
         git rev-parse HEAD
         pip install ".[all_non_platform]"
