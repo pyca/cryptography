23143c035cc289f4755da67e90bc991b4d62ab08
Alex Gaynor <alex.gaynor@gmail.com>
2022-08-31 17:49:33 -0400

    pin to older poetry

--- .github/downstream.d/certbot-josepy.sh
+++ .github/downstream.d/certbot-josepy.sh
@@ -5,7 +5,7 @@ case "${1}" in
         git clone --depth=1 https://github.com/certbot/josepy
         cd josepy
         git rev-parse HEAD
-        curl -sSL https://install.python-poetry.org | python3 -
+        curl -sSL https://install.python-poetry.org | python3 - --version=1.1.15
         "${HOME}/.local/bin/poetry" export -f requirements.txt --dev --without-hashes -o constraints.txt
         pip install -e . pytest -c constraints.txt
         ;;
