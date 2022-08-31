f9cf1cff6247a1144042cba3bdbba7e0db9d546e
Alex Gaynor <alex.gaynor@gmail.com>
2022-08-30 22:04:12 -0400

    Add a benchmark of loading a certificate (#7551)

--- tests/bench/test_x509.py
+++ tests/bench/test_x509.py
@@ -3,7 +3,11 @@
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
 
+import os
+
 from cryptography import x509
+
+from ..utils import load_vectors_from_file
 
 
 def test_object_identier_constructor(benchmark):
@@ -17,3 +21,13 @@ def test_aki_public_bytes(benchmark):
         authority_cert_serial_number=None,
     )
     benchmark(aki.public_bytes)
+
+
+def test_load_pem_certificate(benchmark):
+    cert_bytes = load_vectors_from_file(
+        os.path.join("x509", "cryptography.io.pem"),
+        loader=lambda pemfile: pemfile.read(),
+        mode="rb",
+    )
+
+    benchmark(x509.load_pem_x509_certificate, cert_bytes)
