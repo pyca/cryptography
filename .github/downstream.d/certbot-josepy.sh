163f60be99aef7c392db437d961d28d49ef76854
Alex Gaynor <alex.gaynor@gmail.com>
2022-10-09 15:38:13 -0500

    fixes #7554 -- unpin poetry in certbot-josepy downstream tests (#7684)

--- .github/downstream.d/certbot-josepy.sh
+++ .github/downstream.d/certbot-josepy.sh
@@ -5,8 +5,8 @@ case "${1}" in
         git clone --depth=1 https://github.com/certbot/josepy
         cd josepy
         git rev-parse HEAD
-        curl -sSL https://install.python-poetry.org | python3 - --version=1.1.15
-        "${HOME}/.local/bin/poetry" export -f requirements.txt --dev --without-hashes -o constraints.txt
+        curl -sSL https://install.python-poetry.org | python3 -
+        "${HOME}/.local/bin/poetry" export -f constraints.txt --dev --without-hashes -o constraints.txt
         pip install -e . pytest -c constraints.txt
         ;;
     run)
