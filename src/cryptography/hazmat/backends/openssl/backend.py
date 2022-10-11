14f2c02a950ad8d98b795b497c95ea72725381e1
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-10-11 12:09:36 -0400

    flake

--- src/cryptography/hazmat/backends/openssl/backend.py
+++ src/cryptography/hazmat/backends/openssl/backend.py
@@ -2204,7 +2204,7 @@ class Backend:
             res = self._lib.PKCS12_parse(
                 p12, password_buf, evp_pkey_ptr, x509_ptr, sk_x509_ptr
             )
-        # OpenSSL 3.0.6 leaves errors on the stack even in success, so 
+        # OpenSSL 3.0.6 leaves errors on the stack even in success, so
         # we consume all errors unconditionally.
         self._consume_errors()
         if res == 0:
