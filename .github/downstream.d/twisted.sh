e73561477d47ffe619c28b440749e4a1bf2d827b
Alex Gaynor <alex.gaynor@gmail.com>
2022-10-25 18:43:26 -0400

    Update twisted.sh

--- .github/downstream.d/twisted.sh
+++ .github/downstream.d/twisted.sh
@@ -5,7 +5,7 @@ case "${1}" in
         git clone --depth=1 https://github.com/twisted/twisted
         cd twisted
         git rev-parse HEAD
-        pip install ".[all_non_platform]"
+        pip install ".[all-non-platform]"
         ;;
     run)
         cd twisted
