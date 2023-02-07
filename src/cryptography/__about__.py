c586659188f1ab0a11e6587074eb095340680805
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-02-07 11:57:33 -0600

    bump versions

--- src/cryptography/__about__.py
+++ src/cryptography/__about__.py
@@ -9,7 +9,7 @@ __all__ = [
     "__copyright__",
 ]
 
-__version__ = "39.0.0"
+__version__ = "39.0.1"
 
 __author__ = "The Python Cryptographic Authority and individual contributors"
 __copyright__ = "Copyright 2013-2022 {}".format(__author__)
