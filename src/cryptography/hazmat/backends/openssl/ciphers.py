9fbf84efc861668755ab645530ec7be9cf3c6696
Alex Gaynor <alex.gaynor@gmail.com>
2023-02-07 11:34:18 -0500

    Don't allow update_into to mutate immutable objects (#8230)

--- src/cryptography/hazmat/backends/openssl/ciphers.py
+++ src/cryptography/hazmat/backends/openssl/ciphers.py
@@ -156,7 +156,7 @@ class _CipherContext:
         data_processed = 0
         total_out = 0
         outlen = self._backend._ffi.new("int *")
-        baseoutbuf = self._backend._ffi.from_buffer(buf)
+        baseoutbuf = self._backend._ffi.from_buffer(buf, require_writable=True)
         baseinbuf = self._backend._ffi.from_buffer(data)
 
         while data_processed != total_data_len:
