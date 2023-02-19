848f11df2b7b7a9708f51b62080950bd1ce6af50
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-02-19 15:11:30 -0600

    remove some error bindings

--- src/_cffi_src/openssl/err.py
+++ src/_cffi_src/openssl/err.py
@@ -15,13 +15,10 @@ static const int EVP_R_DATA_NOT_MULTIPLE_OF_BLOCK_LENGTH;
 static const int EVP_R_BAD_DECRYPT;
 static const int EVP_R_UNSUPPORTED_PRIVATE_KEY_ALGORITHM;
 static const int PKCS12_R_PKCS12_CIPHERFINAL_ERROR;
-static const int PEM_R_UNSUPPORTED_ENCRYPTION;
 static const int EVP_R_XTS_DUPLICATED_KEYS;
 
 static const int ERR_LIB_EVP;
-static const int ERR_LIB_PEM;
 static const int ERR_LIB_PROV;
-static const int ERR_LIB_ASN1;
 static const int ERR_LIB_PKCS12;
 
 static const int SSL_TLSEXT_ERR_OK;
