9b0133c4dcb0282124745e211d497ed025a91045
Alex Gaynor <alex.gaynor@gmail.com>
2023-04-24 09:18:51 -0600

    Remove clippy ignore that's no longer required (#8808)

--- src/rust/src/backend/x25519.rs
+++ src/rust/src/backend/x25519.rs
@@ -108,7 +108,7 @@ impl X25519PrivateKey {
     ) -> CryptographyResult<&'p pyo3::types::PyBytes> {
         utils::pkey_private_bytes(
             py,
-            &*slf,
+            slf,
             &slf.borrow().pkey,
             encoding,
             format,
@@ -134,7 +134,7 @@ impl X25519PublicKey {
         encoding: &pyo3::PyAny,
         format: &pyo3::PyAny,
     ) -> CryptographyResult<&'p pyo3::types::PyBytes> {
-        utils::pkey_public_bytes(py, &*slf, &slf.borrow().pkey, encoding, format, false)
+        utils::pkey_public_bytes(py, slf, &slf.borrow().pkey, encoding, format, false)
     }
 
     fn __richcmp__(
