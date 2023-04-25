bb91e8b44950e909c6cfa56b9339ccbcc6c66bfe
Alex Gaynor <alex.gaynor@gmail.com>
2023-04-25 07:23:57 -0600

    See if we can always enable abi3 (#8823)
    Previously it wasn't because pypy doesn't support abi3, but maybe the pyo3 feature works.

--- setup.py
+++ setup.py
@@ -54,12 +54,6 @@ try:
                 "cryptography.hazmat.bindings._rust",
                 "src/rust/Cargo.toml",
                 py_limited_api=True,
-                # Enable abi3 mode if we're not using PyPy.
-                features=(
-                    []
-                    if platform.python_implementation() == "PyPy"
-                    else ["pyo3/abi3-py37"]
-                ),
                 rust_version=">=1.56.0",
             )
         ],
