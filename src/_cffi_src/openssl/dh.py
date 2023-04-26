bda9bc52332ae9a4e1c5a72fec97964e44ad202a
Alex Gaynor <alex.gaynor@gmail.com>
2023-04-25 23:16:21 -0400

    Remove a bunch of unused bindings
    Also replace one DH function with a simpler implementation

--- src/_cffi_src/openssl/dh.py
+++ src/_cffi_src/openssl/dh.py
@@ -15,29 +15,8 @@ const long DH_NOT_SUITABLE_GENERATOR;
 """
 
 FUNCTIONS = """
-DH *DH_new(void);
 void DH_free(DH *);
-int DH_size(const DH *);
-int DH_generate_key(DH *);
-DH *DHparams_dup(DH *);
-
-void DH_get0_pqg(const DH *, const BIGNUM **, const BIGNUM **,
-                 const BIGNUM **);
-int DH_set0_pqg(DH *, BIGNUM *, BIGNUM *, BIGNUM *);
-void DH_get0_key(const DH *, const BIGNUM **, const BIGNUM **);
-int DH_set0_key(DH *, BIGNUM *, BIGNUM *);
-
-int DH_check(const DH *, int *);
-int DH_generate_parameters_ex(DH *, int, int, BN_GENCB *);
-DH *d2i_DHparams_bio(BIO *, DH **);
-int i2d_DHparams_bio(BIO *, DH *);
-DH *d2i_DHxparams_bio(BIO *, DH **);
-int i2d_DHxparams_bio(BIO *, DH *);
 """
 
 CUSTOMIZATIONS = """
-#if !(defined(EVP_PKEY_DHX) && EVP_PKEY_DHX != -1)
-DH *(*d2i_DHxparams_bio)(BIO *bp, DH **x) = NULL;
-int (*i2d_DHxparams_bio)(BIO *bp, DH *x) = NULL;
-#endif
 """
