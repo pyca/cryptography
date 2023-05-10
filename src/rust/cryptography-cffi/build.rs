c28a6028ee3ccc9eceb2e63b6b73de489d4c3e0a
Alex Gaynor <alex.gaynor@gmail.com>
2023-05-09 20:11:40 -0400

    Added a missing rerun-if stanza

--- src/rust/cryptography-cffi/build.rs
+++ src/rust/cryptography-cffi/build.rs
@@ -25,6 +25,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     // FIXME: maybe pyo3-build-config should provide a way to do this?
     let python = env::var("PYO3_PYTHON").unwrap_or_else(|_| "python3".to_string());
+    println!("cargo:rerun-if-env-changed=PYO3_PYTHON");
     println!("cargo:rerun-if-changed=../../_cffi_src/");
     let output = Command::new(&python)
         .env("OUT_DIR", &out_dir)
