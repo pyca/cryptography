ba3437c2e537dd058cd66bb5758009138da9254b
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-05-11 08:17:41 +0800

    test improvements

--- tests/x509/test_x509.py
+++ tests/x509/test_x509.py
@@ -765,6 +765,21 @@ class TestRSAPSSCertificate:
             cert.signature_hash_algorithm,
         )
 
+    def test_load_pss_sha1_mgf1_sha1(self, backend):
+        cert = _load_cert(
+            os.path.join("x509", "ee-pss-sha1-cert.pem"),
+            x509.load_pem_x509_certificate,
+        )
+        assert isinstance(cert, x509.Certificate)
+        pub_key = cert.public_key()
+        assert isinstance(pub_key, rsa.RSAPublicKey)
+        pss = cert.signature_algorithm_parameters
+        assert isinstance(pss, padding.PSS)
+        assert isinstance(pss._mgf, padding.MGF1)
+        assert isinstance(pss._mgf._algorithm, hashes.SHA1)
+        assert pss._salt_length == 20
+        assert isinstance(cert.signature_hash_algorithm, hashes.SHA1)
+
     def test_invalid_mgf(self, backend):
         cert = _load_cert(
             os.path.join("x509", "custom", "rsa_pss_cert_invalid_mgf.der"),
@@ -2404,7 +2419,16 @@ class TestCertificateBuilder:
         # GENERALIZED TIME
         assert parsed.not_after_tag == 0x18
 
-    def test_sign_pss(self, rsa_key_2048: rsa.RSAPrivateKey, backend):
+    @pytest.mark.parametrize(
+        ("alg", "mgf_alg"),
+        [
+            (hashes.SHA512(), hashes.SHA256()),
+            (hashes.SHA3_512(), hashes.SHA3_256()),
+        ],
+    )
+    def test_sign_pss(
+        self, rsa_key_2048: rsa.RSAPrivateKey, alg, mgf_alg, backend
+    ):
         builder = (
             x509.CertificateBuilder()
             .subject_name(
@@ -2418,20 +2442,23 @@ class TestCertificateBuilder:
             .not_valid_before(datetime.datetime(2020, 1, 1))
             .not_valid_after(datetime.datetime(2038, 1, 1))
         )
-        pss = padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=32)
-        cert = builder.sign(rsa_key_2048, hashes.SHA256(), rsa_padding=pss)
+        pss = padding.PSS(
+            mgf=padding.MGF1(mgf_alg), salt_length=alg.digest_size
+        )
+        cert = builder.sign(rsa_key_2048, alg, rsa_padding=pss)
         pk = cert.public_key()
         assert isinstance(pk, rsa.RSAPublicKey)
+        assert isinstance(cert.signature_hash_algorithm, type(alg))
         cert_params = cert.signature_algorithm_parameters
         assert isinstance(cert_params, padding.PSS)
         assert cert_params._salt_length == pss._salt_length
         assert isinstance(cert_params._mgf, padding.MGF1)
-        assert isinstance(cert_params._mgf._algorithm, hashes.SHA256)
+        assert isinstance(cert_params._mgf._algorithm, type(mgf_alg))
         pk.verify(
             cert.signature,
             cert.tbs_certificate_bytes,
             cert_params,
-            hashes.SHA256(),
+            alg,
         )
 
     @pytest.mark.parametrize(
