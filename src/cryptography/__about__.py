f34e8e16bf5281d93c5b7cfc5dfb88e35563fe39
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-08-02 07:17:46 +1200

    bump for 41.0.3

--- src/cryptography/__about__.py
+++ src/cryptography/__about__.py
@@ -10,7 +10,7 @@ __all__ = [
     "__copyright__",
 ]
 
-__version__ = "41.0.2"
+__version__ = "41.0.3"
 
 
 __author__ = "The Python Cryptographic Authority and individual contributors"
--- vectors/cryptography_vectors/__about__.py
+++ vectors/cryptography_vectors/__about__.py
@@ -6,4 +6,4 @@ __all__ = [
     "__version__",
 ]
 
-__version__ = "41.0.2"
+__version__ = "41.0.3"
