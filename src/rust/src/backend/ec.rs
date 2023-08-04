29d6afebe604f8e04a871065f878fe702f86bac3
mr-m0nst3r <songxinlei@gmail.com>
2023-08-04 20:28:08 +0800

    add sm2 to ec.rs

--- src/rust/src/backend/ec.rs
+++ src/rust/src/backend/ec.rs
@@ -49,6 +49,8 @@ fn curve_from_py_curve(
         "sect283k1" => openssl::nid::Nid::SECT283K1,
         "sect409k1" => openssl::nid::Nid::SECT409K1,
         "sect571k1" => openssl::nid::Nid::SECT571K1,
+        "sm2" => openssl::nid::Nid::SM2,
+        
 
         #[cfg(not(any(CRYPTOGRAPHY_IS_LIBRESSL, CRYPTOGRAPHY_IS_BORINGSSL)))]
         "brainpoolP256r1" => openssl::nid::Nid::BRAINPOOL_P256R1,
