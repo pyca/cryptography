9d30d37292c1c26d339b714f9c5fbb365b8c8571
William Woodruff <william@trailofbits.com>
2023-08-14 11:18:11 +0200

    fix hints, add initial store tests
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ tests/x509/test_verification.py
@@ -0,0 +1,24 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+import os
+
+import pytest
+
+from cryptography import x509
+from cryptography.x509.verification import Store
+from tests.x509.test_x509 import _load_cert
+
+
+class TestStore:
+    def test_store_rejects_non_certificates(self):
+        with pytest.raises(TypeError):
+            Store(["not a cert"])  # type: ignore[list-item]
+
+    def test_store_initializes(self):
+        cert = _load_cert(
+            os.path.join("x509", "cryptography.io.pem"),
+            x509.load_pem_x509_certificate,
+        )
+        assert Store([cert]) is not None
