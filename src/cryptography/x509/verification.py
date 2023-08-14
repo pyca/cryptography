357ad5071a3b7c749261a11da88b8c3260598704
William Woodruff <william@trailofbits.com>
2023-08-14 11:01:08 +0200

    Add x509.verification.Store
    One of many sub-breakouts.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ src/cryptography/x509/verification.py
@@ -0,0 +1,7 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+from cryptography.hazmat.bindings._rust import x509 as rust_x509
+
+Store = rust_x509.Store
---
+++ src/rust/src/x509/verify.rs
@@ -0,0 +1,31 @@
+// This file is dual licensed under the terms of the Apache License, Version
+// 2.0, and the BSD License. See the LICENSE file in the root of this repository
+// for complete details.
+
+use pyo3::IntoPy;
+
+use crate::x509::certificate::Certificate as PyCertificate;
+
+#[pyo3::pyclass(name = "Store", module = "cryptography.hazmat.bindings._rust.x509")]
+struct PyStore(pyo3::Py<pyo3::types::PyList>);
+
+#[pyo3::pymethods]
+impl PyStore {
+    #[new]
+    fn new<'p>(py: pyo3::Python<'p>, certs: &'p pyo3::types::PyList) -> pyo3::PyResult<Self> {
+        for cert in certs.iter() {
+            if !cert.is_instance_of::<PyCertificate>() {
+                return Err(pyo3::exceptions::PyTypeError::new_err(
+                    "cannot initialize store with non-certificate member",
+                ));
+            }
+        }
+        Ok(Self(certs.into_py(py)))
+    }
+}
+
+pub(crate) fn add_to_module(module: &pyo3::prelude::PyModule) -> pyo3::PyResult<()> {
+    module.add_class::<PyStore>()?;
+
+    Ok(())
+}
