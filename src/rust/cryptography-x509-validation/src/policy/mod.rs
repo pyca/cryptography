d911a6ba09ffb2106d4e53e57ecd7f1335b6f476
William Woodruff <william@trailofbits.com>
2023-09-06 15:41:28 -0400

    policy: begin fixing things
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/rust/cryptography-x509-validation/src/policy/mod.rs
+++ src/rust/cryptography-x509-validation/src/policy/mod.rs
@@ -17,28 +17,28 @@ static WEBPKI_PERMITTED_ALGORITHMS: Lazy<HashSet<AlgorithmIdentifier<'_>>> = Laz
         // RSASSA‐PKCS1‐v1_5 with SHA‐256
         AlgorithmIdentifier {
             oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::RsaWithSha256(None),
+            params: AlgorithmParameters::RsaWithSha256(Some(())),
         },
         // RSASSA‐PKCS1‐v1_5 with SHA‐384
         AlgorithmIdentifier {
             oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::RsaWithSha384(None),
+            params: AlgorithmParameters::RsaWithSha384(Some(())),
         },
         // RSASSA‐PKCS1‐v1_5 with SHA‐512
         AlgorithmIdentifier {
             oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::RsaWithSha512(None),
+            params: AlgorithmParameters::RsaWithSha512(Some(())),
         },
-        // // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
-        // AlgorithmIdentifier {
-        //     oid: asn1::DefinedByMarker::marker(),
-        //     params: AlgorithmParameters::RsaPss(Some(Box::new(RsaPssParameters {
-        //         hash_algorithm: PSS_SHA256_HASH_ALG,
-        //         mask_gen_algorithm: PSS_SHA256_MASK_GEN_ALG,
-        //         salt_length: 32,
-        //         _trailer_field: Default::default(),
-        //     }))),
-        // },
+        // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
+        AlgorithmIdentifier {
+            oid: asn1::DefinedByMarker::marker(),
+            params: AlgorithmParameters::RsaPss(Some(Box::new(RsaPssParameters {
+                hash_algorithm: PSS_SHA256_HASH_ALG,
+                mask_gen_algorithm: PSS_SHA256_MASK_GEN_ALG,
+                salt_length: 32,
+                _trailer_field: Default::default(),
+            }))),
+        },
         // // RSASSA‐PSS with SHA‐384, MGF‐1 with SHA‐384, and a salt length of 48 bytes
         // AlgorithmIdentifier {
         //     oid: asn1::DefinedByMarker::marker(),
@@ -81,17 +81,25 @@ static WEBPKI_PERMITTED_ALGORITHMS: Lazy<HashSet<AlgorithmIdentifier<'_>>> = Laz
 mod tests {
     use super::WEBPKI_PERMITTED_ALGORITHMS;
 
+    fn decode_hex(s: &str) -> Vec<u8> {
+        (0..s.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap()
+    }
+
     #[test]
     fn test_webpki_permitted_algorithms_canonical_encodings() {
         let mut expected_encodings: Vec<_> = vec![
             // RSASSA‐PKCS1‐v1_5 with SHA‐256
-            b"0\r\x06\t*\x86H\x86\xf7\r\x01\x01\x0b\x05\x00".to_vec(),
+            decode_hex("300d06092a864886f70d01010b0500"),
             // RSASSA‐PKCS1‐v1_5 with SHA‐384
-            b"0\r\x06\t*\x86H\x86\xf7\r\x01\x01\x0c\x05\x00".to_vec(),
+            decode_hex("300d06092a864886f70d01010c0500"),
             // RSASSA‐PKCS1‐v1_5 with SHA‐512
-            b"0\r\x06\t*\x86H\x86\xf7\r\x01\x01\r\x05\x00".to_vec(),
-            // // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
-            // b"0A\x06\t*\x86H\x86\xf7\r\x01\x01\n04\xa0\x0f0\r\x06\t`\x86H\x01e\x03\x04\x02\x01\x05\x00\xa1\x1c0\x1a\x06\t*\x86H\x86\xf7\r\x01\x01\x080\r\x06\t`\x86H\x01e\x03\x04\x02\x01\x05\x00\xa2\x03\x02\x01 ".to_vec(),
+            decode_hex("300d06092a864886f70d01010d0500"),
+            // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
+            decode_hex("304106092a864886f70d01010a3034a00f300d06096086480165030402010500a11c301a06092a864886f70d010108300d06096086480165030402010500a203020120"),
             // // RSASSA‐PSS with SHA‐384, MGF‐1 with SHA‐384, and a salt length of 48 bytes
             // b"0A\x06\t*\x86H\x86\xf7\r\x01\x01\n04\xa0\x0f0\r\x06\t`\x86H\x01e\x03\x04\x02\x02\x05\x00\xa1\x1c0\x1a\x06\t*\x86H\x86\xf7\r\x01\x01\x080\r\x06\t`\x86H\x01e\x03\x04\x02\x02\x05\x00\xa2\x03\x02\x010".to_vec(),
             // // RSASSA‐PSS with SHA‐512, MGF‐1 with SHA‐512, and a salt length of 64 bytes
