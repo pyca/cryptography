f4d4f845d07b3110398fa26fe6ee744ac0b09707
William Woodruff <william@trailofbits.com>
2023-09-06 15:35:24 -0400

    mod: isolate failures to just RSA encodings for now
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/rust/cryptography-x509-validation/src/policy/mod.rs
+++ src/rust/cryptography-x509-validation/src/policy/mod.rs
@@ -29,16 +29,16 @@ static WEBPKI_PERMITTED_ALGORITHMS: Lazy<HashSet<AlgorithmIdentifier<'_>>> = Laz
             oid: asn1::DefinedByMarker::marker(),
             params: AlgorithmParameters::RsaWithSha512(None),
         },
-        // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
-        AlgorithmIdentifier {
-            oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::RsaPss(Some(Box::new(RsaPssParameters {
-                hash_algorithm: PSS_SHA256_HASH_ALG,
-                mask_gen_algorithm: PSS_SHA256_MASK_GEN_ALG,
-                salt_length: 32,
-                _trailer_field: Default::default(),
-            }))),
-        },
+        // // RSASSA‐PSS with SHA‐256, MGF‐1 with SHA‐256, and a salt length of 32 bytes
+        // AlgorithmIdentifier {
+        //     oid: asn1::DefinedByMarker::marker(),
+        //     params: AlgorithmParameters::RsaPss(Some(Box::new(RsaPssParameters {
+        //         hash_algorithm: PSS_SHA256_HASH_ALG,
+        //         mask_gen_algorithm: PSS_SHA256_MASK_GEN_ALG,
+        //         salt_length: 32,
+        //         _trailer_field: Default::default(),
+        //     }))),
+        // },
         // // RSASSA‐PSS with SHA‐384, MGF‐1 with SHA‐384, and a salt length of 48 bytes
         // AlgorithmIdentifier {
         //     oid: asn1::DefinedByMarker::marker(),
@@ -59,28 +59,26 @@ static WEBPKI_PERMITTED_ALGORITHMS: Lazy<HashSet<AlgorithmIdentifier<'_>>> = Laz
         //         _trailer_field: Default::default(),
         //     }))),
         // },
-        // For P-256: the signature MUST use ECDSA with SHA‐256
-        AlgorithmIdentifier {
-            oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::EcDsaWithSha256(None),
-        },
-        // For P-384: the signature MUST use ECDSA with SHA‐384
-        AlgorithmIdentifier {
-            oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::EcDsaWithSha384(None),
-        },
-        // For P-521: the signature MUST use ECDSA with SHA‐512
-        AlgorithmIdentifier {
-            oid: asn1::DefinedByMarker::marker(),
-            params: AlgorithmParameters::EcDsaWithSha512(None),
-        },
+        // // For P-256: the signature MUST use ECDSA with SHA‐256
+        // AlgorithmIdentifier {
+        //     oid: asn1::DefinedByMarker::marker(),
+        //     params: AlgorithmParameters::EcDsaWithSha256(None),
+        // },
+        // // For P-384: the signature MUST use ECDSA with SHA‐384
+        // AlgorithmIdentifier {
+        //     oid: asn1::DefinedByMarker::marker(),
+        //     params: AlgorithmParameters::EcDsaWithSha384(None),
+        // },
+        // // For P-521: the signature MUST use ECDSA with SHA‐512
+        // AlgorithmIdentifier {
+        //     oid: asn1::DefinedByMarker::marker(),
+        //     params: AlgorithmParameters::EcDsaWithSha512(None),
+        // },
     ])
 });
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
-
     use super::WEBPKI_PERMITTED_ALGORITHMS;
 
     #[test]
@@ -98,12 +96,12 @@ mod tests {
             // b"0A\x06\t*\x86H\x86\xf7\r\x01\x01\n04\xa0\x0f0\r\x06\t`\x86H\x01e\x03\x04\x02\x02\x05\x00\xa1\x1c0\x1a\x06\t*\x86H\x86\xf7\r\x01\x01\x080\r\x06\t`\x86H\x01e\x03\x04\x02\x02\x05\x00\xa2\x03\x02\x010".to_vec(),
             // // RSASSA‐PSS with SHA‐512, MGF‐1 with SHA‐512, and a salt length of 64 bytes
             // b"0A\x06\t*\x86H\x86\xf7\r\x01\x01\n04\xa0\x0f0\r\x06\t`\x86H\x01e\x03\x04\x02\x03\x05\x00\xa1\x1c0\x1a\x06\t*\x86H\x86\xf7\r\x01\x01\x080\r\x06\t`\x86H\x01e\x03\x04\x02\x03\x05\x00\xa2\x03\x02\x01@".to_vec(),
-            // ECDSA: P-256 with SHA256
-            b"0\n\x06\x08*\x86H\xce=\x04\x03\x02".to_vec(),
-            // ECDSA: P-384 with SHA384
-            b"0\n\x06\x08*\x86H\xce=\x04\x03\x03".to_vec(),
-            // ECDSA: P-521 with SHA512
-            b"0\n\x06\x08*\x86H\xce=\x04\x03\x04".to_vec(),
+            // // ECDSA: P-256 with SHA256
+            // b"0\n\x06\x08*\x86H\xce=\x04\x03\x02".to_vec(),
+            // // ECDSA: P-384 with SHA384
+            // b"0\n\x06\x08*\x86H\xce=\x04\x03\x03".to_vec(),
+            // // ECDSA: P-521 with SHA512
+            // b"0\n\x06\x08*\x86H\xce=\x04\x03\x04".to_vec(),
         ];
         expected_encodings.sort();
 
@@ -115,9 +113,10 @@ mod tests {
 
         for (exp, act) in expected_encodings
             .iter()
-            .zip(actual_encodings.iter().take(4))
+            .zip(actual_encodings.iter())
+            .take(3)
         {
-            assert_eq!(exp, act);
+            assert_eq!(act, exp);
         }
     }
 }
