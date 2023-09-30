82bd0aa41b14410b4a079b6748089b61ca3043f3
Alex Gaynor <alex.gaynor@gmail.com>
2023-09-30 14:13:13 -0400

    Remove unused bignum functions and bindings

--- src/cryptography/hazmat/backends/openssl/backend.py
+++ src/cryptography/hazmat/backends/openssl/backend.py
@@ -325,30 +325,6 @@ class Backend:
     def _consume_errors(self) -> list[rust_openssl.OpenSSLError]:
         return rust_openssl.capture_error_stack()
 
-    def _bn_to_int(self, bn) -> int:
-        assert bn != self._ffi.NULL
-        self.openssl_assert(not self._lib.BN_is_negative(bn))
-
-        bn_num_bytes = self._lib.BN_num_bytes(bn)
-        bin_ptr = self._ffi.new("unsigned char[]", bn_num_bytes)
-        bin_len = self._lib.BN_bn2bin(bn, bin_ptr)
-        # A zero length means the BN has value 0
-        self.openssl_assert(bin_len >= 0)
-        val = int.from_bytes(self._ffi.buffer(bin_ptr)[:bin_len], "big")
-        return val
-
-    def _int_to_bn(self, num: int):
-        """
-        Converts a python integer to a BIGNUM. The returned BIGNUM will not
-        be garbage collected (to support adding them to structs that take
-        ownership of the object). Be sure to register it for GC if it will
-        be discarded after use.
-        """
-        binary = num.to_bytes(int(num.bit_length() / 8.0 + 1), "big")
-        bn_ptr = self._lib.BN_bin2bn(binary, len(binary), self._ffi.NULL)
-        self.openssl_assert(bn_ptr != self._ffi.NULL)
-        return bn_ptr
-
     def generate_rsa_private_key(
         self, public_exponent: int, key_size: int
     ) -> rsa.RSAPrivateKey:
