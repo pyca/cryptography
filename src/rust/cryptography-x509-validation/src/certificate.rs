2d1055f0d4af5e744592ea797c61831540d78433
Alex Cameron <asc@tetsuo.sh>
2023-10-25 17:02:57 +1100

    rust: Add unit test for certification validation helpers

--- src/rust/cryptography-x509-validation/src/certificate.rs
+++ src/rust/cryptography-x509-validation/src/certificate.rs
@@ -8,13 +8,32 @@ use cryptography_x509::certificate::Certificate;
 
 use crate::ops::CryptoOps;
 
+#[allow(dead_code)]
 pub(crate) fn cert_is_self_issued(cert: &Certificate<'_>) -> bool {
     cert.issuer() == cert.subject()
 }
 
+#[allow(dead_code)]
 pub(crate) fn cert_is_self_signed<B: CryptoOps>(cert: &Certificate<'_>, ops: &B) -> bool {
     match ops.public_key(cert) {
         Ok(pk) => cert_is_self_issued(cert) && ops.verify_signed_by(cert, pk).is_ok(),
         Err(_) => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ops::tests::{cert, v1_cert_pem, NullOps};
+
+    use super::{cert_is_self_issued, cert_is_self_signed};
+
+    #[test]
+    fn test_certificate_validation_helpers() {
+        let cert_pem = v1_cert_pem();
+        let cert = cert(&cert_pem);
+        let ops = NullOps {};
+
+        assert!(!cert_is_self_issued(&cert));
+        assert!(!cert_is_self_signed(&cert, &ops));
     }
 }
