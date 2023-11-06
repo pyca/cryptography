6c702572b5bcc9f5cb65c4ce8ddc4e4ea2334cc6
Alex Gaynor <alex.gaynor@gmail.com>
2023-11-06 08:12:03 -0500

    Temporarily allow a new clippy warning (#9835)
    * Temporarily allow a new clippy warning
    
    * Update lib.rs
    
    * Update lib.rs

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -3,6 +3,8 @@
 // for complete details.
 
 #![deny(rust_2018_idioms, clippy::undocumented_unsafe_blocks)]
+// Work-around for https://github.com/PyO3/pyo3/issues/3561
+#![allow(unknown_lints, clippy::unnecessary_fallible_conversions)]
 
 mod asn1;
 mod backend;
