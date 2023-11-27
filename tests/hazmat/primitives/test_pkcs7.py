b8718465045270bd12cdeff1222ae81f48186e36
Alex Gaynor <alex.gaynor@gmail.com>
2023-11-27 13:24:09 -0500

    Need to accept this to skip test

--- tests/hazmat/primitives/test_pkcs7.py
+++ tests/hazmat/primitives/test_pkcs7.py
@@ -89,7 +89,7 @@ class TestPKCS7Loading:
                 mode="rb",
             )
 
-    def test_load_pkcs7_empty_certificates(self):
+    def test_load_pkcs7_empty_certificates(self, backend):
         der = b"\x30\x0B\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x07\x02"
 
         certificates = pkcs7.load_der_pkcs7_certificates(der)
