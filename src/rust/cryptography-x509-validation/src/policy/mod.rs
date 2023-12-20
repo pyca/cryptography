52448846d177d4580a1c5d771f9de639d421bc3e
William Woodruff <william@yossarian.net>
2023-12-20 16:29:40 -0500

    Update src/rust/cryptography-x509-validation/src/policy/mod.rs
    Co-authored-by: Alex Gaynor <alex.gaynor@gmail.com>

--- src/rust/cryptography-x509-validation/src/policy/mod.rs
+++ src/rust/cryptography-x509-validation/src/policy/mod.rs
@@ -476,7 +476,7 @@ impl<'a, B: CryptoOps> Policy<'a, B> {
 
             if bc
                 .path_length
-                .map_or(false, |len| current_depth as u64 > len)
+                .map_or(false, |len| current_depth.into() > len)
             {
                 return Err(ValidationError::Other(
                     "path length constraint violated".to_string(),
