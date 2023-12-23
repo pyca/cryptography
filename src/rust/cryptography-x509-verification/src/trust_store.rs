20aacdd6960312d901e0fb351b4ef28838a3ec13
Alex Gaynor <alex.gaynor@gmail.com>
2023-12-23 14:08:46 -0500

    Avoid linear scan of the entire trust store (#10052)

--- src/rust/cryptography-x509-verification/src/trust_store.rs
+++ src/rust/cryptography-x509-verification/src/trust_store.rs
@@ -2,27 +2,46 @@
 // 2.0, and the BSD License. See the LICENSE file in the root of this repository
 // for complete details.
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use cryptography_x509::certificate::Certificate;
+use cryptography_x509::name::Name;
 
 /// A `Store` represents the core state needed for X.509 path validation.
-pub struct Store<'a>(HashSet<Certificate<'a>>);
+pub struct Store<'a> {
+    certs: HashSet<Certificate<'a>>,
+    by_subject: HashMap<Name<'a>, Vec<Certificate<'a>>>,
+}
 
 impl<'a> Store<'a> {
     /// Create a new `Store` from the given iterable certificate source.
     pub fn new(trusted: impl IntoIterator<Item = Certificate<'a>>) -> Self {
-        Store(HashSet::from_iter(trusted))
+        let certs = HashSet::from_iter(trusted);
+        let mut by_subject: HashMap<Name<'a>, Vec<Certificate<'a>>> = HashMap::new();
+        for cert in certs.iter() {
+            by_subject
+                .entry(cert.tbs_cert.subject.clone())
+                .or_default()
+                .push(cert.clone());
+        }
+        Store { certs, by_subject }
     }
 
     /// Returns whether this store contains the given certificate.
     pub fn contains(&self, cert: &Certificate<'a>) -> bool {
-        self.0.contains(cert)
+        self.certs.contains(cert)
     }
 
     /// Returns an iterator over all certificates in this store.
     pub fn iter(&self) -> impl Iterator<Item = &Certificate<'a>> {
-        self.0.iter()
+        self.certs.iter()
+    }
+
+    pub fn get_by_subject(&self, subject: &Name<'a>) -> &[Certificate<'a>] {
+        self.by_subject
+            .get(subject)
+            .map(|v| v.as_slice())
+            .unwrap_or_default()
     }
 }
 
