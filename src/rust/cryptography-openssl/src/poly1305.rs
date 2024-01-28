75784b3fc57d295e1cf82658d9c67ad249f36a5a
Alex Gaynor <alex.gaynor@gmail.com>
2024-01-28 14:50:18 -0500

    Fix warnings on libressl

--- src/rust/cryptography-openssl/src/poly1305.rs
+++ src/rust/cryptography-openssl/src/poly1305.rs
@@ -18,9 +18,10 @@ impl Poly1305State {
         let mut ctx: Box<MaybeUninit<ffi::poly1305_state>> =
             Box::new(MaybeUninit::<ffi::poly1305_state>::uninit());
 
-        // After initializing the context, unwrap the Box<MaybeUninit<poly1305_state>> into
-        // a Box<poly1305_state> while keeping the same memory address. See the docstring of the
-        // Poly1305State struct above for the rationale.
+        // SAFETY: After initializing the context, unwrap the
+        // `Box<MaybeUninit<poly1305_state>>` into a `Box<poly1305_state>`
+        // while keeping the same memory address. See the docstring of the
+        // `Poly1305State` struct above for the rationale.
         let initialized_ctx: Box<ffi::poly1305_state> = unsafe {
             ffi::CRYPTO_poly1305_init(ctx.as_mut().as_mut_ptr(), key.as_ptr());
             let raw_ctx_ptr = (*Box::into_raw(ctx)).as_mut_ptr();
@@ -32,14 +33,17 @@ impl Poly1305State {
         }
     }
 
-    pub fn update(&mut self, data: &[u8]) -> () {
+    pub fn update(&mut self, data: &[u8]) {
+        // SAFETY: context is valid, as is the data ptr.
         unsafe {
             ffi::CRYPTO_poly1305_update(self.context.as_mut(), data.as_ptr(), data.len());
         };
     }
 
-    pub fn finalize(&mut self, output: &mut [u8]) -> () {
+    pub fn finalize(&mut self, output: &mut [u8]) {
         assert_eq!(output.len(), 16);
+        // SAFETY: context is valid and we verified that the output is the
+        // right length.
         unsafe { ffi::CRYPTO_poly1305_finish(self.context.as_mut(), output.as_mut_ptr()) };
     }
 }
