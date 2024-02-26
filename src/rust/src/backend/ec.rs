0b9fd570dab1c0aadb8097f3d305d44ccf550eb6
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-02-26 18:11:59 +0100

    Return error rather than panic

--- src/rust/src/backend/ec.rs
+++ src/rust/src/backend/ec.rs
@@ -300,7 +300,12 @@ impl ECPrivateKey {
                 }
             } else {
                 let _ = algo;
-                assert!(!deterministic);
+                return Err(CryptographyError::from(
+                    exceptions::UnsupportedAlgorithm::new_err((
+                        "ECDSA with deterministic signature (RFC 6979) is not supported by this version of OpenSSL.",
+                        exceptions::Reasons::UNSUPPORTED_PUBLIC_KEY_ALGORITHM,
+                    )),
+                ));
             }
         }
 
