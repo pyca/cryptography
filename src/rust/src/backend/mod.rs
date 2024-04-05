4d8945d754fc33a92fe2688f5aeb0fa73083be01
Alex Gaynor <alex.gaynor@gmail.com>
2024-04-05 08:37:19 -0400

    Convert `src/backend/ed448.rs` to new pyo3 APIs (#10725)

--- src/rust/src/backend/mod.rs
+++ src/rust/src/backend/mod.rs
@@ -34,7 +34,7 @@ pub(crate) fn add_to_module(module: &pyo3::prelude::PyModule) -> pyo3::PyResult<
 
     module.add_submodule(ed25519::create_module(module.py())?.into_gil_ref())?;
     #[cfg(all(not(CRYPTOGRAPHY_IS_LIBRESSL), not(CRYPTOGRAPHY_IS_BORINGSSL)))]
-    module.add_submodule(ed448::create_module(module.py())?)?;
+    module.add_submodule(ed448::create_module(module.py())?.into_gil_ref())?;
 
     module.add_submodule(x25519::create_module(module.py())?.into_gil_ref())?;
     #[cfg(all(not(CRYPTOGRAPHY_IS_LIBRESSL), not(CRYPTOGRAPHY_IS_BORINGSSL)))]
