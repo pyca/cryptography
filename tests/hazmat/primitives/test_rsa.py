323a3766c20d09c9b26b4aa69f3045f77f76cf30
Paul Kehrer <paul.l.kehrer@gmail.com>
2024-05-27 14:26:41 +0300

    add the test

--- tests/hazmat/primitives/test_rsa.py
+++ tests/hazmat/primitives/test_rsa.py
@@ -2432,6 +2432,21 @@ class TestRSAPrivateKeySerialization:
         priv_num = key.private_numbers()
         assert loaded_priv_num == priv_num
 
+    @pytest.mark.supported(
+        only_if=lambda backend: backend._fips_enabled,
+        skip_message="Requires FIPS",
+    )
+    def test_traditional_serialization_fips(
+        self, rsa_key_2048: rsa.RSAPrivateKey, backend
+    ):
+        key = rsa_key_2048
+        with pytest.raises(ValueError):
+            key.private_bytes(
+                serialization.Encoding.PEM,
+                serialization.PrivateFormat.TraditionalOpenSSL,
+                serialization.BestAvailableEncryption(b"password"),
+            )
+
     @pytest.mark.parametrize(
         ("encoding", "fmt"),
         [
