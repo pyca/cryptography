7c7c2d1cd37c2fbd703f4659c3419a4cbee5fecc
David Buchanan <d@vidbuchanan.co.uk>
2024-06-27 13:45:30 +0100

    utils: guard against zero-length int_to_bytes

--- src/cryptography/utils.py
+++ src/cryptography/utils.py
@@ -41,6 +41,8 @@ def _check_byteslike(name: str, value: bytes) -> None:
 
 
 def int_to_bytes(integer: int, length: int | None = None) -> bytes:
+    if length == 0:
+        raise ValueError("length argument can't be 0")
     return integer.to_bytes(
         length or (integer.bit_length() + 7) // 8 or 1, "big"
     )
