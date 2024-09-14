4b875f7ae6dcab554f2cf9d0b8a53bc1260174d3
Alex Gaynor <alex.gaynor@gmail.com>
2024-09-14 16:28:07 -0400

    Fix linking against C++ runtime library on Windows, macOS

--- src/rust/cryptography-openssl/build.rs
+++ src/rust/cryptography-openssl/build.rs
@@ -28,6 +28,11 @@ fn main() {
 
     if env::var("DEP_OPENSSL_BORINGSSL").is_ok() {
         println!("cargo:rustc-cfg=CRYPTOGRAPHY_IS_BORINGSSL");
-        println!("cargo:rustc-link-lib=stdc++");
+        if env::var_os("CARGO_CFG_UNIX").is_some() {
+            match env::var("CARGO_CFG_TARGET_OS").as_deref() {
+                Ok("macos") => println!("cargo:rustc-link-lib=c++"),
+                _ => println!("cargo:rustc-link-lib=stdc++"),
+            }
+        }
     }
 }
