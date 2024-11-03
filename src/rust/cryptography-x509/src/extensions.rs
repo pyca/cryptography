4ffd400d6d6937bfd66cb247dafba458a2bbe2ec
oleg.hoefling <oleg.hoefling@gmail.com>
2024-11-03 17:25:51 +0100

    feat(admissions): add admission type for the admissions extension
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- src/rust/cryptography-x509/src/extensions.rs
+++ src/rust/cryptography-x509/src/extensions.rs
@@ -312,6 +312,15 @@ pub struct ProfessionInfo<'a> {
     pub add_profession_info: Option<&'a [u8]>,
 }
 
+pub struct Admission<'a> {
+    pub admission_authority: Option<name::GeneralName<'a>>,
+    pub naming_authority: Option<NamingAuthority<'a>>,
+    pub profession_infos: common::Asn1ReadableOrWritable<
+        asn1::SequenceOf<'a, ProfessionInfo<'a>>,
+        asn1::SequenceOfWriter<'a, ProfessionInfo<'a>, Vec<ProfessionInfo<'a>>>,
+    >,
+}
+
 #[cfg(test)]
 mod tests {
     use super::{BasicConstraints, Extension, Extensions, KeyUsage};
