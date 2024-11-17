510a3926b38232b4d279d5051d6ecc8322922929
Paul Kehrer <paul.l.kehrer@gmail.com>
2024-11-17 09:03:18 -0800

    remove sct abc

--- src/cryptography/x509/certificate_transparency.py
+++ src/cryptography/x509/certificate_transparency.py
@@ -4,12 +4,8 @@
 
 from __future__ import annotations
 
-import abc
-import datetime
-
 from cryptography import utils
 from cryptography.hazmat.bindings._rust import x509 as rust_x509
-from cryptography.hazmat.primitives.hashes import HashAlgorithm
 
 
 class LogEntryType(utils.Enum):
@@ -36,62 +32,4 @@ class SignatureAlgorithm(utils.Enum):
     ECDSA = 3
 
 
-class SignedCertificateTimestamp(metaclass=abc.ABCMeta):
-    @property
-    @abc.abstractmethod
-    def version(self) -> Version:
-        """
-        Returns the SCT version.
-        """
-
-    @property
-    @abc.abstractmethod
-    def log_id(self) -> bytes:
-        """
-        Returns an identifier indicating which log this SCT is for.
-        """
-
-    @property
-    @abc.abstractmethod
-    def timestamp(self) -> datetime.datetime:
-        """
-        Returns the timestamp for this SCT.
-        """
-
-    @property
-    @abc.abstractmethod
-    def entry_type(self) -> LogEntryType:
-        """
-        Returns whether this is an SCT for a certificate or pre-certificate.
-        """
-
-    @property
-    @abc.abstractmethod
-    def signature_hash_algorithm(self) -> HashAlgorithm:
-        """
-        Returns the hash algorithm used for the SCT's signature.
-        """
-
-    @property
-    @abc.abstractmethod
-    def signature_algorithm(self) -> SignatureAlgorithm:
-        """
-        Returns the signing algorithm used for the SCT's signature.
-        """
-
-    @property
-    @abc.abstractmethod
-    def signature(self) -> bytes:
-        """
-        Returns the signature for this SCT.
-        """
-
-    @property
-    @abc.abstractmethod
-    def extension_bytes(self) -> bytes:
-        """
-        Returns the raw bytes of any extensions for this SCT.
-        """
-
-
-SignedCertificateTimestamp.register(rust_x509.Sct)
+SignedCertificateTimestamp = rust_x509.Sct
