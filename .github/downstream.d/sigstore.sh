78722fa6b74e322e004baf7d7a68d128f50aa194
William Woodruff <william@trailofbits.com>
2024-11-27 17:07:44 -0500

    sigstore: add a NOTE
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- .github/downstream.d/sigstore.sh
+++ .github/downstream.d/sigstore.sh
@@ -2,6 +2,8 @@
 
 case "${1}" in
     install)
+        # NOTE: placed in /tmp to avoid inscrutable pytest failures
+        # with 'unrecognized arguments: --benchmark-disable'
         git clone --depth=1 https://github.com/sigstore/sigstore-python /tmp/sigstore-python
         cd /tmp/sigstore-python
         git rev-parse HEAD
