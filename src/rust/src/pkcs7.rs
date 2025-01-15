fb78777f7c45117faa1ecb618c34b0e8cd49aa48
Quentin Retourne <32574188+nitneuqr@users.noreply.github.com>
2025-01-15 16:16:27 +0100

    loading the load_der func for all backends

--- src/rust/src/pkcs7.rs
+++ src/rust/src/pkcs7.rs
@@ -21,7 +21,6 @@ use crate::buf::CffiBuf;
 use crate::error::{CryptographyError, CryptographyResult};
 use crate::padding::PKCS7UnpaddingContext;
 use crate::pkcs12::symmetric_encrypt;
-#[cfg(not(CRYPTOGRAPHY_IS_BORINGSSL))]
 use crate::x509::certificate::load_der_x509_certificate;
 use crate::{exceptions, types, x509};
 
