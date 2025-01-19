d9027111cd28c9bf868383704a69b84c7ddd9771
Alex Gaynor <alex.gaynor@gmail.com>
2025-01-19 10:42:56 -0500

    Move PKCS#8 type to proper module

---
+++ src/rust/cryptography-x509/src/pkcs8.rs
@@ -0,0 +1,12 @@
+// This file is dual licensed under the terms of the Apache License, Version
+// 2.0, and the BSD License. See the LICENSE file in the root of this repository
+// for complete details.
+
+use crate::common::AlgorithmIdentifier;
+
+// RFC 5208, Section 6
+#[derive(asn1::Asn1Write)]
+pub struct EncryptedPrivateKeyInfo<'a> {
+    pub encryption_algorithm: AlgorithmIdentifier<'a>,
+    pub encrypted_data: &'a [u8],
+}
