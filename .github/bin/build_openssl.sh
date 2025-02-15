b521f2fcd844d48db097108cb0601064ae353274
Alex Gaynor <alex.gaynor@gmail.com>
2025-02-15 13:32:31 -0500

    remove handling for building our own OpenSSL<3 (#12463)
    * remove handling for building our own OpenSSL<3
    
    * Update build_openssl.sh
    
    * Update build_openssl.sh

--- .github/bin/build_openssl.sh
+++ .github/bin/build_openssl.sh
@@ -2,18 +2,6 @@
 set -e
 set -x
 
-shlib_sed() {
-  # modify the shlib version to a unique one to make sure the dynamic
-  # linker doesn't load the system one.
-  sed -i "s/^SHLIB_MAJOR=.*/SHLIB_MAJOR=100/" Makefile
-  sed -i "s/^SHLIB_MINOR=.*/SHLIB_MINOR=0.0/" Makefile
-  sed -i "s/^SHLIB_VERSION_NUMBER=.*/SHLIB_VERSION_NUMBER=100.0.0/" Makefile
-}
-shlib_sed_3() {
-  # OpenSSL 3 changes how it does the shlib versioning
-  sed -i "s/^SHLIB_VERSION=.*/SHLIB_VERSION=100/" VERSION.dat
-}
-
 if [[ "${TYPE}" == "openssl" ]]; then
   if [[ "${VERSION}" =~ ^[0-9a-f]{40}$ ]]; then
     git clone https://github.com/openssl/openssl
@@ -24,18 +12,14 @@ if [[ "${TYPE}" == "openssl" ]]; then
     tar zxf "openssl-${VERSION}.tar.gz"
     pushd "openssl-${VERSION}"
   fi
-  # For OpenSSL 3 we need to call this before config
-  if [[ "${VERSION}" =~ ^3. ]] || [[ "${VERSION}" =~ ^[0-9a-f]{40}$ ]]; then
-    shlib_sed_3
-  fi
+
+  # modify the shlib version to a unique one to make sure the dynamic
+  # linker doesn't load the system one.
+  sed -i "s/^SHLIB_VERSION=.*/SHLIB_VERSION=100/" VERSION.dat
 
   # CONFIG_FLAGS is a global coming from a previous step
   ./config ${CONFIG_FLAGS} -fPIC --prefix="${OSSL_PATH}"
 
-  # For OpenSSL 1 we need to call this after config
-  if [[ "${VERSION}" =~ ^1. ]]; then
-    shlib_sed
-  fi
   make depend
   make -j"$(nproc)"
   # avoid installing the docs (for performance)
@@ -45,7 +29,7 @@ if [[ "${TYPE}" == "openssl" ]]; then
   rm -rf "${OSSL_PATH}/bin"
   # For OpenSSL 3.0.0 set up the FIPS config. This does not activate it by
   # default, but allows programmatic activation at runtime
-  if [[ "${VERSION}" =~ ^3. && "${CONFIG_FLAGS}" =~ enable-fips ]]; then
+  if [[ "${CONFIG_FLAGS}" =~ enable-fips ]]; then
       # As of alpha16 we have to install it separately and enable it in the config flags
       make -j"$(nproc)" install_fips
       pushd "${OSSL_PATH}"
