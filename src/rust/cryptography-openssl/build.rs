d038f9988742cf2616bc3be67ce9ea817d418da7
Sean McGrail <mcgrails@amazon.com>
2025-03-26 18:08:52 +0000

    Combine logic

--- src/rust/cryptography-openssl/build.rs
+++ src/rust/cryptography-openssl/build.rs
@@ -21,18 +21,16 @@ fn main() {
         println!("cargo:rustc-cfg=CRYPTOGRAPHY_IS_LIBRESSL");
     }
 
-    if env::var("DEP_OPENSSL_BORINGSSL").is_ok() {
-        println!("cargo:rustc-cfg=CRYPTOGRAPHY_IS_BORINGSSL");
-        if env::var_os("CARGO_CFG_UNIX").is_some() {
-            match env::var("CARGO_CFG_TARGET_OS").as_deref() {
-                Ok("macos") => println!("cargo:rustc-link-lib=c++"),
-                _ => println!("cargo:rustc-link-lib=stdc++"),
-            }
-        }
-    }
+    let is_boringssl = env::var("DEP_OPENSSL_BORINGSSL").is_ok();
+    let is_awslc = env::var("DEP_OPENSSL_AWSLC").is_ok();
 
-    if env::var("DEP_OPENSSL_AWSLC").is_ok() {
-        println!("cargo:rustc-cfg=CRYPTOGRAPHY_IS_AWSLC");
+    if is_boringssl || is_awslc {
+        let cfg_name = if is_boringssl {
+            "CRYPTOGRAPHY_IS_BORINGSSL"
+        } else {
+            "CRYPTOGRAPHY_IS_AWSLC"
+        };
+        println!("cargo:rustc-cfg={cfg_name}");
         if env::var_os("CARGO_CFG_UNIX").is_some() {
             match env::var("CARGO_CFG_TARGET_OS").as_deref() {
                 Ok("macos") => println!("cargo:rustc-link-lib=c++"),
