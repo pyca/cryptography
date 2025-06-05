64dea9132939d10de44ad6e15e8c166f863964c6
Sean McGrail <mcgrails@amazon.com>
2025-06-05 20:19:36 +0000

    Don't export function to python per feedback

--- src/rust/src/backend/rand.rs
+++ src/rust/src/backend/rand.rs
@@ -4,15 +4,22 @@
 
 use crate::error::{CryptographyError, CryptographyResult};
 
-#[pyo3::pyfunction]
 pub(crate) fn get_rand_bytes(
     py: pyo3::Python<'_>,
     size: usize,
 ) -> CryptographyResult<pyo3::Bound<'_, pyo3::types::PyBytes>> {
     Ok(pyo3::types::PyBytes::new_with(py, size, |b| {
-        #[cfg(any(CRYPTOGRAPHY_IS_LIBRESSL, CRYPTOGRAPHY_IS_BORINGSSL))]
+        #[cfg(any(
+            CRYPTOGRAPHY_IS_LIBRESSL,
+            CRYPTOGRAPHY_IS_BORINGSSL,
+            CRYPTOGRAPHY_IS_AWSLC
+        ))]
         openssl::rand::rand_bytes(b).map_err(CryptographyError::from)?;
-        #[cfg(not(any(CRYPTOGRAPHY_IS_LIBRESSL, CRYPTOGRAPHY_IS_BORINGSSL)))]
+        #[cfg(not(any(
+            CRYPTOGRAPHY_IS_LIBRESSL,
+            CRYPTOGRAPHY_IS_BORINGSSL,
+            CRYPTOGRAPHY_IS_AWSLC
+        )))]
         openssl::rand::rand_priv_bytes(b).map_err(CryptographyError::from)?;
         Ok(())
     })?)
