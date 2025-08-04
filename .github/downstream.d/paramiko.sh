0be4bf43d95b1805a0b1c4a6852bf5c94b880d93
Jeff Forcier <jeff@bitprophet.org>
2025-08-04 12:34:33 -0400

    Switch paramiko.sh to be fully uv-based (#13240)
    * Switch paramiko.sh to be fully uv-based
    
    This is ~= what I do in local dev after the 4.0 update. I have some local idiosyncracies re: where UV sticks its venv, and whether the venv has pip in it (I create the venvs via `uv venv --seed`), _but_ I don't believe either of those matter for this environment/use case.
    
    * Add inexact, active flags to uv sync
    
    inexact = don't blow away stuff already in the venv (being managed outside this script), active = also, kinda use that venv plz, there's some confusion re: whether it's actually hitting the same path as exterior env.

--- .github/downstream.d/paramiko.sh
+++ .github/downstream.d/paramiko.sh
@@ -5,9 +5,8 @@ case "${1}" in
         git clone --depth=1 https://github.com/paramiko/paramiko
         cd paramiko
         git rev-parse HEAD
-        uv pip install pip
-        uv pip install -e . --group dev
-        pip install . --group dev
+        uv --version
+        uv sync --inexact --active
         ;;
     run)
         cd paramiko
