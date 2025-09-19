51cdeebe870d817c2f0bc5a2918071008a2d34a5
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2025-09-20 00:24:31 +0200

    Unify ASN.1 encoding and decoding tests
    Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

--- tests/hazmat/asn1/test_serialization.py
+++ tests/hazmat/asn1/test_serialization.py
@@ -2,14 +2,27 @@
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
 
+import typing
+
 import pytest
 
 import cryptography.hazmat.asn1 as asn1
 
 
-class TestEncodeInteger:
+def assert_roundtrip(obj: typing.Any, obj_bytes: bytes) -> None:
+    encoded = asn1.encode_der(obj)
+    assert encoded == obj_bytes, (
+        f"Failed for {obj}: got {encoded.hex()}, expected{obj_bytes.hex()}"
+    )
+
+    decoded = asn1.decode_der(type(obj), encoded)
+    assert isinstance(decoded, type(obj))
+    assert decoded == obj
+
+
+class TestInteger:
     @pytest.mark.parametrize(
-        "value,expected",
+        "obj,obj_bytes",
         [
             (0, b"\x02\x01\x00"),
             (1, b"\x02\x01\x01"),
@@ -23,24 +36,17 @@ class TestEncodeInteger:
             (-129, b"\x02\x02\xff\x7f"),
         ],
     )
-    def test_encode_int(self, value: int, expected: bytes) -> None:
-        encoded = asn1.encode_der(value)
-        assert encoded == expected, (
-            f"Failed for {value}: got {encoded.hex()}, expected"
-            f"{expected.hex()}"
-        )
+    def test_int(self, obj: int, obj_bytes: bytes) -> None:
+        assert_roundtrip(obj, obj_bytes)
 
 
-class TestEncodeSequence:
-    def test_encode_ok_sequence_single_field(self) -> None:
+class TestSequence:
+    def test_ok_sequence_single_field(self) -> None:
         @asn1.sequence
         class Example:
             foo: int
 
-        value = Example(foo=9)
-
-        encoded = asn1.encode_der(value)
-        assert encoded == b"\x30\x03\x02\x01\x09"
+        assert_roundtrip(obj=Example(foo=9), obj_bytes=b"\x30\x03\x02\x01\x09")
 
     def test_encode_ok_sequence_multiple_fields(self) -> None:
         @asn1.sequence
@@ -48,10 +54,10 @@ class TestEncodeSequence:
             foo: int
             bar: int
 
-        value = Example(foo=9, bar=6)
-
-        encoded = asn1.encode_der(value)
-        assert encoded == b"\x30\x06\x02\x01\x09\x02\x01\x06"
+        assert_roundtrip(
+            obj=Example(foo=9, bar=6),
+            obj_bytes=b"\x30\x06\x02\x01\x09\x02\x01\x06",
+        )
 
     def test_encode_ok_nested_sequence(self) -> None:
         @asn1.sequence
@@ -62,7 +68,7 @@ class TestEncodeSequence:
         class Parent:
             foo: Child
 
-        value = Parent(foo=Child(foo=9))
-
-        encoded = asn1.encode_der(value)
-        assert encoded == b"\x30\x05\x30\x03\x02\x01\x09"
+        assert_roundtrip(
+            obj=Parent(foo=Child(foo=9)),
+            obj_bytes=b"\x30\x05\x30\x03\x02\x01\x09",
+        )
