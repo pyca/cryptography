d82d37305a4b34204921bfb51bc01afcdbb60cf7
Paul Kehrer <paul.l.kehrer@gmail.com>
2025-10-07 14:19:05 +0200

    Revert OpenSSL API compatibility level
    We still don't support OpenSSL <3.0 except via the forks though

--- src/_cffi_src/openssl/cryptography.py
+++ src/_cffi_src/openssl/cryptography.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 INCLUDES = r"""
 /* define our OpenSSL API compatibility level to 3.0.0. Any symbols older than
    that will raise an error during compilation. */
-#define OPENSSL_API_COMPAT 0x30000000L
+#define OPENSSL_API_COMPAT 0x10100000L
 
 #if defined(_WIN32)
 #ifndef WIN32_LEAN_AND_MEAN
