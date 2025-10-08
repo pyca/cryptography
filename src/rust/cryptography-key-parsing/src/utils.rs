ed234657dad23f3ecdaf8b264da54ab2a60bf594
Alex Gaynor <alex.gaynor@gmail.com>
2025-10-08 13:47:09 +0200

    refactor key serialization code to use matches (#13627)

--- src/rust/cryptography-key-parsing/src/utils.rs
+++ src/rust/cryptography-key-parsing/src/utils.rs
@@ -2,32 +2,16 @@
 // 2.0, and the BSD License. See the LICENSE file in the root of this repository
 // for complete details.
 
-pub(crate) fn is_ed448(id: openssl::pkey::Id) -> bool {
+pub(crate) fn is_dh(id: openssl::pkey::Id) -> bool {
     cfg_if::cfg_if! {
         if #[cfg(not(any(
             CRYPTOGRAPHY_IS_LIBRESSL,
             CRYPTOGRAPHY_IS_BORINGSSL,
             CRYPTOGRAPHY_IS_AWSLC
         )))] {
-            id == openssl::pkey::Id::ED448
+            id == openssl::pkey::Id::DH || id == openssl::pkey::Id::DHX
         } else {
-            _ = id;
-            false
-        }
-    }
-}
-
-pub(crate) fn is_x448(id: openssl::pkey::Id) -> bool {
-    cfg_if::cfg_if! {
-        if #[cfg(not(any(
-            CRYPTOGRAPHY_IS_LIBRESSL,
-            CRYPTOGRAPHY_IS_BORINGSSL,
-            CRYPTOGRAPHY_IS_AWSLC
-        )))] {
-            id == openssl::pkey::Id::X448
-        } else {
-            _ = id;
-            false
+            id == openssl::pkey::Id::DH
         }
     }
 }
